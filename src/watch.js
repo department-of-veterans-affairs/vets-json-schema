e6ed0d2b3ad6cb9f62a106b6e0168a1edb2ddc40
Lihan Li <frankieteardrop@gmail.com>
2016-08-23 13:02:15 -0400

    add npm script for watch

--- src/watch.js
+++ src/watch.js
@@ -3,6 +3,7 @@ var execSync = require('child_process').execSync;
 var watcher = filewatcher();
 
 watcher.add('src');
+console.log('watching for changes in src dir..');
 
 watcher.on('change', function() {
   console.log('rebuilding json');
