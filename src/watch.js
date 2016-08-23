ddab983563f577659e973110cd4a24bd2d41cb64
Lihan Li <frankieteardrop@gmail.com>
2016-08-23 15:03:23 -0400

    show stdout in build command in watcher

--- src/watch.js
+++ src/watch.js
@@ -4,5 +4,5 @@ var execSync = require('child_process').execSync;
 console.log('watching for changes in src dir..');
 
 watch('src', { recursive: true }, function(){
-  execSync('npm run build');
+  execSync('npm run build', { stdio:[0,1,2] });
 });
