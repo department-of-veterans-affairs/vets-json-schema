090cf48913c7013f79a154bf35189afb7db7035e
Lihan Li <frankieteardrop@gmail.com>
2016-08-23 15:00:52 -0400

    put console log in build script

--- src/generate-schemas.js
+++ src/generate-schemas.js
@@ -1,4 +1,5 @@
 import eduBenefitsSchema from './edu-benefits/schema';
 import jsonfile from 'jsonfile';
 
+console.log('building json');
 jsonfile.writeFileSync('dist/edu-benefits-schema.json', eduBenefitsSchema, { spaces: 2 });
--- src/watch.js
+++ src/watch.js
@@ -4,6 +4,5 @@ var execSync = require('child_process').execSync;
 console.log('watching for changes in src dir..');
 
 watch('src', { recursive: true }, function(){
-  console.log('rebuilding json');
   execSync('npm run build');
 });
