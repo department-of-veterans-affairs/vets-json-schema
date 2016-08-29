c66c57d1cc2fc63240eb8fb33986ba392a72b4b0
lihan <frankieteardrop@gmail.com>
2016-08-29 14:44:29 -0400

    Benefits selection fields (#9)
    * update watcher to build before watching
    
    * add benefits relinquished field
    
    * fix benefits relinquished field to match question flow

--- src/watch.js
+++ src/watch.js
@@ -1,8 +1,12 @@
 import watch from 'node-watch';
 import { execSync } from 'child_process';
 
+const build = () => {
+  execSync('npm run build', { stdio:[0,1,2] });
+};
+
+build();
+
 console.log('watching for changes in src dir..');
 
-watch('src', { recursive: true }, () => {
-  execSync('npm run build', { stdio:[0,1,2] });
-});
+watch('src', { recursive: true }, build);
