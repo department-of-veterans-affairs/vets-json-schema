cb4929f099e5a23515118c35e5a696eed6582163
Lihan Li <frankieteardrop@gmail.com>
2016-10-18 16:00:46 -0400

    regex checks if month and date are valid

--- src/edu-benefits/schema.js
+++ src/edu-benefits/schema.js
@@ -73,7 +73,7 @@ module.exports = {
       minimum: 1900
     },
     date: {
-      pattern: '^\\d{4}-\\d{2}-\\d{2}$',
+      pattern: '^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$',
       type: 'string'
     },
     dateRange: {
