fc9b60d62579a8320b63efbdb92266714bc07dd1
Lihan Li <frankieteardrop@gmail.com>
2017-01-09 16:41:10 -0500

    change of program schema test stub

---
+++ test/change-of-program/schema.spec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { changeOfProgram as schema } from '../../dist/schemas';
+import Ajv from 'ajv';
+import _ from 'lodash';
+
+describe('change of program json schema', () => {
+  it('foo', () => {
+    expect(true).to.equal(true);
+  });
+});
