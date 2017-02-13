c56674daa1c025dd70d21c54e9fbee7db637a6b2
Lihan Li <frankieteardrop@gmail.com>
2017-02-13 17:08:28 -0500

    add service branch

--- src/schemas/transfer-benefits/schema.js
+++ src/schemas/transfer-benefits/schema.js
@@ -91,6 +91,9 @@ export default {
     veteranSocialSecurityNumber: {
       $ref: '#/definitions/ssn'
     },
+    serviceBranch: {
+      type: 'string'
+    },
   },
   required: ['privacyAgreementAccepted']
 };
