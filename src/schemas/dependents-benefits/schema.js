4a95d1d1a261455bd47614f148705e2450803934
Lihan Li <frankieteardrop@gmail.com>
2017-02-22 17:51:56 -0500

    add 5490

---
+++ src/schemas/dependents-benefits/schema.js
@@ -0,0 +1,21 @@
+import definitions from '../../common/definitions';
+import _ from 'lodash';
+
+export default {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: "DEPENDENTS' APPLICATION FOR VA EDUCATION BENEFITS",
+  type: 'object',
+  additionalProperties: false,
+  definitions: _.pick(definitions,
+    'ssn',
+  ),
+  properties: {
+    privacyAgreementAccepted: {
+      $ref: '#/definitions/privacyAgreementAccepted'
+    },
+    relativeSocialSecurityNumber: {
+      $ref: '#/definitions/ssn'
+    },
+  },
+  required: ['privacyAgreementAccepted']
+};
