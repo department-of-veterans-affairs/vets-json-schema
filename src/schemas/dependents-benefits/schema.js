47b70ed821c15c5392551d12766db281c8b0bc43
Lihan Li <frankieteardrop@gmail.com>
2017-02-22 17:54:59 -0500

    add 5490 test

--- src/schemas/dependents-benefits/schema.js
+++ src/schemas/dependents-benefits/schema.js
@@ -7,6 +7,7 @@ export default {
   type: 'object',
   additionalProperties: false,
   definitions: _.pick(definitions,
+    'privacyAgreementAccepted',
     'ssn',
   ),
   properties: {
---
+++ test/schemas/dependents-benefits/schema.spec.js
@@ -0,0 +1,16 @@
+import SchemaTestHelper from '../../support/schema-test-helper';
+import { dependentsBenefits as schema } from '../../../dist/schemas';
+import fixtures from '../../support/fixtures';
+import _ from 'lodash';
+import SharedTests from '../../support/shared-tests';
+
+const schemaDefaults = {
+  privacyAgreementAccepted: true
+};
+
+let schemaTestHelper = new SchemaTestHelper(schema, schemaDefaults);
+let sharedTests = new SharedTests(schemaTestHelper);
+
+describe('transfer benefits schema', () => {
+  sharedTests.runTest('ssn', ['relativeSocialSecurityNumber']);
+});
