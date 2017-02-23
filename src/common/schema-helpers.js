eb501e1f28748398a633ef14e63e6493a5de141e
Lihan Li <frankieteardrop@gmail.com>
2017-02-22 19:02:34 -0500

    addDefinitionToSchema helper

--- src/common/schema-helpers.js
+++ src/common/schema-helpers.js
@@ -1,13 +1,33 @@
-const getDefinition = (definition, key) => {
-  if (key == null) key = definition;
+import definitions from './definitions';
 
+const getDefinition = (definition) => {
   return {
-    [key]: {
-      $ref: `#/definitions/${definition}`
-    }
+    $ref: `#/definitions/${definition}`
   };
 };
 
+const addDefinitionToSchema = (schema, definition, key) => {
+  if (key == null) key = definition;
+  let schemaDefinitions = schema.definitions;
+
+  if (schemaDefinitions[definition] == null) {
+    schemaDefinitions[definition] = definitions[definition];
+  };
+
+  const keysArray = key.split('.');
+  let prop = schema.properties;
+
+  keysArray.forEach((k, i) => {
+    if (i === keysArray.length - 1) {
+      prop[k] = getDefinition(definition);
+    } else {
+      if (prop[k] == null) prop[k] = {};
+
+      prop = prop[k];
+    };
+  });
+};
+
 export default {
-  getDefinition
+  addDefinitionToSchema
 };
--- src/schemas/dependents-benefits/schema.js
+++ src/schemas/dependents-benefits/schema.js
@@ -2,20 +2,22 @@ import definitions from '../../common/definitions';
 import schemaHelpers from '../../common/schema-helpers';
 import _ from 'lodash';
 
-export default {
+let schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: "DEPENDENTS' APPLICATION FOR VA EDUCATION BENEFITS",
   type: 'object',
   additionalProperties: false,
-  definitions: _.pick(definitions,
-    'privacyAgreementAccepted',
-    'ssn',
-    'gender',
-  ),
-  properties: _.merge({},
-    schemaHelpers.getDefinition('privacyAgreementAccepted'),
-    schemaHelpers.getDefinition('ssn', 'relativeSocialSecurityNumber'),
-    schemaHelpers.getDefinition('gender'),
-  ),
+  definitions: {},
+  properties: {},
   required: ['privacyAgreementAccepted']
 };
+
+[
+  ['privacyAgreementAccepted'],
+  ['ssn', 'relativeSocialSecurityNumber'],
+  ['gender'],
+].forEach((args) => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
