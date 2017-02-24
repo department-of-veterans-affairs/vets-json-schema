a08950b8e8832917f72b2e0b7afd5a0d100d382b
Jeff Balboni <jbalboni@gmail.com>
2017-02-24 12:25:46 -0500

    Change order of direct deposit change options (#60)

--- src/schemas/change-of-program/schema.js
+++ src/schemas/change-of-program/schema.js
@@ -85,7 +85,7 @@ export default {
     },
     bankAccountChange: {
       type: 'string',
-      enum: ['startUpdate', 'stop', 'noChange']
+      enum: ['noChange', 'startUpdate', 'stop']
     },
     bankAccount: {
       $ref: '#/definitions/bankAccount'
