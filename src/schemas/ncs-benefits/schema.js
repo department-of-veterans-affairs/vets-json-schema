877e204ad21f36c77c12e5486c69fe3e58db9d6c
Lihan Li <frankieteardrop@gmail.com>
2017-03-09 18:39:40 -0500

    ncs benefits schema stub

---
+++ src/schemas/ncs-benefits/schema.js
@@ -0,0 +1,22 @@
+import definitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
+import _ from 'lodash';
+
+let schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: "APPLICATION FOR VA EDUCATION BENEFITS UNDER THE NATIONAL CALL TO SERVICE (NCS) PROGRAM (22-1990N)",
+  type: 'object',
+  additionalProperties: false,
+  definitions: {},
+  properties: {
+  },
+  required: ['privacyAgreementAccepted']
+};
+
+[
+  ['privacyAgreementAccepted'],
+].forEach((args) => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
