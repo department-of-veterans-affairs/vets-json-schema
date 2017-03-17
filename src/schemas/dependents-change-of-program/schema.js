6cca73412bf3a4e5e9346e53662368a0a5143603
Lihan Li <frankieteardrop@gmail.com>
2017-03-17 15:33:19 -0400

    add schema stub

---
+++ src/schemas/dependents-change-of-program/schema.js
@@ -0,0 +1,25 @@
+import definitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
+import _ from 'lodash';
+
+let schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: "DEPENDENTS' REQUEST FOR CHANGE OF PROGRAM OR PLACE OF TRAINING (22-5495)",
+  type: 'object',
+  additionalProperties: false,
+  definitions: _.pick(definitions, [
+    'educationType',
+    'dateRange'
+  ]),
+  properties: {
+  },
+  required: ['privacyAgreementAccepted']
+};
+
+[
+  ['privacyAgreementAccepted'],
+].forEach((args) => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
