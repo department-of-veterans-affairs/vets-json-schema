b61e88885bf6c22b82228dea5e2fabc8ecfeb4f4
Lihan Li <frankieteardrop@gmail.com>
2017-03-28 15:15:46 -0400

    rename 1995 and 5490

--- dist/schemas.js
+++ dist/schemas.js
@@ -1,12 +1,11 @@
 var fs = require('fs');
-var codingInflector = require('coding-inflector');
 
 var schemas = {};
 
 fs.readdirSync(__dirname).forEach((fileName) => {
   if (fileName === 'schemas.js') return;
 
-  schemas[codingInflector.camelize(fileName.replace('.json', '').replace('-schema', ''))] = require('./' + fileName);
+  schemas[fileName.replace('.json', '').replace('-schema', '')] = require('./' + fileName);
 });
 
 module.exports = schemas;
