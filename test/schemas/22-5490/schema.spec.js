0dd0f5b2965ec744e537d37b33c5f763df656261
Lihan Li <frankieteardrop@gmail.com>
2017-03-30 17:47:13 -0400

    omit anyof in 5490 tests

--- test/schemas/22-5490/schema.spec.js
+++ test/schemas/22-5490/schema.spec.js
@@ -10,7 +10,7 @@ const schemaDefaults = {
   privacyAgreementAccepted: true
 };
 
-let schemaTestHelper = new SchemaTestHelper(schema, schemaDefaults);
+let schemaTestHelper = new SchemaTestHelper(_.omit(schema, 'anyOf'), schemaDefaults);
 let sharedTests = new SharedTests(schemaTestHelper);
 
 describe('dependents benefits schema', () => {
