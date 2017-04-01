24f44d37122fe73519273ac4d8285593dfca98d8
lihan <frankieteardrop@gmail.com>
2017-03-31 23:17:49 -0400

    require file number or ssn for form 5490 (#79)
    * require ssn or file shared test
    
    * omit anyof in 5490 tests
    
    * require ssn or file test in 5490
    
    * use shared test in 1990e
    
    * update version

--- test/schemas/22-5490/schema.spec.js
+++ test/schemas/22-5490/schema.spec.js
@@ -10,7 +10,7 @@ const schemaDefaults = {
   privacyAgreementAccepted: true
 };
 
-let schemaTestHelper = new SchemaTestHelper(schema, schemaDefaults);
+let schemaTestHelper = new SchemaTestHelper(_.omit(schema, 'anyOf'), schemaDefaults);
 let sharedTests = new SharedTests(schemaTestHelper);
 
 describe('dependents benefits schema', () => {
@@ -110,4 +110,6 @@ describe('dependents benefits schema', () => {
       name: 1
     }]
   });
+
+  (new SharedTests(new SchemaTestHelper(schema, schemaDefaults))).requireSsnOrFile();
 });
