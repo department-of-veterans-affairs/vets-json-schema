2cdd432b74c6c1ef712521acb92733a4664886bc
Lihan Li <frankieteardrop@gmail.com>
2017-04-27 23:03:11 -0400

    schema spec stub

---
+++ test/schemas/21-527/schema.spec.js
@@ -0,0 +1,13 @@
+import SchemaTestHelper from '../../support/schema-test-helper';
+import schemas from '../../../dist/schemas';
+import fixtures from '../../support/fixtures';
+import _ from 'lodash';
+import SharedTests from '../../support/shared-tests';
+
+const schema = schemas['21-527'];
+
+let schemaTestHelper = new SchemaTestHelper(schema);
+let sharedTests = new SharedTests(schemaTestHelper);
+
+describe('21-527 schema', () => {
+});
