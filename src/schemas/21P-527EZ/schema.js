ef0b9be1c7bf7e0aa4898edd0c915ad4dd9fbb84
lihan <frankieteardrop@gmail.com>
2017-05-26 17:04:07 -0400

    Va hospital change (#97)
    * hospital treatments arr of dates
    
    * change vers

--- src/schemas/21P-527EZ/schema.js
+++ src/schemas/21P-527EZ/schema.js
@@ -157,7 +157,10 @@ let schema = {
       items: {
         type: 'object',
         properties: {
-          dateRange: schemaHelpers.getDefinition('dateRange'),
+          dates: {
+            type: 'array',
+            items: schemaHelpers.getDefinition('date')
+          },
           name: {
             type: 'string'
           },
