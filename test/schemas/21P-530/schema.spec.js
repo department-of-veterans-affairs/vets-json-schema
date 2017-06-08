cbf601bf30e18bcb143299a41de6e84f36608540
Jeff Balboni <jeff@adhocteam.us>
2017-06-08 09:46:06 -0400

    Start burials schema

---
+++ test/schemas/21P-530/schema.spec.js
@@ -0,0 +1,48 @@
+import SchemaTestHelper from '../../support/schema-test-helper';
+import schemas from '../../../dist/schemas';
+import fixtures from '../../support/fixtures';
+import _ from 'lodash';
+import SharedTests from '../../support/shared-tests';
+
+const schema = schemas['21P-530'];
+
+let schemaTestHelper = new SchemaTestHelper(schema);
+let sharedTests = new SharedTests(schemaTestHelper);
+
+describe('21-530 schema', () => {
+  sharedTests.runTest('fullName', ['claimantFullName', 'veteranFullName']);
+
+  sharedTests.runTest('date', ['deathDate', 'burialDate']);
+
+  sharedTests.runTest('vaFileNumber', ['vaFileNumber']);
+
+  schemaTestHelper.testValidAndInvalid('relationship', {
+    valid: [{
+      type: 'spouse'
+    }, {
+      type: 'other',
+      other: 'Whatever'
+    }],
+    invalid: [{
+      type: 'grandfather'
+    }, {
+      type: 'other',
+      other: 1
+    }]
+  });
+
+  schemaTestHelper.testValidAndInvalid('locationOfDeath', {
+    valid: [{
+      location: 'vaMedicalCenter'
+    }, {
+      location: 'other',
+      other: 'Somewhere'
+    }],
+    invalid: [{
+      location: 'asdfa'
+    }, {
+      location: 'other',
+      other: 1
+    }]
+  });
+});
