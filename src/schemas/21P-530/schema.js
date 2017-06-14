d37932d9a7e3d04bc787d05f975683f1a48e4d49
Jeff Balboni <jeff@adhocteam.us>
2017-06-14 14:18:47 -0400

    Add military history fields

--- src/schemas/21P-530/schema.js
+++ src/schemas/21P-530/schema.js
@@ -1,6 +1,32 @@
-import definitions from '../../common/definitions';
+import originalDefinitions from '../../common/definitions';
 import schemaHelpers from '../../common/schema-helpers';
 import _ from 'lodash';
+
+let definitions = _.cloneDeep(originalDefinitions);
+let modifiedToursOfDuty = definitions.toursOfDuty;
+delete modifiedToursOfDuty.items.properties.benefitsToApplyTo;
+delete modifiedToursOfDuty.items.properties.applyPeriodToSelected;
+delete modifiedToursOfDuty.items.properties.serviceStatus;
+delete modifiedToursOfDuty.items.required;
+
+_.merge(modifiedToursOfDuty, {
+  items: {
+    properties: {
+      rank: {
+        type: 'string'
+      },
+      serviceNumber: {
+        type: 'string'
+      },
+      placeOfEntry: {
+        type: 'string'
+      },
+      placeOfSeparation: {
+        type: 'string'
+      }
+    }
+  }
+});
 
 let schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
@@ -8,6 +34,7 @@ let schema = {
   type: 'object',
   additionalProperties: false,
   definitions: {
+    dateRange: definitions.dateRange
   },
   properties: {
     relationship: {
@@ -40,7 +67,8 @@ let schema = {
           type: 'string'
         }
       }
-    }
+    },
+    toursOfDuty: modifiedToursOfDuty
   }
 };
 
