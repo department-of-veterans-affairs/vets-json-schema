d14b8d36d89d0102fa9a2f69c0690feed2226ddb
Ryan Baker <ryan.baker@adhocteam.us>
2017-11-24 13:36:58 -0800

    Initial 22-1900 schema with section 1 fields

---
+++ src/schemas/28-1900/schema.js
@@ -0,0 +1,64 @@
+import constants from '../../common/constants';
+import _ from 'lodash';
+import definitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
+
+let schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'DISABLED VETERANS APPLICATION FOR VOCATIONAL REHABILITATION (28-1900)',
+	type: 'object',
+  additionalProperties: false,
+  definitions: _.pick(definitions, [
+    'fullName',
+    'address',
+    'phone',
+    'ssn',
+    'date',
+    'privacyAgreementAccepted'
+  ]),
+  properties: {
+    veteranFullName: {
+      $ref: '#/definition/fullName'
+    },
+    veteranSocialSecurityNumber: {
+      $ref: '#/definition/ssn'
+    },
+    veteranDateOfBirth: {
+      $ref: '#/definitions/date'
+    },
+    veteranAddress: {
+      $ref: '#/definitions/address'
+    },
+    newVeteranAddress: {
+      $ref: '#/definitions/address'
+    },
+    homePhone: {
+      $ref: '#/definitions/phone'
+    },
+    mobilePhone: {
+      $ref: '#/definitions/phone'
+    },
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    vaRecordsOffice: {
+      type: 'string',
+    },
+    yearsOfEducation: {
+      type: 'number',
+    },
+    privacyAgreementAccepted: {
+      $ref: '#/definitions/privacyAgreementAccepted'
+    }
+  },
+  required: ['privacyAgreementAccepted', 'veteranFullName']
+};
+
+[
+  ['vaFileNumber'],
+].forEach((args) => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
