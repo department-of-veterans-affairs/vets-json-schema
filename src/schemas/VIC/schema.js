f0562fd925589f24545adaace5158fb3f5d9eed0
Jeff Balboni <jeff@adhocteam.us>
2018-01-19 10:37:05 -0500

    Use branch list

--- src/schemas/VIC/schema.js
+++ src/schemas/VIC/schema.js
@@ -11,7 +11,24 @@ let schema = {
   },
   properties: {
     serviceBranch: {
-      type: 'string'
+      type: 'string',
+      enum: [
+        'F', // Air Force
+        'A', // Army
+        'C', // Coast Guard
+        'D', // DoD
+        'M', // Marine Corps
+        'N', // Navy
+        'O', // NOAA
+        'H', // Public Health Service
+        '4', // Foreign Air Force
+        '1', // Foreign Army
+        '6', // Foreign Coast Guard
+        '3', // Foreign Marine Corps
+        '2', // Foreign Navy
+        'X', // Other
+        'Z', // Unknown
+      ]
     },
     email: {
       type: 'string',
