a6305f0e17d15738de7b403ebc78bc51aeff24d9
Jeff Balboni <jbalboni@gmail.com>
2018-02-22 13:31:14 -0500

    Change education program date fields (#186)

--- src/schemas/28-1900/schema.js
+++ src/schemas/28-1900/schema.js
@@ -10,7 +10,8 @@ let schema = {
   additionalProperties: false,
   definitions: _.pick(definitions, [
     'date',
-    'dateRange'
+    'dateRange',
+    'year'
   ]),
   properties: {
     email: {
@@ -22,6 +23,7 @@ let schema = {
     },
     yearsOfEducation: {
       type: 'integer',
+      minimum: 0
     },
     previousPrograms: {
       type: 'array',
@@ -31,8 +33,11 @@ let schema = {
           program: {
             type: 'string'
           },
-          date: {
-            $ref: '#/definitions/date' // TODO Change if partial dates disallowed
+          yearStarted: {
+            $ref: '#/definitions/year'
+          },
+          yearLeft: {
+            $ref: '#/definitions/year'
           }
         }
       }
