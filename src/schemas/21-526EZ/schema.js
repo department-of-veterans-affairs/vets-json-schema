a860570a75e0b0382237e92ffbd2fb22ea9fcdbe
Mark Greenburg <marek.greenburg@gmail.com>
2018-07-30 16:20:55 -0500

    526 fix email validation mg (#233)
    * Update email validations to match swagger
    
    * Update version number

--- src/schemas/21-526EZ/schema.js
+++ src/schemas/21-526EZ/schema.js
@@ -195,7 +195,7 @@ let schema = {
       properties: {
         emailAddress: {
           type: 'string',
-          format: 'email',
+          minLength: 6,
           maxLength: 80,
           pattern: '^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'
         },
