72e8eb31c05789c9e01f607aa24bdfe75d9ce4a0
Claire Hsu <claire@adhocteam.us>
2018-09-12 15:02:35 -0700

    Enforce uppercase schema names in dist (#256)
    * Add uppercased schema to dist
    
    * Bump version

--- src/generate-schemas.js
+++ src/generate-schemas.js
@@ -3,7 +3,7 @@ import definitions from './common/definitions';
 import fs from 'fs';
 
 fs.readdirSync('src/schemas').forEach((schema) => {
-  jsonfile.writeFileSync(`dist/${schema}-schema.json`, require(`./schemas/${schema}/schema`).default, { spaces: 2 });
+  jsonfile.writeFileSync(`dist/${schema.toUpperCase()}-schema.json`, require(`./schemas/${schema}/schema`).default, { spaces: 2 });
 });
 
 jsonfile.writeFileSync('dist/definitions.json', definitions, { spaces: 2 });
