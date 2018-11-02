8b9f82f2f21db0cfdabb73114b2e74d7a5f607b9
Mark Greenburg <marek.greenburg@gmail.com>
2018-11-02 16:25:41 -0500

    Switch to using common defs

--- src/schemas/21-526EZ-allclaims/schema.js
+++ src/schemas/21-526EZ-allclaims/schema.js
@@ -1,78 +1,10 @@
 import _ from 'lodash/fp';
-
-const documentTypes526 = [
-  { value: 'L015', label: 'Buddy/Lay Statement' },
-  { value: 'L018', label: 'Civilian Police Reports' },
-  { value: 'L029', label: 'Copy of a DD214' },
-  { value: 'L702', label: 'Disability Benefits Questionnaire (DBQ)' },
-  { value: 'L703', label: 'Goldmann Perimetry Chart/Field Of Vision Chart' },
-  { value: 'L034', label: 'Military Personnel Record' },
-  { value: 'L478', label: 'Medical Treatment Records - Furnished by SSA' },
-  { value: 'L048', label: 'Medical Treatment Record - Government Facility' },
-  {
-    value: 'L049',
-    label: 'Medical Treatment Record - Non-Government Facility'
-  },
-  { value: 'L023', label: 'Other Correspondence' },
-  { value: 'L070', label: 'Photographs' },
-  { value: 'L450', label: 'STR - Dental - Photocopy' },
-  { value: 'L451', label: 'STR - Medical - Photocopy' },
-  {
-    value: 'L222',
-    label:
-      'VA Form 21-0779 - Request for Nursing Home Information in Connection with Claim for Aid & Attendance'
-  },
-  {
-    value: 'L228',
-    label: 'VA Form 21-0781 - Statement in Support of Claim for PTSD'
-  },
-  {
-    value: 'L229',
-    label:
-      'VA Form 21-0781a - Statement in Support of Claim for PTSD Secondary to Personal Assault'
-  },
-  {
-    value: 'L102',
-    label:
-      'VA Form 21-2680 - Examination for Housebound Status or Permanent Need for Regular Aid & Attendance'
-  },
-  {
-    value: 'L107',
-    label: 'VA Form 21-4142 - Authorization To Disclose Information'
-  },
-  {
-    value: 'L827',
-    label: 'VA Form 21-4142a - General Release for Medical Provider Information'
-  },
-  {
-    value: 'L115',
-    label:
-      'VA Form 21-4192 - Request for Employment Information in Connection with Claim for Disability'
-  },
-  {
-    value: 'L117',
-    label:
-      'VA Form 21-4502 - Application for Automobile or Other Conveyance and Adaptive Equipment Under 38 U.S.C. 3901-3904'
-  },
-  {
-    value: 'L159',
-    label:
-      'VA Form 26-4555 - Application in Acquiring Specially Adapted Housing or Special Home Adaptation Grant'
-  },
-  {
-    value: 'L133',
-    label: 'VA Form 21-674 - Request for Approval of School Attendance'
-  },
-  {
-    value: 'L139',
-    label: 'VA Form 21-686c - Declaration of Status of Dependents'
-  },
-  {
-    value: 'L149',
-    label:
-      'VA Form 21-8940 - Veterans Application for Increased Compensation Based on Un-employability'
-  }
-];
+import definitions from '../../common/definitions';
+import {
+  documentTypes526,
+  pciuCountries,
+  pciuStates
+} from '../../common/constants';
 
 const serviceBranches = [
   'Air Force',
@@ -84,226 +16,43 @@ const serviceBranches = [
   'Public Health Service'
 ];
 
+const disabilitiesBaseDef = {
+  type: 'array',
+  maxItems: 100,
+  items: {
+    type: 'object',
+    required: ['name', 'disabilityActionType'],
+    properties: {
+      name: {
+        type: 'string'
+      },
+      disabilityActionType: {
+        type: 'string',
+        enum: ['NONE', 'NEW', 'SECONDARY', 'INCREASE', 'REOPEN']
+      },
+      ratedDisabilityId: {
+        type: 'string'
+      },
+      ratingDecisionId: {
+        type: 'string'
+      },
+      diagnosticCode: {
+        type: 'number'
+      },
+      classificationCode: {
+        type: 'string'
+      }
+    }
+  }
+};
+
 const baseAddressDef = {
   type: 'object',
   required: ['country', 'city', 'addressLine1'],
   properties: {
     country: {
       type: 'string',
-      enum: [
-        'Afghanistan',
-        'Albania',
-        'Algeria',
-        'Angola',
-        'Anguilla',
-        'Antigua',
-        'Antigua and Barbuda',
-        'Argentina',
-        'Armenia',
-        'Australia',
-        'Austria',
-        'Azerbaijan',
-        'Azores',
-        'Bahamas',
-        'Bahrain',
-        'Bangladesh',
-        'Barbados',
-        'Barbuda',
-        'Belarus',
-        'Belgium',
-        'Belize',
-        'Benin',
-        'Bermuda',
-        'Bhutan',
-        'Bolivia',
-        'Bosnia-Herzegovina',
-        'Botswana',
-        'Brazil',
-        'Brunei',
-        'Bulgaria',
-        'Burkina Faso',
-        'Burma',
-        'Burundi',
-        'Cambodia',
-        'Cameroon',
-        'Canada',
-        'Cape Verde',
-        'Cayman Islands',
-        'Central African Republic',
-        'Chad',
-        'Chile',
-        'China',
-        'Colombia',
-        'Comoros',
-        'Congo, Democratic Republic of',
-        "Congo, People's Republic of",
-        'Costa Rica',
-        "Cote d'Ivoire",
-        'Croatia',
-        'Cuba',
-        'Cyprus',
-        'Czech Republic',
-        'Denmark',
-        'Djibouti',
-        'Dominica',
-        'Dominican Republic',
-        'Ecuador',
-        'Egypt',
-        'El Salvador',
-        'England',
-        'Equatorial Guinea',
-        'Eritrea',
-        'Estonia',
-        'Ethiopia',
-        'Fiji',
-        'Finland',
-        'France',
-        'French Guiana',
-        'Gabon',
-        'Gambia',
-        'Georgia',
-        'Germany',
-        'Ghana',
-        'Gibraltar',
-        'Great Britain',
-        'Great Britain and Gibraltar',
-        'Greece',
-        'Greenland',
-        'Grenada',
-        'Guadeloupe',
-        'Guatemala',
-        'Guinea',
-        'Guinea, Republic of Guinea',
-        'Guinea-Bissau',
-        'Guyana',
-        'Haiti',
-        'Honduras',
-        'Hong Kong',
-        'Hungary',
-        'Iceland',
-        'India',
-        'Indonesia',
-        'Iran',
-        'Iraq',
-        'Ireland',
-        'Israel (Jerusalem)',
-        'Israel (Tel Aviv)',
-        'Italy',
-        'Jamaica',
-        'Japan',
-        'Jordan',
-        'Kazakhstan',
-        'Kenya',
-        'Kosovo',
-        'Kuwait',
-        'Kyrgyzstan',
-        'Laos',
-        'Latvia',
-        'Lebanon',
-        'Leeward Islands',
-        'Lesotho',
-        'Liberia',
-        'Libya',
-        'Liechtenstein',
-        'Lithuania',
-        'Luxembourg',
-        'Macao',
-        'Macedonia',
-        'Madagascar',
-        'Malawi',
-        'Malaysia',
-        'Mali',
-        'Malta',
-        'Martinique',
-        'Mauritania',
-        'Mauritius',
-        'Mexico',
-        'Moldavia',
-        'Mongolia',
-        'Montenegro',
-        'Montserrat',
-        'Morocco',
-        'Mozambique',
-        'Namibia',
-        'Nepal',
-        'Netherlands',
-        'Netherlands Antilles',
-        'Nevis',
-        'New Caledonia',
-        'New Zealand',
-        'Nicaragua',
-        'Niger',
-        'Nigeria',
-        'North Korea',
-        'Northern Ireland',
-        'Norway',
-        'Oman',
-        'Pakistan',
-        'Panama',
-        'Papua New Guinea',
-        'Paraguay',
-        'Peru',
-        'Philippines',
-        'Philippines (restricted payments)',
-        'Poland',
-        'Portugal',
-        'Qatar',
-        'Republic of Yemen',
-        'Romania',
-        'Russia',
-        'Rwanda',
-        'Sao-Tome/Principe',
-        'Saudi Arabia',
-        'Scotland',
-        'Senegal',
-        'Serbia',
-        'Serbia/Montenegro',
-        'Seychelles',
-        'Sicily',
-        'Sierra Leone',
-        'Singapore',
-        'Slovakia',
-        'Slovenia',
-        'Somalia',
-        'South Africa',
-        'South Korea',
-        'Spain',
-        'Sri Lanka',
-        'St. Kitts',
-        'St. Lucia',
-        'St. Vincent',
-        'Sudan',
-        'Suriname',
-        'Swaziland',
-        'Sweden',
-        'Switzerland',
-        'Syria',
-        'Taiwan',
-        'Tajikistan',
-        'Tanzania',
-        'Thailand',
-        'Togo',
-        'Trinidad and Tobago',
-        'Tunisia',
-        'Turkey (Adana only)',
-        'Turkey (except Adana)',
-        'Turkmenistan',
-        'USA',
-        'Uganda',
-        'Ukraine',
-        'United Arab Emirates',
-        'United Kingdom',
-        'Uruguay',
-        'Uzbekistan',
-        'Vanuatu',
-        'Venezuela',
-        'Vietnam',
-        'Wales',
-        'Western Samoa',
-        'Yemen Arab Republic',
-        'Zambia',
-        'Zimbabwe'
-      ],
+      enum: pciuCountries,
       default: 'USA'
     },
     addressLine1: {
@@ -328,138 +77,8 @@ const baseAddressDef = {
     },
     state: {
       type: 'string',
-      enum: [
-        'AL',
-        'UM',
-        'AS',
-        'AZ',
-        'AR',
-        'AA',
-        'AE',
-        'AP',
-        'CA',
-        'CO',
-        'CT',
-        'DE',
-        'DC',
-        'FM',
-        'FL',
-        'GA',
-        'GU',
-        'HI',
-        'ID',
-        'IL',
-        'IN',
-        'IA',
-        'KS',
-        'KY',
-        'LA',
-        'ME',
-        'MH',
-        'MD',
-        'MA',
-        'MI',
-        'MN',
-        'MS',
-        'AK',
-        'MT',
-        'NE',
-        'NV',
-        'NH',
-        'NJ',
-        'NM',
-        'NY',
-        'NC',
-        'ND',
-        'MP',
-        'OH',
-        'OK',
-        'OR',
-        'PW',
-        'PA',
-        'PR',
-        'RI',
-        'SC',
-        'SD',
-        'TN',
-        'TX',
-        'UT',
-        'VT',
-        'VI',
-        'VA',
-        'WA',
-        'WV',
-        'WI',
-        'WY',
-        'PI',
-        'MO'
-      ],
-      enumNames: [
-        'Alabama',
-        'U.S. Minor Outlying Islands',
-        'American Samoa',
-        'Arizona',
-        'Arkansas',
-        'Armed Forces Americas (AA)',
-        'Armed Forces Europe (AE)',
-        'Armed Forces Pacific (AP)',
-        'California',
-        'Colorado',
-        'Connecticut',
-        'Delaware',
-        'District Of Columbia',
-        'Federated States Of Micronesia',
-        'Florida',
-        'Georgia',
-        'Guam',
-        'Hawaii',
-        'Idaho',
-        'Illinois',
-        'Indiana',
-        'Iowa',
-        'Kansas',
-        'Kentucky',
-        'Louisiana',
-        'Maine',
-        'Marshall Islands',
-        'Maryland',
-        'Massachusetts',
-        'Michigan',
-        'Minnesota',
-        'Mississippi',
-        'Alaska',
-        'Montana',
-        'Nebraska',
-        'Nevada',
-        'New Hampshire',
-        'New Jersey',
-        'New Mexico',
-        'New York',
-        'North Carolina',
-        'North Dakota',
-        'Northern Mariana Islands',
-        'Ohio',
-        'Oklahoma',
-        'Oregon',
-        'Palau',
-        'Pennsylvania',
-        'Puerto Rico',
-        'Rhode Island',
-        'South Carolina',
-        'South Dakota',
-        'Tennessee',
-        'Texas',
-        'Utah',
-        'Vermont',
-        'Virgin Islands',
-        'Virginia',
-        'Washington',
-        'West Virginia',
-        'Wisconsin',
-        'Wyoming',
-        'Philippine Islands',
-        'Missouri'
-      ]
+      enum: pciuStates.map(state => state.value),
+      enumNames: pciuStates.map(state => state.label)
     },
     zipCode: {
       type: 'string',
@@ -485,15 +104,8 @@ const schema = {
   title: 'APPLICATION FOR DISABILITY BENEFITS',
   type: 'object',
   definitions: {
-    phone: {
-      type: 'string',
-      pattern: '^\\d{10}$'
-    },
-    date: {
-      pattern:
-        '^(\\d{4}|XXXX)-(0[1-9]|1[0-2]|XX)-(0[1-9]|[1-2][0-9]|3[0-1]|XX)$',
-      type: 'string'
-    },
+    phone: definitions.usaPhone,
+    date: definitions.date,
     email: {
       type: 'string',
       minLength: 6,
@@ -503,41 +115,21 @@ const schema = {
     },
     address: baseAddressDef,
     vaTreatmentCenterAddress: vaTreatmentCenterAddressDef,
-    dateRangeAllRequired: {
-      type: 'object',
-      properties: {
-        from: {
-          $ref: '#/definitions/date'
-        },
-        to: {
-          $ref: '#/definitions/date'
-        }
-      },
-      required: ['from', 'to']
-    },
-    dateRangeFromRequired: {
-      type: 'object',
-      properties: {
-        from: {
-          $ref: '#/definitions/date'
-        },
-        to: {
-          $ref: '#/definitions/date'
-        }
-      },
-      required: ['from']
-    },
-    dateRange: {
-      type: 'object',
-      properties: {
-        from: {
-          $ref: '#/definitions/date'
-        },
-        to: {
-          $ref: '#/definitions/date'
+    dateRangeAllRequired: _.set(
+      'required',
+      ['from', 'to'],
+      definitions.dateRange
+    ),
+    dateRangeFromRequired: _.set('required', ['from'], definitions.dateRange),
+    dateRange: definitions.dateRange,
+    disabilities: _.merge(disabilitiesBaseDef, {
+      minItems: 1,
+      items: {
+        properties: {
+          secondaryDisabilities: disabilitiesBaseDef
         }
       }
-    },
+    }),
     form4142: {
       type: 'object',
       properties: {
@@ -785,63 +377,7 @@ const schema = {
       type: 'boolean'
     },
     disabilities: {
-      type: 'array',
-      minItems: 1,
-      maxItems: 100,
-      items: {
-        type: 'object',
-        required: ['name', 'disabilityActionType'],
-        properties: {
-          name: {
-            type: 'string'
-          },
-          disabilityActionType: {
-            type: 'string',
-            enum: ['NONE', 'NEW', 'SECONDARY', 'INCREASE', 'REOPEN']
-          },
-          ratedDisabilityId: {
-            type: 'string'
-          },
-          ratingDecisionId: {
-            type: 'string'
-          },
-          diagnosticCode: {
-            type: 'number'
-          },
-          classificationCode: {
-            type: 'string'
-          },
-          secondaryDisabilities: {
-            type: 'array',
-            maxItems: 100,
-            items: {
-              type: 'object',
-              required: ['name', 'disabilityActionType'],
-              properties: {
-                name: {
-                  type: 'string'
-                },
-                disabilityActionType: {
-                  type: 'string',
-                  enum: ['NONE', 'NEW', 'SECONDARY', 'INCREASE', 'REOPEN']
-                },
-                ratedDisabilityId: {
-                  type: 'string'
-                },
-                ratingDecisionId: {
-                  type: 'string'
-                },
-                diagnosticCode: {
-                  type: 'number'
-                },
-                classificationCode: {
-                  type: 'string'
-                }
-              }
-            }
-          }
-        }
-      }
+      $ref: '#/definitions/disabilities'
     },
     newDisabilities: {
       type: 'array',
