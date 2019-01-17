d825b47fc23af7df7f4f746ea766459459daa858
Mark Greenburg <marek.greenburg@gmail.com>
2019-01-17 16:29:06 -0600

    Add `classificationCode` to newDisabilities schema

--- src/schemas/21-526EZ-allclaims/schema.js
+++ src/schemas/21-526EZ-allclaims/schema.js
@@ -162,6 +162,9 @@ const schema = {
             type: 'string',
             enum: ['NEW', 'SECONDARY', 'WORSENED', 'VA']
           },
+          classificationCode: {
+            type: 'string'
+          },
           primaryDescription: {
             type: 'string'
           },
