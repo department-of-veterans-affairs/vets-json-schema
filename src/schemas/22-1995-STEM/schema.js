6364933d34e3d5b3535602637fb1505b47738e0f
Devin McCurdy <mccurdy_devin@bah.com>
2019-08-23 13:37:34 -0400

    Add 1995-stem schema

--- src/schemas/22-1995-STEM/schema.js
+++ src/schemas/22-1995-STEM/schema.js
@@ -6,121 +6,133 @@ let definitions = _.cloneDeep(originalDefinitions);
 definitions.educationType.enum.push('cooperativeTraining');
 
 let schema = {
-    $schema: 'http://json-schema.org/draft-04/schema#',
-    title: 'REQUEST FOR CHANGE OF PROGRAM OR PLACE OF TRAINING (22-1995-STEM)',
-    type: 'object',
-    additionalProperties: false,
-    definitions: _.pick(definitions, [
-        'fullName',
-        'address',
-        'phone',
-        'ssn',
-        'school',
-        'bankAccount',
-        'serviceBefore1977',
-        'date',
-        'dateRange',
-        'educationType',
-        'preferredContactMethod',
-        'privacyAgreementAccepted'
-    ]),
-    anyOf: [
-        {
-            "required" : ["vaFileNumber"]
-        },
-        {
-            "required" : ["veteranSocialSecurityNumber"]
-        }
-    ],
-    properties: {
-        veteranFullName: {
-            $ref: '#/definitions/fullName'
-        },
-        veteranAddress: {
-            $ref: '#/definitions/address'
-        },
-        homePhone: {
-            $ref: '#/definitions/phone'
-        },
-        mobilePhone: {
-            $ref: '#/definitions/phone'
-        },
-        email: {
-            type: 'string',
-            format: 'email'
-        },
-        preferredContactMethod: {
-            $ref: '#/definitions/preferredContactMethod'
-        },
-        veteranSocialSecurityNumber: {
-            $ref: '#/definitions/ssn'
-        },
-        benefit: {
-            type: 'string',
-            enum: ['chapter33', 'chapter30', 'chapter1606', 'transferOfEntitlement', 'chapter32', 'chapter1607']
-        },
-        educationType: {
-            $ref: '#/definitions/educationType'
-        },
-        educationObjective: {
-            type: 'string'
-        },
-        programName: {
-            type: 'string'
-        },
-        newSchool: {
-            $ref: '#/definitions/school'
-        },
-        oldSchool: {
-            $ref: '#/definitions/school'
-        },
-        trainingEndDate: {
-            $ref: '#/definitions/date'
-        },
-        reasonForChange: {
-            type: 'string'
-        },
-        bankAccount: {
-            $ref: '#/definitions/bankAccount'
-        },
-        serviceBefore1977: {
-            $ref: '#/definitions/serviceBefore1977'
-        },
-        toursOfDuty: {
-            type: 'array',
-            items: {
-                type: 'object',
-                properties: {
-                    serviceBranch: {
-                        type: 'string'
-                    },
-                    dateRange: {
-                        $ref: '#/definitions/dateRange'
-                    }
-                }
-            }
-        },
-        civilianBenefitsAssistance: {
-            type: 'boolean'
-        },
-        nonVaAssistance: {
-            type: 'boolean'
-        },
-        remarks: {
-            type: 'string'
-        },
-        privacyAgreementAccepted: {
-            $ref: '#/definitions/privacyAgreementAccepted'
-        }
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'REQUEST FOR CHANGE OF PROGRAM OR PLACE OF TRAINING (22-1995-STEM)',
+  type: 'object',
+  additionalProperties: false,
+  definitions: _.pick(definitions, [
+    'fullName',
+    'address',
+    'phone',
+    'ssn',
+    'school',
+    'bankAccount',
+    'serviceBefore1977',
+    'date',
+    'dateRange',
+    'educationType',
+    'preferredContactMethod',
+    'privacyAgreementAccepted'
+  ]),
+  anyOf: [
+    {
+      "required" : ["vaFileNumber"]
     },
-    required: ['privacyAgreementAccepted', 'veteranFullName']
+    {
+      "required" : ["veteranSocialSecurityNumber"]
+    }
+  ],
+  properties: {
+    veteranFullName: {
+      $ref: '#/definitions/fullName'
+    },
+    veteranAddress: {
+      $ref: '#/definitions/address'
+    },
+    homePhone: {
+      $ref: '#/definitions/phone'
+    },
+    mobilePhone: {
+      $ref: '#/definitions/phone'
+    },
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    preferredContactMethod: {
+      $ref: '#/definitions/preferredContactMethod'
+    },
+    veteranSocialSecurityNumber: {
+      $ref: '#/definitions/ssn'
+    },
+    benefit: {
+      type: 'string',
+      enum: ['chapter33', 'chapter30', 'chapter1606', 'transferOfEntitlement', 'chapter32', 'chapter1607']
+    },
+    educationType: {
+      $ref: '#/definitions/educationType'
+    },
+    educationObjective: {
+      type: 'string'
+    },
+    programName: {
+      type: 'string'
+    },
+    newSchool: {
+      $ref: '#/definitions/school'
+    },
+    oldSchool: {
+      $ref: '#/definitions/school'
+    },
+    trainingEndDate: {
+      $ref: '#/definitions/date'
+    },
+    reasonForChange: {
+      type: 'string'
+    },
+    bankAccount: {
+      $ref: '#/definitions/bankAccount'
+    },
+    serviceBefore1977: {
+      $ref: '#/definitions/serviceBefore1977'
+    },
+    toursOfDuty: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          serviceBranch: {
+            type: 'string'
+          },
+          dateRange: {
+            $ref: '#/definitions/dateRange'
+          }
+        }
+      }
+    },
+    civilianBenefitsAssistance: {
+      type: 'boolean'
+    },
+    nonVaAssistance: {
+      type: 'boolean'
+    },
+    remarks: {
+      type: 'string'
+    },
+    privacyAgreementAccepted: {
+      $ref: '#/definitions/privacyAgreementAccepted'
+    },
+    activeDuty: {
+      type: "boolean"
+    },
+    applyingForStem: {
+      type: 'boolean'
+    },
+    currentlyEnrolledInStemProgram: {
+      type: 'boolean'
+    },
+    pursuingStemTeachingCertification: {
+      type: 'boolean'
+    },
+  },
+  required: ['privacyAgreementAccepted', 'veteranFullName']
 };
 
 [
-    ['vaFileNumber'],
-    ['bankAccountChange']
+  ['vaFileNumber'],
+  ['bankAccountChange']
 ].forEach((args) => {
-    schemaHelpers.addDefinitionToSchema(schema, ...args);
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
 });
 
 export default schema;
