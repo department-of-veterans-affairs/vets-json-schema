01a639cb7a1e8bd22f1d0cf07ae60dc6e6496874
LinnJS <justinslinn727@gmail.com>
2020-01-09 17:27:58 -0500

    Stored repeated values in constants

--- src/schemas/10-10CG/schema.js
+++ src/schemas/10-10CG/schema.js
@@ -13,6 +13,38 @@ const booleanType = {
   type: 'boolean'
 };
 
+const fullName = {
+  $ref: "#/definitions/fullName"
+};
+
+const ssn = {
+  $ref: "#/definitions/ssn"
+};
+
+const dateOfBirth = {
+  $ref: '#/definitions/dateOfBirth'
+};
+
+const gender = {
+  $ref: "#/definitions/gender"
+};
+
+const phone = {
+  $ref: "#/definitions/phone"
+};
+
+const email = {
+  $ref: "#/definitions/email"
+};
+
+const address = {
+  $ref: "#/definitions/address"
+};
+
+const vetRelationship = {
+  type: 'string',
+};
+
 
 const schema = {
   $schema: "http://json-schema.org/draft-04/schema#",
@@ -25,38 +57,22 @@ const schema = {
       type: "object",
       required: [],
       properties: {
-        fullName: {
-          $ref: "#/definitions/fullName"
-        },
+        fullName: fullName,
         ssnOrTin: ssnOrTin,
-        ssn: {
-          $ref: "#/definitions/ssn"
-        },
+        ssn: ssn,
         tin: tin,
-        dateOfBirth: {
-          $ref: '#/definitions/dateOfBirth'
-        },
-        gender: {
-          $ref: "#/definitions/gender"
-        }
+        dateOfBirth: dateOfBirth,
+        gender: gender
       }
     },
     veteranInfoTwo: {
       type: "object",
       required: [],
       properties: {
-        fullName: {
-          $ref: "#/definitions/address"
-        },
-        primaryPhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        alternativePhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        email: {
-          $ref: "#/definitions/email"
-        }
+        fullName: fullName,
+        primaryPhoneNumber: phone,
+        alternativePhoneNumber: phone,
+        email: email
       }
     },
     veteranInfoThree: {
@@ -82,35 +98,21 @@ const schema = {
       type: "object",
       required: [],
       properties: {
-        fullName: {
-          $ref: "#/definitions/fullName"
-        },
+        fullName: fullName,
         ssnOrTin: ssnOrTin,
-        ssn: {
-          $ref: "#/definitions/ssn"
-        },
+        ssn: ssn,
         tin: tin,
-        dateOfBirth: {
-          $ref: '#/definitions/dateOfBirth'
-        },
-        gender: {
-          $ref: "#/definitions/gender"
-        }
+        dateOfBirth: dateOfBirth,
+        gender: gender
       }
     },
     primaryCaregiverInfoTwo: {
       type: "object",
       required: [],
       properties: {
-        primaryPhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        alternativePhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        email: {
-          $ref: "#/definitions/email"
-        },
+        primaryPhoneNumber: phone,
+        alternativePhoneNumber: phone,
+        email: email,
         vetRelationship: {
           type: 'string',
         },
@@ -129,273 +131,37 @@ const schema = {
       type: "object",
       required: [],
       properties: {
-        fullName: {
-          $ref: "#/definitions/fullName"
-        },
+        fullName: fullName,
         ssnOrTin: ssnOrTin,
-        ssn: {
-          $ref: "#/definitions/ssn"
-        },
+        ssn: ssn,
         tin: tin,
-        dateOfBirth: {
-          $ref: '#/definitions/dateOfBirth'
-        },
-        gender: {
-          $ref: "#/definitions/gender"
-        },
-        address: {
-          $ref: "#/definitions/address"
-        },
-        primaryPhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        alternativePhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        email: {
-          $ref: "#/definitions/email"
-        },
-        vetRelationship: {
-          type: 'string',
-        },
+        dateOfBirth: dateOfBirth,
+        gender: gender,
+        address: address,
+        primaryPhoneNumber: phone,
+        alternativePhoneNumber: phone,
+        email: email,
+        vetRelationship: vetRelationship,
       }
     },
     secondaryTwoCaregiverInfo: {
       type: "object",
       required: [],
       properties: {
-        fullName: {
-          $ref: "#/definitions/fullName"
-        },
+        fullName: fullName,
         ssnOrTin: ssnOrTin,
-        ssn: {
-          $ref: "#/definitions/ssn"
-        },
+        ssn: ssn,
         tin: tin,
-        dateOfBirth: {
-          $ref: '#/definitions/dateOfBirth'
-        },
-        gender: {
-          $ref: "#/definitions/gender"
-        },
-        address: {
-          $ref: "#/definitions/address"
-        },
-        primaryPhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        alternativePhoneNumber: {
-          $ref: "#/definitions/phone"
-        },
-        email: {
-          $ref: "#/definitions/email"
-        },
-        vetRelationship: {
-          type: 'string',
-        },
+        dateOfBirth: dateOfBirth,
+        gender: gender,
+        address: address,
+        primaryPhoneNumber: phone,
+        alternativePhoneNumber: phone,
+        email: email,
+        vetRelationship: vetRelationship,
         hasSecondaryOneCaregiver: booleanType,
       }
     },
-    address: {
-      type: "object",
-      oneOf: [
-        {
-          properties: {
-            country: {
-              type: "string",
-              enum: ["CAN"]
-            },
-            state: {
-              type: "string",
-              enum: [
-                "AB",
-                "BC",
-                "MB",
-                "NB",
-                "NF",
-                "NT",
-                "NV",
-                "NU",
-                "ON",
-                "PE",
-                "QC",
-                "SK",
-                "YT"
-              ]
-            },
-            postalCode: {
-              type: "string",
-              maxLength: 10
-            }
-          }
-        },
-        {
-          properties: {
-            country: {
-              type: "string",
-              enum: ["MEX"]
-            },
-            state: {
-              type: "string",
-              enum: [
-                "aguascalientes",
-                "baja-california-norte",
-                "baja-california-sur",
-                "campeche",
-                "chiapas",
-                "chihuahua",
-                "coahuila",
-                "colima",
-                "distrito-federal",
-                "durango",
-                "guanajuato",
-                "guerrero",
-                "hidalgo",
-                "jalisco",
-                "mexico",
-                "michoacan",
-                "morelos",
-                "nayarit",
-                "nuevo-leon",
-                "oaxaca",
-                "puebla",
-                "queretaro",
-                "quintana-roo",
-                "san-luis-potosi",
-                "sinaloa",
-                "sonora",
-                "tabasco",
-                "tamaulipas",
-                "tlaxcala",
-                "veracruz",
-                "yucatan",
-                "zacatecas"
-              ]
-            },
-            postalCode: {
-              type: "string",
-              maxLength: 10
-            }
-          }
-        },
-        {
-          properties: {
-            country: {
-              type: "string",
-              enum: ["USA"]
-            },
-            state: {
-              type: "string",
-              enum: [
-                "AL",
-                "AK",
-                "AS",
-                "AZ",
-                "AR",
-                "AA",
-                "AE",
-                "AP",
-                "CA",
-                "CO",
-                "CT",
-                "DE",
-                "DC",
-                "FM",
-                "FL",
-                "GA",
-                "GU",
-                "HI",
-                "ID",
-                "IL",
-                "IN",
-                "IA",
-                "KS",
-                "KY",
-                "LA",
-                "ME",
-                "MH",
-                "MD",
-                "MA",
-                "MI",
-                "MN",
-                "MS",
-                "MO",
-                "MT",
-                "NE",
-                "NV",
-                "NH",
-                "NJ",
-                "NM",
-                "NY",
-                "NC",
-                "ND",
-                "MP",
-                "OH",
-                "OK",
-                "OR",
-                "PW",
-                "PA",
-                "PR",
-                "RI",
-                "SC",
-                "SD",
-                "TN",
-                "TX",
-                "UT",
-                "VT",
-                "VI",
-                "VA",
-                "WA",
-                "WV",
-                "WI",
-                "WY"
-              ]
-            },
-            postalCode: {
-              type: "string",
-              "ui:title": "Postal Code",
-              maxLength: 10
-            }
-          }
-        },
-        {
-          properties: {
-            country: {
-              not: {
-                type: "string",
-                enum: ["CAN", "MEX", "USA"]
-              }
-            },
-            state: {
-              type: "string",
-              maxLength: 51
-            },
-            postalCode: {
-              type: "string",
-              maxLength: 51
-            }
-          }
-        }
-      ],
-      properties: {
-        street: {
-          type: "string",
-          minLength: 1,
-          maxLength: 50
-        },
-        street2: {
-          "ui:title": "Street Two",
-          type: "string",
-          minLength: 1,
-          maxLength: 50
-        },
-        city: {
-          type: "string",
-          minLength: 1,
-          maxLength: 51
-        }
-      }
-    }
   }
 };
 
