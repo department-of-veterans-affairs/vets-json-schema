b4b5c0e2be2e7739241e45407c70b47d3246ab40
LinnJS <justinslinn727@gmail.com>
2020-01-09 16:07:29 -0500

    Add labels on test data

--- src/schemas/10-10CG/test-data.js
+++ src/schemas/10-10CG/test-data.js
@@ -1,42 +1,27 @@
 const schema = {
-  secondaryOneFullName: {
-    first: "John Doe",
-    last: "Doe",
-    suffix: "II"
-  },
-  secondaryOneSsn: "787787854",
-  secondaryOneDateOfBirth: "1989-11-06",
-  secondaryOneGender: "Male",
-  secondaryOneAddress: {
+  // Vet Info
+  veteranVaEnrolled: false,
+  veteranFacilityType: "hospital",
+  veteranPreviousTreatmentFacility: "adsadad",
+  veteranAddress: {
     street: "111 2nd St S",
     city: "Seattle",
     country: "USA",
-    state: "IL",
+    state: "IN",
     postalCode: "33771"
   },
-  secondaryOnePrimaryPhoneNumber: "8887775544",
-  secondaryOneAlternativePhoneNumber: "8887775544",
-  secondaryOneEmail: "test@email.com",
-  secondaryOneVetRelationship: "asdasdas",
-  hasSecondaryTwoCaregiver: true,
-  secondaryTwoFullName: {
-    first: "John Doe",
+  veteranTelephoneNumber: "8887775544",
+  veteranCellNumber: "8887775544",
+  veteranEmail: "test@email.com",
+  veteranFullName: {
+    first: "Jane Doe",
     last: "Doe"
   },
-  secondaryTwoSsn: "585784787",
-  secondaryTwoDateOfBirth: "1994-08-04",
-  secondaryTwoGender: "Male",
-  secondaryTwoAddress: {
-    street: "111 2nd St S",
-    city: "Seattle",
-    country: "USA",
-    state: "Florida",
-    postalCode: "33771"
-  },
-  secondaryTwoPrimaryPhoneNumber: "8887775544",
-  secondaryTwoAlternativePhoneNumber: "8887775544",
-  secondaryTwoEmail: "test@email.com",
-  secondaryTwoVetRelationship: "asdasdas",
+  veteranSsnOrTin: "Tax Identification Number",
+  veteranTin: "789",
+  veteranDateOfBirth: "1990-07-03",
+  veteranGender: "Male",
+  // Primary Caregiver Info
   primaryAddress: {
     street: "111 2nd St S",
     city: "Seattle",
@@ -63,25 +48,44 @@ const schema = {
   primaryTin: "789",
   primaryDateOfBirth: "1978-07-03",
   primaryGender: "Female",
-  veteranVaEnrolled: false,
-  veteranFacilityType: "hospital",
-  veteranPreviousTreatmentFacility: "adsadad",
-  veteranAddress: {
+  // Secondary Caregiver One
+  secondaryOneFullName: {
+    first: "John Doe",
+    last: "Doe",
+    suffix: "II"
+  },
+  secondaryOneSsn: "787787854",
+  secondaryOneDateOfBirth: "1989-11-06",
+  secondaryOneGender: "Male",
+  secondaryOneAddress: {
     street: "111 2nd St S",
     city: "Seattle",
     country: "USA",
-    state: "IN",
+    state: "IL",
     postalCode: "33771"
   },
-  veteranTelephoneNumber: "8887775544",
-  veteranCellNumber: "8887775544",
-  veteranEmail: "test@email.com",
-  veteranFullName: {
-    first: "Jane Doe",
+  secondaryOnePrimaryPhoneNumber: "8887775544",
+  secondaryOneAlternativePhoneNumber: "8887775544",
+  secondaryOneEmail: "test@email.com",
+  secondaryOneVetRelationship: "asdasdas",
+  // Secondary Caregiver Two
+  hasSecondaryTwoCaregiver: true,
+  secondaryTwoFullName: {
+    first: "John Doe",
     last: "Doe"
   },
-  veteranSsnOrTin: "Tax Identification Number",
-  veteranTin: "789",
-  veteranDateOfBirth: "1990-07-03",
-  veteranGender: "Male"
+  secondaryTwoSsn: "585784787",
+  secondaryTwoDateOfBirth: "1994-08-04",
+  secondaryTwoGender: "Male",
+  secondaryTwoAddress: {
+    street: "111 2nd St S",
+    city: "Seattle",
+    country: "USA",
+    state: "Florida",
+    postalCode: "33771"
+  },
+  secondaryTwoPrimaryPhoneNumber: "8887775544",
+  secondaryTwoAlternativePhoneNumber: "8887775544",
+  secondaryTwoEmail: "test@email.com",
+  secondaryTwoVetRelationship: "asdasdas",
 };
