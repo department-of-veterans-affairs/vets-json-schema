813dd6a0ada57bb0f22479b1d2c162830b8599b7
camerontesterman <cameronbtesterman@gmail.com>
2020-02-04 11:47:28 -0600

    added MDOT schema

---
+++ src/schemas/MDOT/schema.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import schemaHelpers from '../../common/schema-helpers';
+import originalDefinitions from '../../common/definitions';
+import constants from '../../common/constants';
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'MEDICAL DEVICES ORDERING TOOL',
+  type: 'object',
+  additionalProperties: false,
+  definitions: {},
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    dateOfBirth: {
+      type: 'string',
+      format: 'date'
+    }
+  },
+  required: ['privacyAgreementAccepted'],
+};
+
+[
+  ['privacyAgreementAccepted'],
+  ['fullName', 'veteranFullName'],
+  ['address', 'veteranAddress'],
+  ['gender'],
+].forEach(args => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
