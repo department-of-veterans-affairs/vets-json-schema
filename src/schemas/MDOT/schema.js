e4d4a2021693039c7db7c1b5aee606ed417a75ef
mr0sari0 <mahariel.rosario@gmail.com>
2020-02-27 17:51:03 -0500

    Change veteranAddress to address and veteranFullName to fullName

--- src/schemas/MDOT/schema.js
+++ src/schemas/MDOT/schema.js
@@ -1,3 +1,7 @@
+// import _ from 'lodash';
+// import schemaHelpers from '../../common/schema-helpers';
+// import originalDefinitions from '../../common/definitions';
+// import constants from '../../common/constants';
 
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
@@ -5,341 +9,336 @@ const schema = {
   type: 'object',
   additionalProperties: false,
   definitions: {
-    "privacyAgreementAccepted": {
-      "type": "boolean",
-      "enum": [true]
+    privacyAgreementAccepted: {
+      type: 'boolean',
+      enum: [true],
     },
-    "veteranFullName": {
-      "type": "object",
-      "properties": {
-        "first": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 30
+    fullName: {
+      type: 'object',
+      properties: {
+        first: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
         },
-        "middle": {
-          "type": "string"
+        middle: {
+          type: 'string',
         },
-        "last": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 30
+        last: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
         },
-        "suffix": {
-          "type": "string",
-          "enum": ["Jr.", "Sr.", "II", "III", "IV"]
-        }
+        suffix: {
+          type: 'string',
+          enum: ['Jr.', 'Sr.', 'II', 'III', 'IV'],
+        },
       },
-      "required": ["first", "last"]
+      required: ['first', 'last'],
     },
-    "veteranAddress": {
-      "type": "object",
-      "oneOf": [
+    address: {
+      type: 'object',
+      oneOf: [
         {
-          "properties": {
-            "country": {
-              "type": "string",
-              "enum": ["CAN"]
+          properties: {
+            country: {
+              type: 'string',
+              enum: ['CAN'],
             },
-            "state": {
-              "type": "string",
-              "enum": [
-                "AB",
-                "BC",
-                "MB",
-                "NB",
-                "NF",
-                "NT",
-                "NV",
-                "NU",
-                "ON",
-                "PE",
-                "QC",
-                "SK",
-                "YT"
-              ]
+            state: {
+              type: 'string',
+              enum: ['AB', 'BC', 'MB', 'NB', 'NF', 'NT', 'NV', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'],
             },
-            "postalCode": {
-              "type": "string",
-              "maxLength": 10
-            }
-          }
+            postalCode: {
+              type: 'string',
+              maxLength: 10,
+            },
+          },
         },
         {
-          "properties": {
-            "country": {
-              "type": "string",
-              "enum": ["MEX"]
+          properties: {
+            country: {
+              type: 'string',
+              enum: ['MEX'],
             },
-            "state": {
-              "type": "string",
-              "enum": [
-                "aguascalientes",
-                "baja-california-norte",
-                "baja-california-sur",
-                "campeche",
-                "chiapas",
-                "chihuahua",
-                "coahuila",
-                "colima",
-                "distrito-federal",
-                "durango",
-                "guanajuato",
-                "guerrero",
-                "hidalgo",
-                "jalisco",
-                "mexico",
-                "michoacan",
-                "morelos",
-                "nayarit",
-                "nuevo-leon",
-                "oaxaca",
-                "puebla",
-                "queretaro",
-                "quintana-roo",
-                "san-luis-potosi",
-                "sinaloa",
-                "sonora",
-                "tabasco",
-                "tamaulipas",
-                "tlaxcala",
-                "veracruz",
-                "yucatan",
-                "zacatecas"
-              ]
+            state: {
+              type: 'string',
+              enum: [
+                'aguascalientes',
+                'baja-california-norte',
+                'baja-california-sur',
+                'campeche',
+                'chiapas',
+                'chihuahua',
+                'coahuila',
+                'colima',
+                'distrito-federal',
+                'durango',
+                'guanajuato',
+                'guerrero',
+                'hidalgo',
+                'jalisco',
+                'mexico',
+                'michoacan',
+                'morelos',
+                'nayarit',
+                'nuevo-leon',
+                'oaxaca',
+                'puebla',
+                'queretaro',
+                'quintana-roo',
+                'san-luis-potosi',
+                'sinaloa',
+                'sonora',
+                'tabasco',
+                'tamaulipas',
+                'tlaxcala',
+                'veracruz',
+                'yucatan',
+                'zacatecas',
+              ],
             },
-            "postalCode": {
-              "type": "string",
-              "maxLength": 10
-            }
-          }
+            postalCode: {
+              type: 'string',
+              maxLength: 10,
+            },
+          },
         },
         {
-          "properties": {
-            "country": {
-              "type": "string",
-              "enum": ["USA"]
+          properties: {
+            country: {
+              type: 'string',
+              enum: ['USA'],
             },
-            "state": {
-              "type": "string",
-              "enum": [
-                "AL",
-                "AK",
-                "AS",
-                "AZ",
-                "AR",
-                "AA",
-                "AE",
-                "AP",
-                "CA",
-                "CO",
-                "CT",
-                "DE",
-                "DC",
-                "FM",
-                "FL",
-                "GA",
-                "GU",
-                "HI",
-                "ID",
-                "IL",
-                "IN",
-                "IA",
-                "KS",
-                "KY",
-                "LA",
-                "ME",
-                "MH",
-                "MD",
-                "MA",
-                "MI",
-                "MN",
-                "MS",
-                "MO",
-                "MT",
-                "NE",
-                "NV",
-                "NH",
-                "NJ",
-                "NM",
-                "NY",
-                "NC",
-                "ND",
-                "MP",
-                "OH",
-                "OK",
-                "OR",
-                "PW",
-                "PA",
-                "PR",
-                "RI",
-                "SC",
-                "SD",
-                "TN",
-                "TX",
-                "UT",
-                "VT",
-                "VI",
-                "VA",
-                "WA",
-                "WV",
-                "WI",
-                "WY"
-              ]
+            state: {
+              type: 'string',
+              enum: [
+                'AL',
+                'AK',
+                'AS',
+                'AZ',
+                'AR',
+                'AA',
+                'AE',
+                'AP',
+                'CA',
+                'CO',
+                'CT',
+                'DE',
+                'DC',
+                'FM',
+                'FL',
+                'GA',
+                'GU',
+                'HI',
+                'ID',
+                'IL',
+                'IN',
+                'IA',
+                'KS',
+                'KY',
+                'LA',
+                'ME',
+                'MH',
+                'MD',
+                'MA',
+                'MI',
+                'MN',
+                'MS',
+                'MO',
+                'MT',
+                'NE',
+                'NV',
+                'NH',
+                'NJ',
+                'NM',
+                'NY',
+                'NC',
+                'ND',
+                'MP',
+                'OH',
+                'OK',
+                'OR',
+                'PW',
+                'PA',
+                'PR',
+                'RI',
+                'SC',
+                'SD',
+                'TN',
+                'TX',
+                'UT',
+                'VT',
+                'VI',
+                'VA',
+                'WA',
+                'WV',
+                'WI',
+                'WY',
+              ],
             },
-            "postalCode": {
-              "type": "string",
-              "maxLength": 10
-            }
-          }
+            postalCode: {
+              type: 'string',
+              maxLength: 10,
+            },
+          },
         },
         {
-          "properties": {
-            "country": {
-              "not": {
-                "type": "string",
-                "enum": ["CAN", "MEX", "USA"]
-              }
+          properties: {
+            country: {
+              not: {
+                type: 'string',
+                enum: ['CAN', 'MEX', 'USA'],
+              },
             },
-            "state": {
-              "type": "string",
-              "maxLength": 51
+            state: {
+              type: 'string',
+              maxLength: 51,
             },
-            "postalCode": {
-              "type": "string",
-              "maxLength": 51
-            }
-          }
-        }
+            postalCode: {
+              type: 'string',
+              maxLength: 51,
+            },
+          },
+        },
       ],
-      "properties": {
-        "street": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 50
+      properties: {
+        street: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 50,
         },
-        "street2": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 50
+        street2: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 50,
         },
-        "city": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 51
+        city: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 51,
         },
-        "country": {
-          "type": "string",
-          "default": "USA"
+        country: {
+          type: 'string',
+          default: 'USA',
         },
-        "state": {
-          "type": "string",
-          "enum": [
-            "AL",
-            "AK",
-            "AS",
-            "AZ",
-            "AR",
-            "AA",
-            "AE",
-            "AP",
-            "CA",
-            "CO",
-            "CT",
-            "DE",
-            "DC",
-            "FM",
-            "FL",
-            "GA",
-            "GU",
-            "HI",
-            "ID",
-            "IL",
-            "IN",
-            "IA",
-            "KS",
-            "KY",
-            "LA",
-            "ME",
-            "MH",
-            "MD",
-            "MA",
-            "MI",
-            "MN",
-            "MS",
-            "MO",
-            "MT",
-            "NE",
-            "NV",
-            "NH",
-            "NJ",
-            "NM",
-            "NY",
-            "NC",
-            "ND",
-            "MP",
-            "OH",
-            "OK",
-            "OR",
-            "PW",
-            "PA",
-            "PR",
-            "RI",
-            "SC",
-            "SD",
-            "TN",
-            "TX",
-            "UT",
-            "VT",
-            "VI",
-            "VA",
-            "WA",
-            "WV",
-            "WI",
-            "WY"
-          ]
+        state: {
+          type: 'string',
+          enum: [
+            'AL',
+            'AK',
+            'AS',
+            'AZ',
+            'AR',
+            'AA',
+            'AE',
+            'AP',
+            'CA',
+            'CO',
+            'CT',
+            'DE',
+            'DC',
+            'FM',
+            'FL',
+            'GA',
+            'GU',
+            'HI',
+            'ID',
+            'IL',
+            'IN',
+            'IA',
+            'KS',
+            'KY',
+            'LA',
+            'ME',
+            'MH',
+            'MD',
+            'MA',
+            'MI',
+            'MN',
+            'MS',
+            'MO',
+            'MT',
+            'NE',
+            'NV',
+            'NH',
+            'NJ',
+            'NM',
+            'NY',
+            'NC',
+            'ND',
+            'MP',
+            'OH',
+            'OK',
+            'OR',
+            'PW',
+            'PA',
+            'PR',
+            'RI',
+            'SC',
+            'SD',
+            'TN',
+            'TX',
+            'UT',
+            'VT',
+            'VI',
+            'VA',
+            'WA',
+            'WV',
+            'WI',
+            'WY',
+          ],
         },
-        "postalCode": {
-          "type": "string",
-          "maxLength": 51
-        }
+        postalCode: {
+          type: 'string',
+          maxLength: 51,
+        },
       },
-      "required": ["street", "city", "state", "country", "postalCode"]
+      required: ['street', 'city', 'state', 'country', 'postalCode'],
     },
-    "gender": {
-      "type": "string",
-      "enum": ["F", "M"]
+    gender: {
+      type: 'string',
+      enum: ['F', 'M'],
     },
-    "dateOfBirth": {
-      "type": "string",
-      "format": "date"
+    dateOfBirth: {
+      type: 'string',
+      format: 'date',
     },
-    "email": {
-      "type": "string",
-      "format": "email"
-    }
+    email: {
+      type: 'string',
+      format: 'email',
+    },
   },
   properties: {
-    "email": {
-      "$ref": "#/definitions/email"
+    email: {
+      $ref: '#/definitions/email',
     },
-    "dateOfBirth": {
-      "$ref": "#/definitions/dateOfBirth"
+    dateOfBirth: {
+      $ref: '#/definitions/dateOfBirth',
     },
-    "privacyAgreementAccepted": {
-      "$ref": "#/definitions/privacyAgreementAccepted"
+    privacyAgreementAccepted: {
+      $ref: '#/definitions/privacyAgreementAccepted',
     },
-    "veteranFullName": {
-      "$ref": "#/definitions/veteranFullName"
+    veteranFullName: {
+      $ref: '#/definitions/fullName',
     },
-    "veteranAddress": {
-      "$ref": "#/definitions/veteranAddress"
+    veteranAddress: {
+      $ref: '#/definitions/address',
     },
-    "gender": {
-      "$ref": "#/definitions/gender"
-    }
+    gender: {
+      $ref: '#/definitions/gender',
+    },
   },
-  required: ['privacyAgreementAccepted', 'veteranFullName', 'veteranAddress', 'gender', 'email', 'dateOfBirth'],
+  required: ['privacyAgreementAccepted', 'fullName', 'address', 'gender', 'email', 'dateOfBirth'],
 };
+
+// [
+//   ['privacyAgreementAccepted'],
+//   ['fullName', 'veteranFullName'],
+//   ['address', 'veteranAddress'],
+//   ['gender'],
+// ].forEach(args => {
+//   schemaHelpers.addDefinitionToSchema(schema, ...args);
+// });
 
 export default schema;
