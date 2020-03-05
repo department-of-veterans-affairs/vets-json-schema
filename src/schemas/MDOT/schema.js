738a38407d3321096355ee4dab00f5a8104cada2
mr0sari0 <mahariel.rosario@gmail.com>
2020-03-05 17:22:25 -0500

    Use USA state labels instead of hard coding them

--- src/schemas/MDOT/schema.js
+++ src/schemas/MDOT/schema.js
@@ -1,196 +1,125 @@
-import constants from '../../common/constants';
+import { countries, states } from '../../common/constants';
 import commonDefinitions from '../../common/definitions';
 import schemaHelpers from '../../common/schema-helpers';
 
 const { fullName, email, gender, date } = commonDefinitions;
 
-const {countries, states} = constants;
-
-const currentCountryLabels = countries.filter(country => country.value === 'USA' || country.value === 'CAN' || country.value === 'MEX').map(country => country.label);
+const currentCountryLabels = countries
+  .filter(country => country.value === 'USA' || country.value === 'CAN' || country.value === 'MEX')
+  .map(country => country.label);
 
 const [USALabel, CANLabel, MEXLabel] = currentCountryLabels;
 
-const {CAN, MEX, USA} = states;
+const { CAN, MEX, USA } = states;
 
 const CANStateLabels = CAN.map(state => state.label);
 const USAStateLabels = USA.map(state => state.label);
 const MEXStateLabels = MEX.map(state => state.label);
 
 const veteranAddress = {
-  type: "object",
+  type: 'object',
   oneOf: [
     {
-      "properties": {
-        "country": {
-          "type": "string",
-          "enum": [CANLabel],
+      properties: {
+        country: {
+          type: 'string',
+          enum: [CANLabel],
         },
-        "state": {
-          "type": "string",
-          "enum": CANStateLabels,
+        state: {
+          type: 'string',
+          enum: CANStateLabels,
         },
-        "postalCode": {
-          "type": "string",
-          "maxLength": 10
-        }
-      }
+        postalCode: {
+          type: 'string',
+          maxLength: 10,
+        },
+      },
     },
     {
-      "properties": {
-        "country": {
-          "type": "string",
-          "enum": [MEXLabel],
+      properties: {
+        country: {
+          type: 'string',
+          enum: [MEXLabel],
         },
-        "state": {
-          "type": "string",
-          "enum": MEXStateLabels,
+        state: {
+          type: 'string',
+          enum: MEXStateLabels,
         },
-        "postalCode": {
-          "type": "string",
-          "maxLength": 10
-        }
-      }
+        postalCode: {
+          type: 'string',
+          maxLength: 10,
+        },
+      },
     },
     {
-      "properties": {
-        "country": {
-          "type": "string",
-          "enum": [USALabel]
+      properties: {
+        country: {
+          type: 'string',
+          enum: [USALabel],
         },
-        "state": {
-          "type": "string",
-          "enum": USAStateLabels,
+        state: {
+          type: 'string',
+          enum: USAStateLabels,
         },
-        "postalCode": {
-          "type": "string",
-          "maxLength": 10
-        }
-      }
+        postalCode: {
+          type: 'string',
+          maxLength: 10,
+        },
+      },
     },
     {
-      "properties": {
-        "country": {
-          "not": {
-            "type": "string",
-            "enum": currentCountryLabels
-          }
+      properties: {
+        country: {
+          not: {
+            type: 'string',
+            enum: currentCountryLabels,
+          },
         },
-        "state": {
-          "type": "string",
-          "maxLength": 51,
+        state: {
+          type: 'string',
+          maxLength: 51,
         },
-        "postalCode": {
-          "type": "string",
-          "maxLength": 51
-        }
-      }
-    }
+        postalCode: {
+          type: 'string',
+          maxLength: 51,
+        },
+      },
+    },
   ],
   properties: {
     street: {
-      type: "string",
+      type: 'string',
       minLength: 1,
-      maxLength: 50
+      maxLength: 50,
     },
     street2: {
-      type: "string",
+      type: 'string',
       minLength: 1,
-      maxLength: 50
+      maxLength: 50,
     },
     city: {
-      type: "string",
+      type: 'string',
       minLength: 1,
-      maxLength: 51
+      maxLength: 51,
     },
     state: {
-      title: "State",
-      type: "string",
+      title: 'State',
+      type: 'string',
       maxLength: 51,
-      enum: [
-        "Alabama",
-        "Alaska",
-        "American Samoa",
-        "Arizona",
-        "Arkansas",
-        "Armed Forces Americas (AA)",
-        "Armed Forces Europe (AE)",
-        "Armed Forces Pacific (AP)",
-        "California",
-        "Colorado",
-        "Connecticut",
-        "Delaware",
-        "District Of Columbia",
-        "Federated States Of Micronesia",
-        "Florida",
-        "Georgia",
-        "Guam",
-        "Hawaii",
-        "Idaho",
-        "Illinois",
-        "Indiana",
-        "Iowa",
-        "Kansas",
-        "Kentucky",
-        "Louisiana",
-        "Maine",
-        "Marshall Islands",
-        "Maryland",
-        "Massachusetts",
-        "Michigan",
-        "Minnesota",
-        "Mississippi",
-        "Missouri",
-        "Montana",
-        "Nebraska",
-        "Nevada",
-        "New Hampshire",
-        "New Jersey",
-        "New Mexico",
-        "New York",
-        "North Carolina",
-        "North Dakota",
-        "Northern Mariana Islands",
-        "Ohio",
-        "Oklahoma",
-        "Oregon",
-        "Palau",
-        "Pennsylvania",
-        "Puerto Rico",
-        "Rhode Island",
-        "South Carolina",
-        "South Dakota",
-        "Tennessee",
-        "Texas",
-        "Utah",
-        "Vermont",
-        "Virgin Islands",
-        "Virginia",
-        "Washington",
-        "West Virginia",
-        "Wisconsin",
-        "Wyoming"
-      ]
+      enum: USAStateLabels,
     },
     country: {
-      default: "United States",
-      type: "string",
-      enum: [
-        "United States"
-      ]
+      default: USALabel,
+      type: 'string',
+      enum: [USALabel],
     },
     postalCode: {
-      type: "string",
-      maxLength: 10
-    }
+      type: 'string',
+      maxLength: 10,
+    },
   },
-  required: [
-    "street",
-    "city",
-    "state",
-    "country",
-    "postalCode"
-  ]
-}
+  required: ['street', 'city', 'state', 'country', 'postalCode'],
+};
 
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
@@ -202,16 +131,21 @@ const schema = {
     email,
     gender,
     date,
-    veteranAddress
+    veteranAddress,
   },
   properties: {},
   required: ['privacyAgreementAccepted', 'veteranFullName', 'veteranAddress', 'gender', 'email', 'dateOfBirth'],
 };
 
-[['privacyAgreementAccepted'], ['email'], ['fullName', 'veteranFullName'], ['veteranAddress'], ['gender'], ['date', 'dateOfBirth']].forEach(
-  args => {
-    schemaHelpers.addDefinitionToSchema(schema, ...args);
-  },
-);
+[
+  ['privacyAgreementAccepted'],
+  ['email'],
+  ['fullName', 'veteranFullName'],
+  ['veteranAddress'],
+  ['gender'],
+  ['date', 'dateOfBirth'],
+].forEach(args => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
 
 export default schema;
