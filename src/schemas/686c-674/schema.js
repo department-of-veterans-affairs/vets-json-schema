cf7ba96a6f380119373235502637076d6d0414da
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-04-13 12:46:03 -0700

    Added schema for wizard

--- src/schemas/686c-674/schema.js
+++ src/schemas/686c-674/schema.js
@@ -9,104 +9,144 @@ import {
   textOnlyPattern,
 } from '../../common/constants.js';
 
-export const genericSchemas = {
-  genericLocation: {
-    type: 'object',
-    required: ['city', 'state'],
-    properties: {
-      state: {
-        type: 'string',
-        maxLength: 30,
-        pattern: textOnlyPattern,
-      },
-      city: {
-        type: 'string',
-        maxLength: 30,
-        pattern: textOnlyPattern,
-      },
-    },
-  },
-  genericTextInput: {
-    type: 'string',
-    maxLength: 50,
-  },
-  genericTrueFalse: {
-    type: 'boolean',
-  },
-  genericNumberAndDashInput: {
-    type: 'string',
-    maxLength: 50,
-    pattern: numberAndDashPattern,
-  },
-  genericUSAStateDropdown: {
-    type: 'string',
-    enum: states50AndDC.map(state => state.value),
-    default: states50AndDC.map(state => state.label),
-  },
-  countryDropdown: {
-    type: 'string',
-    enum: countries.map(country => country.label),
-  },
-  fullName: {
-    type: 'object',
-    properties: {
-      first: {
-        type: 'string',
-        minLength: 1,
-        maxLength: 30,
-        pattern: textOnlyPattern,
-      },
-      middle: {
-        type: 'string',
-        maxLength: 20,
-        pattern: textOnlyPattern,
-      },
-      last: {
-        type: 'string',
-        minLength: 1,
-        maxLength: 30,
-        pattern: textOnlyPattern,
-      },
-      suffix: {
-        type: 'string',
-        enum: suffixes,
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'SUPPLEMENTAL CLAIM FOR COMPENSATION (21-526EZ)',
+  type: 'object',
+  definitions: {
+    genericLocation: {
+      type: 'object',
+      required: ['city', 'state'],
+      properties: {
+        state: {
+          type: 'string',
+          maxLength: 30,
+          pattern: textOnlyPattern,
+        },
+        city: {
+          type: 'string',
+          maxLength: 30,
+          pattern: textOnlyPattern,
+        },
       },
     },
-    required: ['first', 'last'],
-  },
-  date: {
-    type: 'string',
-    pattern: datePattern,
-  },
-  emailInput: {
-    type: 'string',
-    format: 'email',
-  },
-  phoneInput: {
-    type: 'string',
-    pattern: phonePattern,
-  },
-  currencyInput: {
-    type: 'string',
-    pattern: currencyAmountPattern,
-  },
-  fileSchema: {
-    type: 'array',
-    items: {
+    genericTextInput: {
+      type: 'string',
+      maxLength: 50,
+    },
+    genericTrueFalse: {
+      type: 'boolean',
+    },
+    genericNumberAndDashInput: {
+      type: 'string',
+      maxLength: 50,
+      pattern: numberAndDashPattern,
+    },
+    genericUSAStateDropdown: {
+      type: 'string',
+      enum: states50AndDC.map(state => state.value),
+      default: states50AndDC.map(state => state.label),
+    },
+    countryDropdown: {
+      type: 'string',
+      enum: countries.map(country => country.label),
+    },
+    fullName: {
       type: 'object',
       properties: {
-        name: { type: 'string' },
-        size: { type: 'integer' },
-        confirmationCode: { type: 'string' },
+        first: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
+          pattern: textOnlyPattern,
+        },
+        middle: {
+          type: 'string',
+          maxLength: 20,
+          pattern: textOnlyPattern,
+        },
+        last: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 30,
+          pattern: textOnlyPattern,
+        },
+        suffix: {
+          type: 'string',
+          enum: suffixes,
+        },
+      },
+      required: ['first', 'last'],
+    },
+    date: {
+      type: 'string',
+      pattern: datePattern,
+    },
+    emailInput: {
+      type: 'string',
+      format: 'email',
+    },
+    phoneInput: {
+      type: 'string',
+      pattern: phonePattern,
+    },
+    currencyInput: {
+      type: 'string',
+      pattern: currencyAmountPattern,
+    },
+    fileSchema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          size: { type: 'integer' },
+          confirmationCode: { type: 'string' },
+        },
       },
     },
   },
-};
-
-const schema = {
-  type: 'object',
   properties: {
+    optionSelection: {
+      type: 'object',
+        'view:selectable686Options': {
+          type: 'object',
+          properties: {
+            addChild: { type: 'boolean', default: false },
+            addSpouse: { type: 'boolean', default: false },
+            reportDivorce: { type: 'boolean', default: false },
+            reportDeath: { type: 'boolean', default: false },
+            reportStepchildNotInHousehold: { type: 'boolean', default: false },
+            reportMarriageOfChildUnder18: { type: 'boolean', default: false },
+            reportChild18orOlderIsNotAttendingSchool: {
+              type: 'boolean',
+              default: false,
+            },
+            report674: {
+              type: 'boolean',
+              default: false,
+            },
+          },
+        },
+    },
 
+    veteranInformation: {},
+
+    addChild: {},
+
+    addSpouse: {},
+
+    reportDivorce: {},
+
+    deceasedDependents: {},
+
+    reportChildMarriage: {},
+
+    reportChildStoppedAttendingSchool: {},
+
+    reportStepchildNotInHousehold: {},
+
+    report674: {},
   },
 }
 
