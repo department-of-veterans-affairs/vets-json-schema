053ccd1ceea44e26c0743cb0e1a67a6622a33732
Brad Conley <bradl3yC@users.noreply.github.com>
2020-04-17 12:53:46 -0400

    bradl3yC - 7099 - Update schema for form 40-10007 (#405)
    * Update schema for form 40-10007
    
    * correct enums

--- src/schemas/40-10007/schema.js
+++ src/schemas/40-10007/schema.js
@@ -210,6 +210,7 @@ const schema = {
           type: 'object',
           required: [
             'currentName',
+            'ethnicity',
             'gender',
             'isDeceased',
             'maritalStatus',
@@ -233,6 +234,11 @@ const schema = {
             gender: {
               type: 'string',
               enum: ['Female', 'Male'],
+            },
+            ethnicity: {
+              type: 'string',
+              enumNames: ['American Indian or Alaska Native', 'Black or African American', 'Hispanic or Latino', 'Native Hawaiian or Pacific Islander', 'White'],
+              enum: ['americanIndian', 'blackOrAfrican', 'hispanicOrLatino', 'nativeHawaiianOrIslander', 'white'],
             },
             isDeceased: {
               type: 'string',
