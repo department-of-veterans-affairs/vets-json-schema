3986aa4498d1f1f78eb739ed82020ba97e13912b
Brad Conley <brad.conl3y@gmail.com>
2020-04-20 09:44:03 -0400

    Revert 404 changes

---
+++ src/schemas/VIC/schema.js
@@ -0,0 +1,109 @@
+import _ from 'lodash';
+import originalDefinitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
+import constants from '../../common/constants';
+
+const address = (() => {
+  const countries = constants.salesforceCountries.map(object => object.value);
+  const countriesWithStates = Object.keys(constants.salesforceStates).filter(x => _.includes(countries, x));
+  const countriesWithNoState = _.difference(countries, countriesWithStates);
+  const countryStateProperties = _.map(constants.salesforceStates, (value, key) => ({
+    properties: {
+      country: {
+        type: 'string',
+        enum: [key],
+      },
+      state: {
+        type: 'string',
+        enum: value.map(x => x.value),
+      },
+      postalCode: {
+        type: 'string',
+        maxLength: 10,
+      },
+    },
+  }));
+  countryStateProperties.push({
+    properties: {
+      country: {
+        type: 'string',
+        enum: countriesWithNoState,
+      },
+      postalCode: {
+        type: 'string',
+        maxLength: 51,
+      },
+    },
+  });
+
+  return {
+    type: 'object',
+    oneOf: countryStateProperties,
+    properties: {
+      street: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 50,
+      },
+      street2: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 50,
+      },
+      city: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 51,
+      },
+    },
+  };
+})();
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'VETERAN IDENTIFICATION CARD FORM',
+  type: 'object',
+  additionalProperties: false,
+  definitions: {
+    address,
+  },
+  properties: {
+    serviceBranch: {
+      type: 'string',
+      enum: [
+        'F', // Air Force
+        'A', // Army
+        'C', // Coast Guard
+        'M', // Marine Corps
+        'N', // Navy
+      ],
+    },
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    photo: originalDefinitions.files.items,
+    dd214: { ...originalDefinitions.files, minItems: 1 },
+    veteranDateOfBirth: {
+      type: 'string',
+      format: 'date',
+    },
+    processAsAnonymous: {
+      type: 'boolean',
+    },
+  },
+  required: ['privacyAgreementAccepted'],
+};
+
+[
+  ['privacyAgreementAccepted'],
+  ['fullName', 'veteranFullName'],
+  ['address', 'veteranAddress'],
+  ['ssn', 'veteranSocialSecurityNumber'],
+  ['phone'],
+  ['gender'],
+].forEach(args => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
+export default schema;
