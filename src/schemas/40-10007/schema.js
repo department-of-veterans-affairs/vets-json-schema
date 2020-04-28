0a1dbc9ba6cf0177a60ca216f74b3eaed6241271
Brad Conley <bradl3yC@users.noreply.github.com>
2020-04-28 08:26:12 -0400

    Remove ethnicity from schema (#423)

--- src/schemas/40-10007/schema.js
+++ src/schemas/40-10007/schema.js
@@ -210,7 +210,6 @@ const schema = {
           type: 'object',
           required: [
             'currentName',
-            'ethnicity',
             'gender',
             'isDeceased',
             'maritalStatus',
@@ -234,10 +233,6 @@ const schema = {
             gender: {
               type: 'string',
               enum: ['Female', 'Male'],
-            },
-            ethnicity: {
-              type: 'string',
-              enum: ['americanIndian', 'blackOrAfrican', 'hispanicOrLatino', 'nativeHawaiianOrIslander', 'white'],
             },
             isDeceased: {
               type: 'string',
