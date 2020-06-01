8a1e91213e28610d0f682a67797e65fe862f3b65
Rob Garrison <robin.garrison@adhocteam.us>
2020-06-01 12:06:23 -0500

    [Development] Update HLR schema (#429)
    * Move benefit types to constants
    
    * Update HLR schema

--- src/schemas/20-0996/schema.js
+++ src/schemas/20-0996/schema.js
@@ -1,194 +1,104 @@
-import definitions from '../../common/definitions';
-import { pciuCountries, pciuStates } from '../../common/constants';
+import { benefitTypes } from '../../common/constants';
 
-// Only compensation is available for the HLR MVP. These definitions match
-// caseflow:
-// https://github.com/department-of-veterans-affairs/caseflow/blob/master/client/constants/BENEFIT_TYPES.json
-const benefitTypes = [
-  { label: 'Compensation', value: 'compensation' },
-  // { label: 'Pension/survivors benefits', value: 'pension' },
-  // { label: 'Fiduciary', value: 'fiduciary' },
-  // { label: 'Education', value: 'education' },
-  // { label: 'Veterans Health Administration' , value: 'vha' },
-  // { label: 'Vocational rehabilitation and employment', value: 'voc_rehab'},
-  // { label: 'Loan guaranty', value: 'loan_guaranty'},
-  // { label: 'Insurance', value: 'insurance' },
-  // { label: 'National Cemetary Administration', value: 'nca' },
-];
+// Only compensation is available for the HLR MVP
+const supportedBenefits = ['compensation'];
+const supportedBenefitTypes = benefitTypes.filter(benefit => supportedBenefits.includes(benefit.value));
 
-const baseAddressDef = {
-  addressLine1: {
-    $ref: '#/definitions/addressLine1',
-  },
-  addressLine2: {
-    $ref: '#/definitions/addressLine2',
-  },
-  city: {
-    $ref: '#/definitions/city',
-  },
-  stateOrProvinceCode: {
-    $ref: '#/definitions/state',
-  },
-  zipPostalCode: {
-    $ref: '#/definitions/zipCode',
-  },
-  countryCode: {
-    $ref: '#/definitions/country',
+const phoneDef = {
+  type: 'object',
+  properties: {
+    areaCode: {
+      type: 'string',
+      pattern: '^[2-9][0-9]{2}$',
+    },
+    phoneNumber: {
+      type: 'string',
+      pattern: '^[0-9]{1,14}$',
+    },
+    phoneNumberCountryCode: {
+      type: 'string',
+      pattern: '^[0-9]+$',
+    },
+    phoneNumberExt: {
+      type: 'string',
+      pattern: '^[a-zA-Z0-9]{1,10}$',
+    },
   },
 };
 
-const basePhoneDef = {
-  phoneNumber: {
-    $ref: '#/definitions/phone',
-  },
-  phoneNumberCountryCode: {
-    $ref: '#/definitions/phoneCountryCode',
-  },
-  phoneNumberExt: {
-    $ref: '#/definitions/phoneExt',
-  },
-};
-
+// Note: patterns copied from lighthouse schema
+// https://github.com/department-of-veterans-affairs/vets-api/blob/master/modules/appeals_api/config/schemas/200996.json
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'HIGHER-LEVEL REVIEW',
   type: 'object',
-  definitions: {
-    formType: {
-      type: 'string',
-      enum: ['HigherLevelReview'],
-    },
-    benefitType: {
-      type: 'string',
-      enum: benefitTypes.map(b => b.value),
-      enumNames: benefitTypes.map(b => b.label),
-    },
-    addressLine1: {
-      type: 'string',
-      maxLength: 50,
-    },
-    addressLine2: {
-      type: 'string',
-      maxLength: 20,
-    },
-    city: {
-      type: 'string',
-      minLength: 1,
-      maxLength: 51,
-    },
-    state: {
-      type: 'string',
-      enum: pciuStates.map(state => state.value),
-      enumNames: pciuStates.map(state => state.label),
-    },
-    zipCode: definitions.usaPostalCode,
-    country: {
-      type: 'string',
-      enum: pciuCountries,
-      default: 'USA',
-    },
-    phone: definitions.usaPhone,
-    phoneCountryCode: {
-      type: 'string',
-      maxLength: 10,
-    },
-    phoneExt: {
-      type: 'string',
-      maxLength: 10,
-    },
-    email: {
-      type: 'string',
-      format: 'email',
-    },
-    date: definitions.date,
-    // Used for forwarding address
-    // dateRange: definitions.dateRange,
-  },
   properties: {
     data: {
       type: 'object',
       properties: {
         type: {
-          $ref: '#/definitions/formType',
+          type: 'string',
+          enum: ['HigherLevelReview'],
+          enumNames: ['HLR'],
         },
         attributes: {
           type: 'object',
-          required: [
-            'receiptDate',
-            'benefitType',
-            'legacyOptInApproved',
-            'sameOffice',
-            'veteran',
-            'informalConference',
-          ],
-          properties: {
-            receiptDate: {
-              $ref: '#/definitions/date',
-            },
-            benefitType: {
-              $ref: '#/definitions/benefitType',
-            },
-            legacyOptInApproved: {
-              type: 'boolean',
-            },
-            sameOffice: {
-              type: 'boolean',
-            },
-            veteran: {
-              type: 'object',
-              required: ['countryCode', 'city', 'addressLine1'],
-              properties: {
-                // fileNumberOrSsn: { type: 'string' },
-                ...baseAddressDef,
-                ...basePhoneDef,
-                emailAddress: {
-                  $ref: '#/definitions/email',
-                },
-                /* Not yet supported by CaseFlow
-                forwardingAddress: {
-                  effectiveDates: {
-                    $ref: '#/definitions/dateRange',
+          required: ['benefitType', 'veteran', 'sameOffice', 'informalConference'],
+          benefitType: {
+            type: 'string',
+            enum: supportedBenefitTypes.map(b => b.value),
+            enumNames: supportedBenefitTypes.map(b => b.label),
+          },
+          veteran: {
+            type: 'object',
+            required: ['address'],
+            properties: {
+              address: {
+                type: 'object',
+                required: ['zipCode5'],
+                properties: {
+                  zipCode5: {
+                    type: 'string',
+                    pattern: '^[0-9]{5}$',
                   },
-                  ...baseAddressDef,
-                },
-                */
-              },
-            },
-            /* Not included in MVP *
-            claimant: {
-              type: 'object',
-              properties: {
-                participantId: {
-                  type: 'string',
-                },
-                payeeCode: {
-                  type: 'string',
                 },
               },
-            },
-            */
-
-            informalConference: {
-              type: 'boolean',
-            },
-            informalConferenceRep: {
-              type: 'object',
-              properties: {
-                name: {
-                  type: 'string',
-                },
-                ...basePhoneDef,
-              },
-            },
-            informalConferenceTimes: {
-              type: 'array',
-              minItems: 1,
-              maxItems: 2,
-              uniqueItems: true,
-              items: {
+              phone: phoneDef,
+              emailAddressText: {
                 type: 'string',
-                enum: ['800-1000', '1000-1230', '1230-200', '200-430'],
+                format: 'email',
               },
+              // Lighthouse is including enum time zone values here, but they are all
+              // built into the browser - see the "TZ database name" column here:
+              // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+              timeZone: {
+                type: 'string',
+              },
+            },
+          },
+          sameOffice: {
+            type: 'boolean',
+          },
+          informalConference: {
+            type: 'boolean',
+          },
+          informalConferenceRep: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+              phone: phoneDef,
+            },
+          },
+          informalConferenceTimes: {
+            type: 'array',
+            minItems: 1,
+            maxItems: 2,
+            uniqueItems: true,
+            items: {
+              type: 'string',
+              enum: ['800-1000 ET', '1000-1230 ET', '1230-1400 ET', '1400-1630 ET'],
             },
           },
         },
@@ -208,10 +118,15 @@ const schema = {
           },
           attributes: {
             type: 'object',
+            required: ['issue', 'decisionDate'],
             properties: {
-              notes: {
+              issue: {
                 type: 'string',
-                maxLength: 400,
+                maxLength: 140,
+              },
+              decisionDate: {
+                type: 'string',
+                pattern: '^[0-9]{4}-[0-9]{2}-[0-9]{2}$',
               },
               decisionIssueId: {
                 type: 'number',
