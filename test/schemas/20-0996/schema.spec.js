9073478447bddd496c90306874ac93a3c26b65e6
Robin Garrison <robin.garrison@adhocteam.us>
2020-06-01 10:00:48 -0500

    Update HLR schema

--- test/schemas/20-0996/schema.spec.js
+++ test/schemas/20-0996/schema.spec.js
@@ -1,169 +1,91 @@
-/* global it:false */
-/* eslint-disable no-unused-expressions */
-import ajv from 'ajv';
-import { expect } from 'chai';
-import _ from 'lodash';
+import { cloneDeep } from 'lodash';
 
-import schemas from '../../../dist/schemas';
+import schema from '../../../dist/20-0996-schema.json';
 import SharedTests from '../../support/shared-tests';
 import SchemaTestHelper from '../../support/schema-test-helper';
 
-const schema = _.cloneDeep(schemas['20-0996']);
-// Flatten schema
+const schemaCopy = cloneDeep(schema);
+
+delete schemaCopy.properties.data.properties.attributes.required;
+delete schemaCopy.properties.data.properties.attributes.veteran.required;
+delete schemaCopy.properties.data.properties.attributes.veteran.properties.address.required;
+delete schemaCopy.properties.data.properties.attributes.informalConferenceRep.minItems;
+delete schemaCopy.properties.data.properties.attributes.informalConferenceTimes.minItems;
+delete schemaCopy.properties.included.items.requred;
+delete schemaCopy.properties.included.items.properties.attributes.required;
+delete schemaCopy.properties.included.minItems;
+
 const flatSchema = {
-  ...schema,
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'HIGHER-LEVEL REVIEW',
   properties: {
-    type: schema.properties.data.properties.type,
-    included: schema.properties.included,
-    ...schema.properties.data.properties.attributes.properties,
+    ...schemaCopy.properties.data.properties.attributes,
+    included: schemaCopy.properties.included,
   },
 };
-delete flatSchema.properties.veteran.required;
-delete flatSchema.properties.included.required;
+
+delete flatSchema.properties.type;
+
 const schemaTestHelper = new SchemaTestHelper(flatSchema);
 const sharedTests = new SharedTests(schemaTestHelper);
 
-const jsonValidator = ajv({
-  allErrors: true,
-  errorDataPath: 'property',
-  removeAdditional: true,
-  useDefaults: true,
-});
-
-function definitionValidator(field) {
-  const properties = {
-    field: schema.definitions[field],
-  };
-  const tinySchema = {
-    $schema: 'http://json-schema.org/draft-04/schema#',
-    type: 'object',
-    properties,
-  };
-
-  return function validator(value) {
-    const fn = jsonValidator.compile(tinySchema);
-    return fn({
-      field: value,
-    });
-  };
-}
-
-function stringGenerate(length, char = 'a') {
-  return new Array(length + 1).join(char);
-}
+const phoneData = {
+  valid: [
+    { phoneNumberCountryCode: '999999' },
+    { areaCode: '999' },
+    { phoneNumber: '1234' },
+    { phoneNumber: '5551212' },
+    { phoneNumberExt: 'abc1234567' },
+    { phoneNumberExt: 'abcdefghij' },
+    { phonenumberCountryCode: '1', areaCode: '800', phoneNumber: '5551212', phoneNumberExt: 'x123' },
+  ],
+  invalid: [
+    { phoneNumberCountryCode: 'abc' },
+    { phoneNumberCountryCode: 123 },
+    { areaCode: '1' },
+    { areaCode: '123' },
+    { areaCode: '9876' },
+    { areaCode: 456 },
+    { phoneNumber: '' },
+    { phoneNumber: '123456789012345' },
+    { phoneNumberExt: '12345678901' },
+  ],
+};
 
 describe('20-0996 (Higher-Level Review) schema', () => {
-  // Definitions:
-  [
-    {
-      name: 'formType',
-      valid: ['HigherLevelReview'],
-      invalid: [null, '', 'something-else'],
-    },
-    {
-      name: 'benefitType',
-      valid: ['compensation'],
-      invalid: ['something-else'],
-    },
-    {
-      name: 'addressLine1',
-      valid: ['foo'],
-      invalid: [stringGenerate(51)],
-    },
-    {
-      name: 'addressLine2',
-      valid: ['bar'],
-      invalid: [stringGenerate(21)],
-    },
-    {
-      name: 'city',
-      valid: ['foo city'],
-      invalid: ['', stringGenerate(52)],
-    },
-    {
-      name: 'state',
-      valid: ['AL'],
-      invalid: ['ZZZZ'],
-    },
-    {
-      name: 'zipCode',
-      valid: ['12345', '12345-9999'],
-      invalid: ['', '123', 12345, 'abcde', '12345-1', '12345-12345'],
-    },
-    {
-      name: 'country',
-      valid: ['Bahamas'],
-      invalid: ['Candyland'],
-    },
-    {
-      name: 'phone',
-      valid: ['0123456789'],
-      invalid: ['', 'abcdefghij', '1-456-7890', 1234567890],
-    },
-    {
-      name: 'phoneCountryCode',
-      valid: ['1'],
-      invalid: [stringGenerate(11), stringGenerate(11, '1')],
-    },
-    {
-      name: 'phoneExt',
-      valid: ['x1234'],
-      invalid: [stringGenerate(11), stringGenerate(11, '1')],
-    },
-    {
-      name: 'email',
-      valid: ['email@emal.com'],
-      invalid: ['', 'email', 'email@', '@email.com'],
-    },
-    {
-      name: 'date',
-      valid: ['2020-01-01'],
-      invalid: ['', '01-01-2020', '2020'],
-    },
-  ].forEach(definition => {
-    describe(definition.name, () => {
-      const validator = definition.extra
-        ? definitionValidator(definition.name, definition.extra)
-        : definitionValidator(definition.name);
-      definition.valid.forEach(value => {
-        it(`validates "${value}" as correct`, () => {
-          expect(validator(value)).to.be.true;
-        });
-      });
-
-      definition.invalid.forEach(value => {
-        it(`validates "${value}" as incorrect`, () => {
-          expect(validator(value)).to.be.false;
-        });
-      });
-    });
-  });
-
-  // Properties
-  sharedTests.runTest('date', ['receiptDate']);
-  // ** These shared tests are not working properly **
-  // sharedTests.runTest('usaPhone', ['phoneNumber']);
-  // sharedTests.runTest('usaPostalCode', ['zipPostalCode']);
-  // sharedTests.runTest('email', ['emailAddress']);
-
-  schemaTestHelper.testValidAndInvalid('type', {
-    valid: ['HigherLevelReview'],
-    invalid: ['anything else', 1234],
-  });
+  // Not testing this entry since ajv doesn't seem to like an entry with the
+  // key of "type"
+  // schemaTestHelper.testValidAndInvalid('type', {
+  //   valid: ['HigherLevelReview'],
+  //   invalid: [null, 'anything else', 1234],
+  // });
 
   schemaTestHelper.testValidAndInvalid('benefitType', {
     valid: ['compensation'],
-    invalid: ['anything else', 1234],
+    invalid: [null, 'anything else', 1234],
   });
 
-  schemaTestHelper.testValidAndInvalid('legacyOptInApproved', {
-    valid: [true, false],
-    invalid: ['foo', 1234, {}],
+  schemaTestHelper.testValidAndInvalid('veteran.address.zipCode5', {
+    valid: ['12345', '98765'],
+    invalid: [null, '123', 1234, {}],
+  });
+
+  schemaTestHelper.testValidAndInvalid('veteran.phone', phoneData);
+
+  sharedTests.runTest('email', ['veteran.emailAddressText']);
+
+  schemaTestHelper.testValidAndInvalid('veteran.timeZone', {
+    valid: ['EST', 'Etc/GMT-10', 'Pacific/Samoa'],
+    // The schema isn't including full enum list of all time zones, so any
+    // string will pass here; the front-end is using
+    // `Intl.DateTimeFormat().resolvedOptions().timeZone` to pass the time zone
+    // on submission; so the user has no control over it
+    invalid: [null, 1234, {}],
   });
 
   schemaTestHelper.testValidAndInvalid('sameOffice', {
     valid: [true, false],
-    invalid: ['foo', 1234, {}],
+    invalid: [null, 'foo', 1234, {}],
   });
 
   schemaTestHelper.testValidAndInvalid('informalConference', {
@@ -171,73 +93,22 @@ describe('20-0996 (Higher-Level Review) schema', () => {
     invalid: ['foo', 1234, {}],
   });
 
-  schemaTestHelper.testValidAndInvalid('veteran', {
-    valid: [
-      {
-        addressLine1: 'foo',
-        city: 'bar',
-        countryCode: 'USA',
-      },
-      {
-        addressLine1: 'foo',
-        city: 'bar',
-        countryCode: 'USA',
-      },
-      {
-        addressLine1: 'foo',
-        addressLine2: 'foz',
-        city: 'Some City',
-        stateOrProvinceCode: 'GU',
-        countryCode: 'USA',
-        phoneNumber: '1234567890',
-        phoneNumberCountryCode: '1',
-        phoneNumberExt: 'x9876',
-        emailAddress: 'a@bc.com',
-      },
-    ],
-    invalid: [
-      {
-        addressLine1: null,
-        city: 'bar',
-        countryCode: 'USA',
-      },
-      {
-        addressLine1: 'foo',
-        city: '',
-        countryCode: 'USA',
-      },
-      {
-        addressLine1: 'foo',
-        city: 'bar',
-        countryCode: '',
-      },
-    ],
+  schemaTestHelper.testValidAndInvalid('informalConferenceRep', {
+    valid: [{ name: 'foo' }, { name: 'foozus barrius' }, { name: 'foo bar, III' }],
+    invalid: [{ name: null }, { name: 123 }, { name: {} }],
   });
 
-  schemaTestHelper.testValidAndInvalid('informalConferenceRep', {
-    valid: [
-      {
-        name: 'foo',
-        phoneNumber: '0123456789',
-      },
-      {
-        name: 'bar',
-        phoneNumber: '0123456789',
-        phoneNumberCountryCode: '1',
-        phoneNumberExt: 'x123',
-      },
-    ],
-    invalid: [
-      {
-        name: '',
-        phoneNumber: '',
-      },
-    ],
-  });
+  schemaTestHelper.testValidAndInvalid('informalConferenceRep.phone', phoneData);
 
   schemaTestHelper.testValidAndInvalid('informalConferenceTimes', {
-    valid: [['800-1000'], ['1000-1230', '1230-200'], ['200-430']],
-    invalid: [[], [''], ['1200'], ['1000-430'], ['800-1000', '800-1000'], ['800-1000', '1000-1230', '1230-200']],
+    valid: [['800-1000 ET'], ['1000-1230 ET', '1230-1400 ET'], ['1400-1630 ET']],
+    invalid: [
+      [''],
+      ['1200'],
+      ['1000-430 ET'],
+      ['800-1000 ET', '800-1000 ET'],
+      ['800-1000 ET', '1000-1230 ET', '1230-1400 ET'],
+    ],
   });
 
   schemaTestHelper.testValidAndInvalid('included', {
@@ -252,7 +123,7 @@ describe('20-0996 (Higher-Level Review) schema', () => {
         {
           type: 'ContestableIssue',
           attributes: {
-            notes: 'Lorem ipsum',
+            issue: 'Lorem ipsum',
             decisionIssueId: 12345,
             ratingIssueId: '012345',
             ratingDecisionIssueId: '67890',
@@ -261,7 +132,7 @@ describe('20-0996 (Higher-Level Review) schema', () => {
       ],
     ],
     invalid: [
-      [],
+      [{}],
       [
         {
           type: 'ContestableIssuez',
