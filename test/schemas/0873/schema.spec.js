aeecacb8f395a5d57cb293396142e971962c391b
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-08-06 13:47:38 -0400

    [department-of-veterans-affairs/orchid#17] adds json schema for IRIS Ask a Question form

---
+++ test/schemas/0873/schema.spec.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+import SchemaTestHelper from '../../support/schema-test-helper';
+import schemas from '../../../dist/schemas';
+import SharedTests from '../../support/shared-tests';
+
+const schema = schemas['0873'];
+
+const schemaDefaults = {
+  privacyAgreementAccepted: true,
+  fullName: {
+    first: 'bob',
+    last: 'smith',
+  },
+  preferredContactMethod: 'email',
+};
+
+const schemaTestHelper = new SchemaTestHelper(_.omit(schema, 'anyOf'), schemaDefaults);
+const sharedTests = new SharedTests(schemaTestHelper);
+
+describe('IRIS Ask a Question json schema', () => {
+  [
+    ['fullName', ['fullName']],
+    ['preferredContactMethod'],
+    ['email'],
+    ['phone', ['phone']],
+    ['address', ['address']],
+  ].forEach(test => {
+    sharedTests.runTest(...test);
+  });
+
+  schemaTestHelper.testValidAndInvalid('topic', {
+    valid: ['Policy Questions'],
+    invalid: ['foo'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('inquiryType', {
+    valid: ['Question'],
+    invalid: ['foo'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('query', {
+    valid: ['Some query'],
+    invalid: [3],
+  });
+});
