b438c9f1c8e13991fe0a80287cce4914cd4f3964
Jesse Cohn <jesse.cohn@adhocteam.us>
2020-08-31 12:10:44 -0700

    Added schema for chapter 36

--- src/schemas/28-8832/schema.js
+++ src/schemas/28-8832/schema.js
@@ -1,84 +1,104 @@
-import _ from 'lodash';
+import { merge } from 'lodash';
 import definitions from '../../common/definitions';
 import schemaHelpers from '../../common/schema-helpers';
+import { countries, states50AndDC } from '../../common/constants';
+
+// patterns
+const textOnlyPattern = '^(?!\\s)(?!.*?\\s{2,})[^<>%$#@!^&*0-9]+$';
+const numberAndDashPattern = '^[0-9]*[-]*[0-9]*[-]*[0-9]*$';
+
+const currencyAmountPattern = '^\\d+(\\.\\d{1,2})?$';
 
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'EDUCATIONAL/VOCATIONAL COUNSELING APPLICATION (28-8832)',
   type: 'object',
   additionalProperties: false,
-  definitions: _.pick(definitions, ['dateRange']),
+  definitions: {},
   properties: {
-    applicantEmail: {
-      type: 'string',
-      format: 'email',
-    },
-    applicantRelationshipToVeteran: {
-      type: 'string',
-    },
-    seekingRestorativeTraining: {
-      type: 'boolean',
-    },
-    seekingVocationalTraining: {
-      type: 'boolean',
-    },
-    receivedPamphlet: {
-      type: 'boolean',
-    },
-    veteranServiceBranch: {
-      type: 'string',
-    },
-    divorceOrAnnulmentPending: {
-      type: 'boolean',
-    },
-    applicantAgeAtRemarriage: {
-      type: 'integer',
-    },
-    previousBenefitApplications: {
+    claimantInformation: {
       type: 'object',
       properties: {
-        chapter31: {
-          type: 'boolean',
+        fullName: definitions.fullName,
+        ssn: definitions.ssn,
+        vaFileNumber: definitions.vaFileNumber,
+      },
+    },
+    claimantStaticInformation: {
+      type: 'object',
+      properties: {},
+    },
+    claimantAddress: {
+      type: 'object',
+      properties: {
+        'view:livesOnMilitaryBase': {
+          $ref: '#/definitions/genericTrueFalse',
         },
-        ownServiceBenefits: {
-          type: 'boolean',
+        'view:livesOnMilitaryBaseInfo': {
+          type: 'object',
+          properties: {},
         },
-        dic: {
-          type: 'boolean',
+        countryName: {
+          type: 'string',
+          enum: countries.map(country => country.value),
+          enumNames: countries.map(country => country.label),
         },
-        other: {
-          type: 'boolean',
+        addressLine1: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 100,
+          pattern: '^.*\\S.*',
         },
-        otherExplanation: {
+        addressLine2: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 100,
+          pattern: '^.*\\S.*',
+        },
+        addressLine3: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 100,
+          pattern: '^.*\\S.*',
+        },
+        city: {
+          type: 'string',
+        },
+        stateCode: {
+          type: 'string',
+          enum: states50AndDC.map(state => state.value),
+          enumNames: states50AndDC.map(state => state.label),
+        },
+        province: {
+          type: 'string',
+        },
+        zipCode: {
+          type: 'string',
+          pattern: '^\\d{5}$',
+        },
+        internationalPostalCode: {
           type: 'string',
         },
       },
     },
+    statusSelection: {
+      type: 'string',
+      enum: ['isActiveDuty', 'isVeteran', 'isSpouse', 'isChild'],
+      enumNames: [
+        'Active duty service member',
+        'Veteran',
+        'Spouse of a Veteran or service member',
+        'Child of a Veteran or service member',
+      ],
+    },
+    veteranInformation: {
+      type: 'object',
+      properties: {
+        fullName: definitions.fullName,
+        ssn: definitions.ssn,
+      },
+    },
   },
-  required: ['privacyAgreementAccepted'], // TODO Determine set of required fields
 };
-
-[
-  ['address', 'applicantAddress'],
-  ['date', 'applicantDateOfBirth'], // TODO Change if partial dates disallowed
-  ['date', 'veteranDateOfBirth'], // TODO Change if partial dates disallowed
-  ['date', 'veteranDateOfDeathMIAPOW'], // TODO Change if partial dates disallowed
-  ['fullName', 'applicantFullName'],
-  ['fullName', 'veteranFullName'],
-  ['fullName', 'previousVeteranBenefitsFullName'],
-  ['gender', 'applicantGender'],
-  ['gender', 'veteranGender'],
-  ['phone', 'applicantPrimaryPhone'],
-  ['phone', 'applicantOtherPhone'],
-  ['ssn', 'applicantSocialSecurityNumber'],
-  ['ssn', 'veteranSocialSecurityNumber'],
-  ['vaFileNumber', 'applicantVaFileNumber'],
-  ['vaFileNumber', 'veteranVaFileNumber'],
-  ['vaFileNumber', 'previousBenefitsVaFileNumber'],
-  ['serviceHistory', 'applicantServiceHistory'],
-  ['privacyAgreementAccepted'],
-].forEach(args => {
-  schemaHelpers.addDefinitionToSchema(schema, ...args);
-});
 
 export default schema;
--- test/schemas/28-8832/schema.spec.js
+++ test/schemas/28-8832/schema.spec.js
@@ -4,36 +4,13 @@ import SharedTests from '../../support/shared-tests';
 
 const schema = schemas['28-8832'];
 
-let schemaTestHelper = new SchemaTestHelper(
-  schema,
-  {
-    privacyAgreementAccepted: true
-  }
-);
-let sharedTests = new SharedTests(schemaTestHelper);
+const schemaTestHelper = new SchemaTestHelper(schema, {
+  privacyAgreementAccepted: true,
+});
+const sharedTests = new SharedTests(schemaTestHelper);
 
 describe('disabled veterans vocational rehabilitation schema', () => {
-  [
-    ['fullName', ['applicantFullName', 'veteranFullName', 'previousVeteranBenefitsFullName']],
-    ['ssn', ['applicantSocialSecurityNumber', 'veteranSocialSecurityNumber']],
-    ['date', ['applicantDateOfBirth', 'veteranDateOfBirth', 'veteranDateOfDeathMIAPOW']],
-    ['address', ['applicantAddress']],
-    ['phone', ['applicantPrimaryPhone', 'applicantOtherPhone']],
-    ['gender', ['applicantGender', 'veteranGender']],
-    ['email', ['applicantEmail']],
-    ['vaFileNumber', ['applicantVaFileNumber', 'veteranVaFileNumber', 'previousBenefitsVaFileNumber']],
-    ['serviceHistory', ['applicantServiceHistory']]
-  ].forEach((args) => {
+  [['ssn', ['dependentInformation.dependentInformation.ssn']]].forEach(args => {
     sharedTests.runTest(...args);
-  });
-  
-  schemaTestHelper.testValidAndInvalid('previousBenefitApplications', {
-    valid: [{
-      other: true,
-      otherExplanation: 'Another benefit application explanation'
-    }],
-    invalid: [{
-      other: 1
-    }]
   });
 });
