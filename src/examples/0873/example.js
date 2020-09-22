f67c0a9cfec4a4adff915aef7ab3d92b08095ff6
Kevin Mircovich <kmircovich1@gmail.com>
2020-09-22 14:59:44 -0400

    Add example for 0873

---
+++ src/examples/0873/example.js
@@ -0,0 +1,14 @@
+const example = {
+  topic: "Policy Question",
+  inquiryType: "Question",
+  query: "Some question?",
+  fullName: {
+    first: "Jane",
+    last: "Doe",
+  },
+  preferredContactMethod: "email",
+  email: "jane.doe@va.gov",
+  privacyAgreementAccepted: true,
+};
+
+export default example;
