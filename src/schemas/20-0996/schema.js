e947c012be2b8cba77fb623702355ee52e856530
Ed Mangimelli <edmangimelli@gmail.com>
2020-10-01 09:40:53 -0400

    change the 20-0996 schema to provide room for including data in the prefill that won't necessarily be submitted (but is necessary for display)

--- src/schemas/20-0996/schema.js
+++ src/schemas/20-0996/schema.js
@@ -1,1 +1,15 @@
-export default require('../HLR-create-request-body/schema.js').default;
+import { cloneDeep } from 'lodash';
+
+const schema = cloneDeep(require('../HLR-create-request-body/schema.js').default);
+
+const hlrCreate = schema.definitions.hlrCreate;
+
+schema.definitions.hlrCreate = {
+  ...hlrCreate,
+  properties: {
+    ...hlrCreate.properties,
+    nonPrefill: { type: 'object' },
+  },
+};
+
+export default schema;
