e0216945057692d1349e3fc6993b8d49a851e8e3
camerontesterman <cameronbtesterman@gmail.com>
2020-10-01 08:56:03 -0500

    adding tests

--- test/schemas/5655/schema.spec.js
+++ test/schemas/5655/schema.spec.js
@@ -1,4 +1,5 @@
-import _ from 'lodash';
+import { expect } from 'chai';
+import { _, cloneDeep } from 'lodash';
 import SchemaTestHelper from '../../support/schema-test-helper';
 import schemas from '../../../dist/schemas';
 import SharedTests from '../../support/shared-tests';
@@ -7,8 +8,35 @@ const schema = schemas['5655'];
 const schemaTestHelper = new SchemaTestHelper(schema);
 const sharedTests = new SharedTests(schemaTestHelper);
 
+const personalDataTestData = {
+  married: {
+    valid: [true, false],
+    invalid: ['yes', 123, null]
+  },
+};
+
 describe('5655 schema', () => {
+  expect(schema.required).to.deep.equal([
+    'personalData',
+    'income',
+    'expenses',
+    'discretionaryIncome',
+    'assets',
+    'installmentContractsAndOtherDebts',
+    'additionalData',
+  ]);
+
+  expect(schema.properties.personalData.required).to.deep.equal([
+    'veteranFullName',
+    'veteranAddress',
+    'phoneNumber',
+    'dateOfBirth',
+    'married',
+  ]);
+
+  // Personal Data
   sharedTests.runTest('fullName', ['personalData.veteranFullName', 'personalData.spouseFullName']);
   sharedTests.runTest('address', ['personalData.veteranAddress']);
   sharedTests.runTest('phone', ['personalData.phoneNumber']);
+  sharedTests.runTest('date', ['personalData.dateOfBirth']);
 });
