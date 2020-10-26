03d8be298d7c0639d66f9068f4562588e073b817
Micah Chiang <micahkchiang@gmail.com>
2020-10-26 16:05:37 -0400

    Add tests for chapter 31 schema

--- test/schemas/28-1900/schema.spec.js
+++ test/schemas/28-1900/schema.spec.js
@@ -1,12 +1,58 @@
-// import SchemaTestHelper from '../../support/schema-test-helper';
-// import schemas from '../../../dist/schemas';
-// import SharedTests from '../../support/shared-tests';
+import SchemaTestHelper from '../../support/schema-test-helper';
+import schemas from '../../../dist/schemas';
+import SharedTests from '../../support/shared-tests';
 
-// const schema = schemas['28-1900'];
+const schema = schemas['28-1900'];
 
-// const schemaTestHelper = new SchemaTestHelper(schema, {
-//   privacyAgreementAccepted: true,
-// });
-// const sharedTests = new SharedTests(schemaTestHelper);
+const schemaTestHelper = new SchemaTestHelper(schema);
+const sharedTests = new SharedTests(schemaTestHelper);
 
-describe.skip('disabled veterans vocational rehabilitation schema', () => {});
+const testData = {
+  yearsOfEducation: {
+    valid: ['10', '11', '5', '23', '4'],
+    invalid: ['four', 'seventeen', 'thirty', 'five', 'ten'],
+  },
+  boolean: {
+    valid: [true, false],
+    invalid: ['false', null, 42],
+  },
+  address: {
+    valid: [
+      {
+        country: 'USA',
+        street: '123 at home dr',
+        street2: 'apt 1',
+        city: 'a city',
+        state: 'a state',
+        postalCode: '12345',
+      },
+    ],
+    invalid: [
+      {
+        country: 'ABC',
+        street: true,
+        city: null,
+        state: false,
+        postalCode: 12345,
+      },
+    ],
+  },
+};
+
+describe('veteran readiness and employment', () => {
+  sharedTests.runTest('fullName', ['veteranInformation.fullName']);
+  sharedTests.runTest('ssn', ['veteranInformation.ssn']);
+  sharedTests.runTest('vaFileNumber', ['veteranInformation.vaFileNumber']);
+  sharedTests.runTest('date', ['veteranInformation.dob']);
+  sharedTests.runTest('email', ['email']);
+  sharedTests.runTest('phone', ['mainPhone', 'cellPhone']);
+  schemaTestHelper.testValidAndInvalid('isMoving', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('useEva', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('useTelecounseling', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('appointmentTimePreferences.morning', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('appointmentTimePreferences.midDay', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('appointmentTimePreferences.afternoon', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('appointmentTimePreferences.other', testData.boolean);
+  schemaTestHelper.testValidAndInvalid('veteranAddress', testData.address);
+  schemaTestHelper.testValidAndInvalid('newAddress', testData.address);
+});
