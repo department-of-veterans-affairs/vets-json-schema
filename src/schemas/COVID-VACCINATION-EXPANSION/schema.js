0d5bdb1ed7d3b7129a23ab8e11d3234cb90009d8
Patrick Bateman <batemapf@gmail.com>
2021-04-08 11:34:07 -0400

    bump version

--- src/schemas/COVID-VACCINATION-EXPANSION/schema.js
+++ src/schemas/COVID-VACCINATION-EXPANSION/schema.js
@@ -91,15 +91,14 @@ const schema = {
     veteranInformation: {
       type: 'object',
       properties: {
-        veteranBirthDate: {
+        veteranFirstName: {
           type: 'string',
-          pattern: '^\\d{4}-\\d{2}-\\d{2}$',
         },
-        veteranSsn: {
+        veteranLastName: {
           type: 'string',
         },
       },
-      required: ['veteranBirthDate', 'veteranSsn'],
+      required: ['veteranFirstName', 'veteranLastName'],
     },
     personalInformation: {
       type: 'object',
