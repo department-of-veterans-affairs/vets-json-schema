0bc8fc3e9b2d582fb0b027dc5f5d6861480ab014
Mike <michel.mcdonald@adhocteam.us>
2021-10-26 19:38:25 -0400

    Update comment

--- src/schemas/HLR-get-legacy-appeals-response-200/schema.js
+++ src/schemas/HLR-get-legacy-appeals-response-200/schema.js
@@ -1,4 +1,4 @@
-// This schema is pulled directly from the OpenAPI documentation provided by Lighthouse https://api.va.gov/services/appeals/docs/v1/decision_reviews
+// This schema is pulled directly from the OpenAPI documentation provided by Lighthouse https://api.va.gov/services/appeals/docs/v2/decision_reviews
 
 import schema from './schema.json';
 schema.$schema = 'http://json-schema.org/draft-04/schema#';
