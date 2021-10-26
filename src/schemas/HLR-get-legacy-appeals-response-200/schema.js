fd1a0e84ec752c4f57b703c03150ecae8222a5e3
Mike <michel.mcdonald@adhocteam.us>
2021-10-26 19:18:52 -0400

    Add new Lighthouse Legacy Appeals Endpoint schems

---
+++ src/schemas/HLR-get-legacy-appeals-response-200/schema.js
@@ -0,0 +1,5 @@
+// This schema is pulled directly from the OpenAPI documentation provided by Lighthouse https://api.va.gov/services/appeals/docs/v1/decision_reviews
+
+import schema from './schema.json';
+schema.$schema = 'http://json-schema.org/draft-04/schema#';
+export default schema;
