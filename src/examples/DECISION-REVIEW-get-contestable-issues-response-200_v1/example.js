f22a498e76e0781bb391ce6bb0b6c50e74266dcb
Scott Meyers <scottmey@gmail.com>
2022-07-22 10:20:22 -0400

    adds example

--- src/examples/DECISION-REVIEW-get-contestable-issues-response-200_v1/example.js
+++ src/examples/DECISION-REVIEW-get-contestable-issues-response-200_v1/example.js
@@ -1,1 +1,1 @@
-export default require('../../schemas/DECISION-REVIEW-get-contestable-issues-200_v1/example.json');
+export default require('../../schemas/DECISION-REVIEW-get-contestable-issues-response-200_v1/example.json');
