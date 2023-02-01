cf055d39174dd6ab00b27599af2f42e93d15925d
Tze-chiu Lei <tclei2009@gmail.com>
2023-01-31 17:40:44 -0700

    Build 26-4555 schema

--- src/schemas/26-4555/schema.js
+++ src/schemas/26-4555/schema.js
@@ -1,191 +1,109 @@
+import cloneDeep from 'lodash/cloneDeep';
+import pick from 'lodash/pick';
+
+import commonDefinitions from '../../common/definitions';
+
+const definitions = cloneDeep(commonDefinitions);
+
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Apply for Specially Adapted Housing Grant (26-4555)',
   type: 'object',
   additionalProperties: false,
-  definitions: {
-    address: {
+  definitions: pick(definitions, ['date', 'email', 'fullName', 'phone', 'profileAddress', 'ssn', 'vaFileNumber']),
+  properties: {
+    // "Step 1"
+    personalChapter: {
       type: 'object',
-      oneOf: [
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['CAN'],
-            },
-            state: {
-              type: 'string',
-              enum: ['AB', 'BC', 'MB', 'NB', 'NF', 'NT', 'NV', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['MEX'],
-            },
-            state: {
-              type: 'string',
-              enum: [
-                'aguascalientes',
-                'baja-california-norte',
-                'baja-california-sur',
-                'campeche',
-                'chiapas',
-                'chihuahua',
-                'coahuila',
-                'colima',
-                'distrito-federal',
-                'durango',
-                'guanajuato',
-                'guerrero',
-                'hidalgo',
-                'jalisco',
-                'mexico',
-                'michoacan',
-                'morelos',
-                'nayarit',
-                'nuevo-leon',
-                'oaxaca',
-                'puebla',
-                'queretaro',
-                'quintana-roo',
-                'san-luis-potosi',
-                'sinaloa',
-                'sonora',
-                'tabasco',
-                'tamaulipas',
-                'tlaxcala',
-                'veracruz',
-                'yucatan',
-                'zacatecas',
-              ],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['USA'],
-            },
-            state: {
-              type: 'string',
-              enum: [
-                'AL',
-                'AK',
-                'AS',
-                'AZ',
-                'AR',
-                'AA',
-                'AE',
-                'AP',
-                'CA',
-                'CO',
-                'CT',
-                'DE',
-                'DC',
-                'FM',
-                'FL',
-                'GA',
-                'GU',
-                'HI',
-                'ID',
-                'IL',
-                'IN',
-                'IA',
-                'KS',
-                'KY',
-                'LA',
-                'ME',
-                'MH',
-                'MD',
-                'MA',
-                'MI',
-                'MN',
-                'MS',
-                'MO',
-                'MT',
-                'NE',
-                'NV',
-                'NH',
-                'NJ',
-                'NM',
-                'NY',
-                'NC',
-                'ND',
-                'MP',
-                'OH',
-                'OK',
-                'OR',
-                'PW',
-                'PA',
-                'PR',
-                'RI',
-                'SC',
-                'SD',
-                'TN',
-                'TX',
-                'UT',
-                'VT',
-                'VI',
-                'VA',
-                'WA',
-                'WV',
-                'WI',
-                'WY',
-              ],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              not: {
-                type: 'string',
-                enum: ['CAN', 'MEX', 'USA'],
-              },
-            },
-            state: {
-              type: 'string',
-              maxLength: 51,
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 51,
-            },
-          },
-        },
-      ],
+      // additionalProperties: false,
       properties: {
-        street: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 50,
+        personalPage1: {
+          type: 'object',
+          properties: {
+            fullName: {
+              $ref: '#/definitions/fullName',
+            },
+            birthDate: {
+              $ref: '#/definitions/date',
+            },
+          },
         },
-        street2: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 50,
+        personalPage2: {
+          type: 'object',
+          properties: {
+            ssn: { $ref: '#/definitions/ssn' },
+            vaFileNumber: { $ref: '#/definitions/vaFileNumber' },
+          },
         },
-        city: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 51,
+      },
+    },
+    // "Step 2"
+    contactChapter: {
+      type: 'object',
+      contactPage1: {
+        type: 'object',
+        properties: {
+          address: { $ref: '#/definitions/profileAddress' },
         },
+      },
+      contactPage2: {
+        type: 'object',
+        properties: {
+          homePhone: { $ref: '#/definitions/phone' },
+          mobilePhone: { $ref: '#/definitions/phone' },
+          email: { $ref: '#/definitions/email' },
+        },
+      },
+    },
+    // "Step 3"
+    previousApplicationsChapter: {
+      type: 'object',
+      previousSahApplicationPage1: {
+        type: 'object',
+        properties: {
+          hasPreviousSahApplication: { type: 'boolean ' },
+        },
+      },
+      previousSahApplicationPage2: {
+        type: 'object',
+        properties: {
+          previousSahApplicationDate: { $ref: '#/definitions/date ' },
+          previousSahApplicationAddress: { $ref: '#/definitions/address' },
+        },
+      },
+      previousHiApplicationPage1: {
+        type: 'object',
+        properties: {
+          hasPreviousHiApplication: { type: 'boolean' },
+        },
+      },
+      previousHiApplicationPage2: {
+        type: 'object',
+        properties: {
+          previousHiApplicationDate: { $ref: '#/definitions/date ' },
+          previousHiApplicationAddress: { $ref: '#/definitions/profileAddress' },
+        },
+      },
+    },
+    // Step 4
+    livingSituationChapter: {
+      type: 'object',
+      livingSituationPage1: {
+        type: 'object',
+        properties: {
+          isInCareFacility: { type: 'boolean' },
+        },
+      },
+      livingSituationPage2: {
+        type: 'object',
+        properties: {
+          careFacilityName: { type: 'string' },
+          careFacilityAddress: { $ref: '#/definitions/profileAddress' },
+        },
+      },
+      livingSituationPage3: {
+        type: 'object',
+        otherMedicalInformation: { type: 'string' },
       },
     },
   },
