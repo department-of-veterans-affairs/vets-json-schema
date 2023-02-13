ecbc43bccdc8789d629597a4fb515c79a2be7bc0
Tze-chiu Lei <tclei2009@gmail.com>
2023-02-13 15:28:53 -0700

    Add schema for new VA Form 26-4555 (#746)
    * Build 26-4555 schema
    
    * Refactor 26-4555 schema
    
    * Refine 26-4555 schema
    
    * Add 26-4555 schema test-spec
    
    * Update 26-4555 json-schema
    
    * Refactor 26-4555 schema
    
    * Fix 26-4555 schema
    
    * Remove 26-4555 schema's formNumber prop
    
    * Generate updated 26-4555 json-schema
    
    * Bump to v20.25.3
    
    * Change version to 2.25.0
    
    * Change version to v20.25.0
    
    * Fix 26-4555 schema
    
    * Fix 26-4555 schema
    
    * Fix 26-4555 schema-test
    
    * Remove duplicate tests from 26-4555 spec
    
    * Remove more duplicate tests from 26-4555 spec
    
    * Fixing tests by using lodash omit to remove required properties
    
    * Add custom-pattern comment to 26-4555 schema-spec
    
    * Switch 26-4555 veteran address prop to profileAddress
    
    ---------
    
    Co-authored-by: Nick Sprinkle <ndsprinkle@gmail.com>

--- src/schemas/26-4555/schema.js
+++ src/schemas/26-4555/schema.js
@@ -1,192 +1,83 @@
+import cloneDeep from 'lodash/cloneDeep';
+import pick from 'lodash/pick';
+
+import commonDefinitions from '../../common/definitions';
+
+const definitions = cloneDeep(commonDefinitions);
+
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Apply for Specially Adapted Housing Grant (26-4555)',
   type: 'object',
   additionalProperties: false,
-  definitions: {
-    address: {
+  definitions: pick(definitions, [
+    'address',
+    'date',
+    'email',
+    'fullName',
+    'phone',
+    'profileAddress',
+    'ssn',
+    'vaFileNumber',
+  ]),
+  properties: {
+    veteran: {
       type: 'object',
-      oneOf: [
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['CAN'],
-            },
-            state: {
-              type: 'string',
-              enum: ['AB', 'BC', 'MB', 'NB', 'NF', 'NT', 'NV', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['MEX'],
-            },
-            state: {
-              type: 'string',
-              enum: [
-                'aguascalientes',
-                'baja-california-norte',
-                'baja-california-sur',
-                'campeche',
-                'chiapas',
-                'chihuahua',
-                'coahuila',
-                'colima',
-                'distrito-federal',
-                'durango',
-                'guanajuato',
-                'guerrero',
-                'hidalgo',
-                'jalisco',
-                'mexico',
-                'michoacan',
-                'morelos',
-                'nayarit',
-                'nuevo-leon',
-                'oaxaca',
-                'puebla',
-                'queretaro',
-                'quintana-roo',
-                'san-luis-potosi',
-                'sinaloa',
-                'sonora',
-                'tabasco',
-                'tamaulipas',
-                'tlaxcala',
-                'veracruz',
-                'yucatan',
-                'zacatecas',
-              ],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              type: 'string',
-              enum: ['USA'],
-            },
-            state: {
-              type: 'string',
-              enum: [
-                'AL',
-                'AK',
-                'AS',
-                'AZ',
-                'AR',
-                'AA',
-                'AE',
-                'AP',
-                'CA',
-                'CO',
-                'CT',
-                'DE',
-                'DC',
-                'FM',
-                'FL',
-                'GA',
-                'GU',
-                'HI',
-                'ID',
-                'IL',
-                'IN',
-                'IA',
-                'KS',
-                'KY',
-                'LA',
-                'ME',
-                'MH',
-                'MD',
-                'MA',
-                'MI',
-                'MN',
-                'MS',
-                'MO',
-                'MT',
-                'NE',
-                'NV',
-                'NH',
-                'NJ',
-                'NM',
-                'NY',
-                'NC',
-                'ND',
-                'MP',
-                'OH',
-                'OK',
-                'OR',
-                'PW',
-                'PA',
-                'PR',
-                'RI',
-                'SC',
-                'SD',
-                'TN',
-                'TX',
-                'UT',
-                'VT',
-                'VI',
-                'VA',
-                'WA',
-                'WV',
-                'WI',
-                'WY',
-              ],
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 10,
-            },
-          },
-        },
-        {
-          properties: {
-            country: {
-              not: {
-                type: 'string',
-                enum: ['CAN', 'MEX', 'USA'],
-              },
-            },
-            state: {
-              type: 'string',
-              maxLength: 51,
-            },
-            postalCode: {
-              type: 'string',
-              maxLength: 51,
-            },
-          },
-        },
-      ],
+      additionalProperties: false,
+      required: ['fullName', 'ssn', 'address', 'homePhone'],
       properties: {
-        street: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 50,
+        fullName: {
+          $ref: '#/definitions/fullName',
         },
-        street2: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 50,
+        dateOfBirth: {
+          $ref: '#/definitions/date',
         },
-        city: {
-          type: 'string',
-          minLength: 1,
-          maxLength: 51,
-        },
+        ssn: { $ref: '#/definitions/ssn' },
+        vaFileNumber: { $ref: '#/definitions/vaFileNumber' },
+        address: { $ref: '#/definitions/profileAddress' },
+        homePhone: { $ref: '#/definitions/phone' },
+        mobilePhone: { $ref: '#/definitions/phone' },
+        email: { $ref: '#/definitions/email' },
       },
+    },
+    previousSahApplication: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['hasPreviousSahApplication'],
+      properties: {
+        hasPreviousSahApplication: { type: 'boolean' },
+        // next 2 props are on a separate, conditional form-page
+        // split from 1 single field on the PDF-form
+        previousSahApplicationDate: { $ref: '#/definitions/date' },
+        previousSahApplicationAddress: { $ref: '#/definitions/address' },
+      },
+    },
+    previousHiApplication: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['hasPreviousHiApplication'],
+      properties: {
+        hasPreviousHiApplication: { type: 'boolean' },
+        // next 2 props are on a separate, conditional form-page
+        // split from 1 single field on the PDF-form
+        previousHiApplicationDate: { $ref: '#/definitions/date' },
+        previousHiApplicationAddress: { $ref: '#/definitions/address' },
+      },
+    },
+    livingSituation: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['isInCareFacility'],
+      properties: {
+        isInCareFacility: { type: 'boolean' },
+        // next 2 props are on a separate, conditional form-page
+        // split from 1 single field on the PDF-form
+        careFacilityName: { type: 'string' },
+        careFacilityAddress: { $ref: '#/definitions/address' },
+      },
+    },
+    remarks: {
+      type: 'string',
     },
   },
 };
