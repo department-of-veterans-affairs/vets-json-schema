ff4af34c217df2b702c11ac7540831e079b1807c
Tze-chiu Lei <tze@adhocteam.us>
2023-04-06 16:30:35 -0700

    Add form 21-10210

---
+++ src/schemas/21-10210/schema.js
@@ -0,0 +1,95 @@
+import cloneDeep from 'lodash/cloneDeep';
+import pick from 'lodash/pick';
+
+import commonDefinitions from '../../common/definitions';
+
+const definitions = cloneDeep(commonDefinitions);
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'Lay/Witness Statement (21-10210)',
+  type: 'object',
+  additionalProperties: false,
+  definitions: pick(definitions, [
+    'address',
+    'date',
+    'email',
+    'fullName',
+    'phone',
+    'profileAddress',
+    'ssn',
+    'centralMailVaFile',
+    'privacyAgreementAccepted',
+  ]),
+  properties: {
+    claimInformation: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['claimOwner', 'claimantType'],
+      properties: {
+        claimOwner: {
+          type: 'string',
+          enum: ['mine', 'someone-else'],
+        },
+        claimantType: {
+          type: 'string',
+          enum: ['veteran', 'non-veteran'],
+        },
+      },
+    },
+    nonVeteran: {
+      type: 'object',
+      additionalProperties: false,
+      required: [],
+      properties: {
+        fullName: {
+          $ref: '#/definitions/fullName',
+        },
+        relationshipToVeteran: {
+          type: 'string',
+          enum: ['served-with', 'family-or-friend', 'coworker-or-supervisor'],
+        },
+        relationshipToVeteranOther: {
+          type: 'string',
+        },
+        phone: { $ref: '#/definitions/phone' },
+        email: { $ref: '#/definitions/email' },
+        agreeToReceiveEmails: {
+          type: 'boolean',
+        },
+      },
+    },
+    veteran: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['fullName', 'ssn', 'address', 'homePhone'],
+      properties: {
+        fullName: {
+          $ref: '#/definitions/fullName',
+        },
+        dateOfBirth: {
+          $ref: '#/definitions/date',
+        },
+        ssn: { $ref: '#/definitions/ssn' },
+        vaFileNumber: { $ref: '#/definitions/centralMailVaFile' },
+        vaInsFileNumber: {
+          type: 'string',
+          // TODO: Add pattern.
+        },
+        address: { $ref: '#/definitions/profileAddress' },
+        homePhone: { $ref: '#/definitions/phone' },
+        mobilePhone: { $ref: '#/definitions/phone' },
+        email: { $ref: '#/definitions/email' },
+      },
+    },
+    statement: {
+      type: 'string',
+    },
+    privacyAgreementAccepted: {
+      $ref: '#/definitions/privacyAgreementAccepted',
+    },
+  },
+  required: ['statement', 'privacyAgreementAccepted'],
+};
+
+export default schema;
---
+++ test/schemas/21-10210/schema.spec.js
@@ -0,0 +1,138 @@
+import _ from 'lodash';
+import SchemaTestHelper from '../../support/schema-test-helper';
+import schemas from '../../../dist/schemas';
+import SharedTests from '../../support/shared-tests';
+import fixtures from '../../support/fixtures';
+
+const schema = schemas['21-10210'];
+const usAddressFixture = {
+  ...fixtures.address,
+  state: 'AL',
+  postalCode: '54321',
+};
+
+const schemaDefaults = {
+  claimInformation: {
+    claimOwner: 'mine',
+    claimantType: 'veteran',
+  },
+  nonVeteran: {
+    fullName: {
+      first: 'Jane',
+      last: 'Doe',
+    },
+    phone: fixtures.phone,
+    email: 'jane.doe@example.com',
+    agreeToReceiveEmails: true,
+  },
+  veteran: {
+    fullName: {
+      first: 'John',
+      last: 'Doe',
+    },
+    ssn: fixtures.ssn,
+    address: usAddressFixture,
+    homePhone: fixtures.phone,
+    mobilePhone: fixtures.phone,
+    email: 'john.doe@example.com',
+  },
+  statement: '[Test statement content]',
+  privacyAgreementAccepted: true,
+};
+
+const testData = {
+  claimInformation: {
+    valid: [
+      {
+        claimOwner: 'mine',
+        claimantType: 'veteran',
+      },
+      {
+        claimOwner: 'mine',
+        claimantType: 'non-veteran',
+      },
+      {
+        claimOwner: 'someone-else',
+        claimantType: 'non-veteran',
+      },
+      {
+        claimOwner: 'someone-else',
+        claimantType: 'veteran',
+      },
+    ],
+    invalid: [
+      {
+        claimOwner: 'my-own',
+        claimantType: '1st-party',
+      },
+      {
+        claimOwner: '',
+        claimantType: '',
+      },
+      {
+        claimOwner: 1,
+        claimantType: true,
+      },
+    ],
+  },
+  address: {
+    valid: [
+      {
+        country: 'USA',
+        street: '123 at home dr',
+        street2: 'apt 1',
+        city: 'a city',
+        state: 'AL',
+        postalCode: '12345',
+      },
+    ],
+    invalid: [
+      {
+        country: 'ABC',
+        street: true,
+        city: null,
+        state: false,
+        postalCode: 12345,
+      },
+    ],
+  },
+  statement: {
+    valid: [
+      'This is a valid statement',
+      'This is a valid statement with a number 1',
+      'This is a valid statement with a number 1 and a symbol !',
+    ],
+    invalid: [123, true, null, '', 'This is an invalid statement with a symbol #'],
+  },
+};
+
+// need to remove any required props from schema first,
+// in order for tests to work.
+const schemaTestHelper = new SchemaTestHelper(
+  _.omit(
+    schema,
+    'anyOf',
+    'required',
+    'properties.claimInformation.required',
+    'properties.nonVeteran.required',
+    'properties.veteran.required',
+  ),
+  schemaDefaults,
+);
+const sharedTests = new SharedTests(schemaTestHelper);
+
+describe('21-10210 Lay/Witness Statement json-schema', () => {
+  [
+    ['date', ['veteran.dateOfBirth']],
+    ['email', ['veteran.email', 'nonVeteran.email']],
+    ['fullName', ['veteran.fullName', 'nonVeteran.fullName']],
+    ['phone', ['veteran.homePhone', 'veteran.mobilePhone', 'nonVeteran.phone']],
+    ['ssn', ['veteran.ssn']],
+    ['address', ['veteran.address']],
+  ].forEach(test => {
+    sharedTests.runTest(...test);
+  });
+
+  schemaTestHelper.testValidAndInvalid('veteran.address', testData.address);
+  schemaTestHelper.testValidAndInvalid('claimInformation', testData.claimInformation);
+});
