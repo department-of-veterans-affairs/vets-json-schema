65b38067bcb3c7e009917fbc48182a61c6f11ad0
Lihan Li <frankieteardrop@gmail.com>
2023-04-08 02:29:53 +0700

    only freeze 1010 schemas

--- lib/vets_json_schema.rb
+++ lib/vets_json_schema.rb
@@ -5,6 +5,11 @@ require 'ice_nine'
 module VetsJsonSchema
   base_dir = "#{__dir__}/../"
 
+  FROZEN_SCHEMAS = %w[
+    10-10CG
+    10-10EZ
+  ]
+
   CONSTANTS = IceNine.deep_freeze(
     MultiJson.load(
       File.read("#{base_dir}dist/constants.json")
@@ -17,9 +22,13 @@ module VetsJsonSchema
     ScriptUtils.directories("#{base_dir}src/schemas").each do |schema|
       schema = File.basename(schema).upcase
       return_val[schema] = MultiJson.load(File.read("#{base_dir}dist/#{schema}-schema.json"))
+
+      if FROZEN_SCHEMAS.include?(schema)
+        return_val[schema] = IceNine.deep_freeze(return_val[schema])
+      end
     end
 
-    IceNine.deep_freeze(return_val)
+    return_val
   end.()
 
   # Provides an example of valid form data conforming to given schema
