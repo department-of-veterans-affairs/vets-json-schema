767390179991e5eaf076eff0b61fde311fdda25f
lihan li <frankieteardrop@gmail.com>
2023-08-20 19:10:38 -0700

    remove old provider spec

--- test/schemas/10-10EZR/schema.spec.js
+++ test/schemas/10-10EZR/schema.spec.js
@@ -70,44 +70,6 @@ describe('1010ezr json schema', () => {
     });
   });
 
-  describe('provider', () => {
-    const providerValidation = definitionValidator('provider');
-
-    [
-      ['insuranceName', 100],
-      ['insurancePolicyHolderName', 50],
-      ['insurancePolicyNumber', 30],
-      ['insuranceGroupCode', 30],
-    ].forEach(providerFieldData => {
-      const providerField = providerFieldData[0];
-      const providerFieldMaxLength = providerFieldData[1];
-      const policyNumber = {
-        insurancePolicyNumber: '123',
-      };
-
-      it(`allows ${providerField} with less than ${providerFieldMaxLength} chars`, () => {
-        expect(providerValidation({ ...policyNumber, [providerField]: stringGenerate(providerFieldMaxLength) })).to.be
-          .true;
-      });
-
-      it(`doesnt allow ${providerField} with more than ${providerFieldMaxLength} chars`, () => {
-        expect(providerValidation({ ...policyNumber, [providerField]: stringGenerate(providerFieldMaxLength + 1) })).to
-          .be.false;
-      });
-    });
-
-    it('requires policy number or group code', () => {
-      expect(providerValidation({ insurancePolicyNumber: '123' })).to.be.true;
-      expect(providerValidation({ insuranceGroupCode: '123' })).to.be.true;
-      expect(providerValidation({})).to.be.false;
-    });
-
-    it('doesnt allow only spaces for insurancePolicyNumber or insuranceGroupCode', () => {
-      expect(providerValidation({ insuranceGroupCode: ' ' })).to.be.false;
-      expect(providerValidation({ insurancePolicyNumber: ' ' })).to.be.false;
-    });
-  });
-
   schemaTestHelper.testValidAndInvalid('veteranFullName', {
     valid: [
       {
