76ce6de1c10b471bee312cb55ac97ef5283343f1
Annie Tran <annie.tran@coforma.io>
2023-12-05 12:14:02 -0600

    remove timezone enum for HLR create request body v1

--- src/schemas/HLR-create-request-body_v1/schema.js
+++ src/schemas/HLR-create-request-body_v1/schema.js
@@ -1,4 +1,4 @@
 // The 20-0996 schema is supplied to us from Lighthouse: https://api.va.gov/services/appeals/v2/decision_reviews/higher_level_reviews/schema
 import schema from './schema.json';
-schema.$schema = 'http://json-schema.org/draft-07/schema#';
+schema.$schema = 'http://json-schema.org/draft-07/schema#'; // draft-07 not supported by Ajv testing tool
 export default schema;
