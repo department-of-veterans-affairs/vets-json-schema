896ed79041ca974a6451e6bf0eaa06306c666850
Jarred Simon <jarred.simon@va.gov>
2024-02-29 08:52:08 -0600

    #VYE-224 #comment address direct deposit schemas created with unit tests

--- src/schemas/VYE-Address-Change/schema.js
+++ src/schemas/VYE-Address-Change/schema.js
@@ -1,44 +1,81 @@
-import _ from 'lodash';
-import definitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
+import constants from '../../../dist/constants.json';
+
+// create an object of states using this pattern - {abbreviation}: {full name}
+// e.g. "DC": "District of Columnbia"
+const STATES = constants.states.USA.reduce(
+  (stateList, { value, label }) => ({
+    ...stateList,
+    [value]: label,
+  }),
+  {},
+);
+
+export const blockURLsRegEx =
+  '^((?!http|www\\.|\\.co|\\.net|\\.gov|\\.edu|\\.org).)*$';
+
+const STREET_LINE_MAX_LENGTH = 20;
 
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'VERIFY YOUR ENROLLMENT CHANGE OF ADDRESS',
   type: 'object',
-  definitions:_.merge(
-    _.set(
-      _.pick(definitions,['address']),
-      'address.properties',
-      {
-        "street": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 20
-        },
-        "street2": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 20
-        },
-        "street3": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 20
-        },
-        "city": {
-          "type": "string",
-          "minLength": 1,
-          "maxLength": 51
-        }
-      }
-    )
-  ),
   additionalProperties: false,
+  definitions:{},
   properties: {
-    veteranAddress: {
-      $ref: '#/definitions/address',
+
+  veteranName: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 100,
     },
+  address1: {
+      type: 'string',
+      minLength: 1,
+      maxLength: STREET_LINE_MAX_LENGTH,
+      pattern: blockURLsRegEx,
   },
-  required: ['veteranAddress'],
+  address2: {
+      type: 'string',
+      minLength: 1,
+      maxLength: STREET_LINE_MAX_LENGTH,
+      pattern: blockURLsRegEx,
+  },
+  address3: {
+      type: 'string',
+      minLength: 1,
+      maxLength: STREET_LINE_MAX_LENGTH,
+      pattern: blockURLsRegEx,
+  },
+  address4: {
+      type: 'string',
+      minLength: 1,
+      maxLength: STREET_LINE_MAX_LENGTH,
+      pattern: blockURLsRegEx,
+  },
+  city: {
+      type: 'string',
+      pattern: blockURLsRegEx,
+      minLength: 1,
+      maxLength: 51,
+  },
+  state: {
+      type: 'string',
+      enum: Object.keys(STATES),
+      enumNames: Object.values(STATES),
+  },
+  zipCode: {
+      type: 'string',
+      pattern: '^\\d{5}$',
+      minLength: 5,
+      maxLength: 5,
+  },
+  },
+  required: ['veteranName','address1', 'city', 'state', 'zipCode'],
 };
+
+[].forEach(args => {
+  schemaHelpers.addDefinitionToSchema(schema, ...args);
+});
+
 export default schema;
--- test/schemas/VYE-Address-Change/schema.spec.js
+++ test/schemas/VYE-Address-Change/schema.spec.js
@@ -1,11 +1,67 @@
 import SchemaTestHelper from '../../support/schema-test-helper';
 import schemas from '../../../dist/schemas';
-import SharedTests from '../../support/shared-tests';
+// import SharedTests from '../../support/shared-tests';
 
 const schema = schemas['VYE-ADDRESS-CHANGE'];
-const schemaTestHelper = new SchemaTestHelper(schema);
-const sharedTests = new SharedTests(schemaTestHelper)
+
+const schemaTestHelper = new SchemaTestHelper(schema,{
+  veteranName:'John T. Doe',
+  address1:'123 Test address',
+  address2:'123 Test address',
+  address3:'123 Test address',
+  address4:'123 Test address',
+  city:'Norman',
+  state:'OK',
+  zipCode:'12345'
+});
+
+// const sharedTests = new SharedTests(schemaTestHelper)
 
 describe('VYE address change', () => {
-  sharedTests.runTest('address', ['veteranAddress']);
+
+  schemaTestHelper.testValidAndInvalid('veteranName', {
+    valid: ['John T. Doe'],
+    invalid: [123],
+  });
+
+  schemaTestHelper.testValidAndInvalid('address1', {
+    valid: ['123 Test address'],
+    invalid: ['123 Test address APT 2011'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('address2', {
+    valid: ['123 Test address'],
+    invalid: ['123 Test address APT 2011'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('address3', {
+    valid: ['123 Test address'],
+    invalid: ['123 Test address APT 2011'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('address4', {
+    valid: ['123 Test address'],
+    invalid: ['123 Test address APT 2011'],
+  });
+
+  schemaTestHelper.testValidAndInvalid('city', {
+    valid: ['Norman'],
+    invalid: [123],
+  });
+
+  schemaTestHelper.testValidAndInvalid('state', {
+    valid: ['OK'],
+    invalid: [123],
+  });
+
+  schemaTestHelper.testValidAndInvalid('state', {
+    valid: ['TX'],
+    invalid: ['ZQ'],
+  });
+
+
+  schemaTestHelper.testValidAndInvalid('zipCode', {
+    valid: ['12345'],
+    invalid: ['123'],
+  });
 });
