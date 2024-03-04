9cfe2f2a5d6a340aab090a48b23219d7805a9b04
lihan li <830084+lihanli@users.noreply.github.com>
2024-03-04 13:07:18 -0800

    add tera test to ezr

--- test/schemas/10-10EZR/schema.spec.js
+++ test/schemas/10-10EZR/schema.spec.js
@@ -1,20 +1,18 @@
 import { omit } from 'lodash';
 import schemas from '../../../dist/schemas';
 import SchemaTestHelper from '../../support/schema-test-helper';
+import { definitions } from '../../../dist/schemas';
+import { expect } from 'chai';
+import { difference } from 'lodash';
 
 const applicationSchema = schemas['10-10EZR'];
 
 const schemaTestHelper = new SchemaTestHelper(omit(applicationSchema, 'required'));
 
 describe('ezr json schema', () => {
-  schemaTestHelper.testValidAndInvalid('otherToxicExposure', {
-    valid: [
-      'foo',
-      'foo bar',
-      'Foo123'
-    ],
-    invalid: [
-      '$'
-    ],
+  it('has tera fields', () => {
+    expect(
+      difference(Object.keys(definitions.teraQuestions), Object.keys(applicationSchema.properties)).length
+    ).to.equal(0);
   });
 });
