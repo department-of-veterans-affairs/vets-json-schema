b9e207556c2d1fcd9d00ad58d2c3d830b5f53009
Matt Long <longmd@me.com>
2024-03-07 14:02:25 -0500

    Update 10-10EZ/ERZ schemas for validation on insurance fields (#853)

--- test/common/definitions.spec.js
+++ test/common/definitions.spec.js
@@ -40,6 +40,8 @@ describe('schema definitions', () => {
         insuranceGroupCode: stringGenerate(31),
       },
       {},
+      { insuranceName: ' ' },
+      { insurancePolicyHolderName: ' ' },
       { insuranceGroupCode: ' ' },
       { insurancePolicyNumber: ' ' },
     ],
