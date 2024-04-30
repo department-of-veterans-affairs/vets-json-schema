138d212eef59437d16572a7c3af1d8cf51c917fd
Midge-dev <sean.midgley.dev@gmail.com>
2024-04-30 14:24:51 -0700

    added type and defs

--- src/schemas/686c-674-v2/schema.js
+++ src/schemas/686c-674-v2/schema.js
@@ -1,1 +1,7 @@
-export default {};
+export default {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'SUPPLEMENTAL CLAIM FOR COMPENSATION (21-686C & 21-674)',
+  type: 'object',
+  definitions: {},
+  properties: {},
+};
