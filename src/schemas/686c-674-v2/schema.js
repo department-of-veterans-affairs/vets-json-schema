ad396ff29199653506750265e21289ab4782a814
Sean Midgley <57480791+Midge-dev@users.noreply.github.com>
2024-05-02 09:04:27 -0700

    Dependents: Created blank placeholder schema for 686 v2 (#889)
    * created blank placeholder schema for v2
    
    * added type and defs
    
    * version
    
    * version again

---
+++ src/schemas/686c-674-v2/schema.js
@@ -0,0 +1,7 @@
+export default {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'SUPPLEMENTAL CLAIM FOR COMPENSATION (21-686C & 21-674)',
+  type: 'object',
+  definitions: {},
+  properties: {},
+};
