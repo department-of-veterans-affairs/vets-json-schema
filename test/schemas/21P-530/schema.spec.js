1b49f7f612226013602db7e21b0f56b9a6e46608
Dakota Larson <133443504+DLarson-Oddball@users.noreply.github.com>
2024-06-17 14:10:49 -0700

    Revert "Copy burials v2 schema to v1. (#909)"
    This reverts commit 80587a2843e2c38236a0ce8cdba8ec5c3e723041.

--- test/schemas/21P-530/schema.spec.js
+++ test/schemas/21P-530/schema.spec.js
@@ -1,20 +1,20 @@
-import _ from 'lodash';
-import { expect } from 'chai';
 import SchemaTestHelper from '../../support/schema-test-helper';
 import schemas from '../../../dist/schemas';
 import fixtures from '../../support/fixtures';
+import _ from 'lodash';
 import SharedTests from '../../support/shared-tests';
+import { expect } from 'chai';
 
 const schema = schemas['21P-530'];
-const schemaWithoutRequired = _.cloneDeep(schema);
+let schemaWithoutRequired = _.cloneDeep(schema);
 delete schemaWithoutRequired.required;
 delete schemaWithoutRequired.anyOf;
 
-const schemaTestHelper = new SchemaTestHelper(schemaWithoutRequired);
-const sharedTests = new SharedTests(schemaTestHelper);
+let schemaTestHelper = new SchemaTestHelper(schemaWithoutRequired);
+let sharedTests = new SharedTests(schemaTestHelper);
 
 describe('21-530 schema', () => {
-  // sharedTests.runTest('usaPhone', ['claimantPhone']);
+  sharedTests.runTest('usaPhone', ['claimantPhone']);
 
   it('should have the right required fields', () => {
     expect(schema.required).to.deep.equal(['privacyAgreementAccepted', 'claimantAddress', 'veteranFullName']);
@@ -28,108 +28,88 @@ describe('21-530 schema', () => {
 
   sharedTests.runTest('centralMailAddress', ['claimantAddress']);
 
-  // sharedTests.runTest('email', ['claimantEmail']);
+  sharedTests.runTest('email', ['claimantEmail']);
 
   sharedTests.runTest('files', ['deathCertificate', 'transportationReceipts']);
 
-  // schemaTestHelper.testValidAndInvalid('relationship', {
-  //   valid: [{
-  //     type: 'spouse'
-  //   }, {
-  //     type: 'other',
-  //     other: 'Whatever'
-  //   }],
-  //   invalid: [{
-  //     type: 'grandfather'
-  //   }, {
-  //     type: 'other',
-  //     other: 1
-  //   }]
-  // });
+  schemaTestHelper.testValidAndInvalid('relationship', {
+    valid: [{
+      type: 'spouse'
+    }, {
+      type: 'other',
+      other: 'Whatever'
+    }],
+    invalid: [{
+      type: 'grandfather'
+    }, {
+      type: 'other',
+      other: 1
+    }]
+  });
 
   schemaTestHelper.testValidAndInvalid('locationOfDeath', {
-    valid: [
-      {
-        location: 'vaMedicalCenter',
-      },
-      {
-        location: 'other',
-        other: 'Somewhere',
-      },
-    ],
-    invalid: [
-      {
-        location: 'asdfa',
-      },
-      {
-        location: 'other',
-        other: 1,
-      },
-    ],
+    valid: [{
+      location: 'vaMedicalCenter'
+    }, {
+      location: 'other',
+      other: 'Somewhere'
+    }],
+    invalid: [{
+      location: 'asdfa'
+    }, {
+      location: 'other',
+      other: 1
+    }]
   });
 
   schemaTestHelper.testValidAndInvalid('previousNames', {
     valid: [[fixtures.fullName, fixtures.fullName]],
-    invalid: [[false]],
+    invalid: [[false]]
   });
 
   schemaTestHelper.testValidAndInvalid('toursOfDuty', {
-    valid: [
-      [
-        {
-          serviceBranch: 'Army',
-          dateRange: {
-            to: '2010-01-01',
-            from: '2010-01-02',
-          },
-          rank: 'test',
-          serviceNumber: 'some text',
-          placeOfEntry: 'DC',
-          placeOfSeparation: 'VA',
-        },
-      ],
-    ],
-    invalid: [
-      [
-        {
-          serviceBranch: 'Army',
-          dateRange: '2010-01-02',
-          rank: 'test',
-          serviceNumber: 'some text',
-          placeOfEntry: 'DC',
-          placeOfSeparation: 'VA',
-        },
-      ],
-    ],
+    valid: [[{
+      serviceBranch: 'Army',
+      dateRange: {
+        to: '2010-01-01',
+        from: '2010-01-02'
+      },
+      rank: 'test',
+      serviceNumber: 'some text',
+      placeOfEntry: 'DC',
+      placeOfSeparation: 'VA'
+    }]],
+    invalid: [[{
+      serviceBranch: 'Army',
+      dateRange: '2010-01-02',
+      rank: 'test',
+      serviceNumber: 'some text',
+      placeOfEntry: 'DC',
+      placeOfSeparation: 'VA'
+    }]]
   });
 
-  [
-    'federalCemetery',
-    'stateCemetery',
-    'govtContributions',
-    'previouslyReceivedAllowance',
-    'benefitsUnclaimedRemains',
-  ].forEach(attr => {
+  ['federalCemetery', 'stateCemetery', 'govtContributions', 'previouslyReceivedAllowance', 'benefitsUnclaimedRemains', 'burialAllowance', 'plotAllowance', 'transportation'].forEach(attr => {
     schemaTestHelper.testValidAndInvalid(attr, {
       valid: [true, false],
-      invalid: ['012345678x', 1],
+      invalid: ['012345678x', 1]
     });
   });
 
-  ['burialCost'].forEach(attr => {
+  ['amountIncurred', 'burialCost', 'amountGovtContribution'].forEach(attr => {
     schemaTestHelper.testValidAndInvalid(attr, {
       valid: [3, 3.5],
-      invalid: ['012345678x', false],
+      invalid: ['012345678x', false]
     });
   });
 
   schemaTestHelper.testValidAndInvalid('burialAllowanceRequested', {
     valid: ['nonService', 'service', 'vaMC'],
-    invalid: [],
+    invalid: ['whatever', 3, false]
   });
 
   schemaTestHelper.testValidAndInvalid('placeOfRemains', {
     valid: ['whatever', '1'],
-    invalid: [3, false],
+    invalid: [3, false]
   });
 });
