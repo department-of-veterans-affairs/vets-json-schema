7ea07865614fa3fc9abbde4e7621b3f74e3ceb63
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-09-20 00:38:05 -0400

    updated schema

--- src/schemas/22-10282/schema.js
+++ src/schemas/22-10282/schema.js
@@ -5,14 +5,19 @@ import constants from '../../common/constants';
 
 const definitions = _.cloneDeep(originalDefinitions);
 const { salesforceCountries: countries } = constants;
-const nonUSACountries = countries.filter(country => country.value !== 'USA');
+// const nonUSACountries = countries.filter(country => country.value !== 'USA');
 
+definitions.country = {
+  type: 'string',
+  enum: countries.map(country => country.label),
+};
+const pickedDefinitions = _.pick(definitions, ['fullName', 'email', 'usaPhone', 'country']);
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: '22-10282 IBM Skillsbuild Training Program Intake Application',
   type: 'object',
   additionalProperties: false,
-  definitions: _.pick(definitions, ['fullName', 'description', 'usaPhone']),
+  definitions: pickedDefinitions,
   properties: {
     veteranFullName: {
       $ref: '#/definitions/fullName',
@@ -41,8 +46,7 @@ const schema = {
       $ref: '#/definitions/usaPhone',
     },
     country: {
-      type: 'string', // TYPE: text (255)
-      enum: nonUSACountries.map(country => country.label),
+      $ref: '#/definitions/country',
     },
     state: {
       type: 'string',
@@ -82,7 +86,7 @@ const schema = {
   },
   required: ['veteranFullName', 'veteranDesc', 'email', 'country', 'state'],
 };
-[['fullName', 'veteranFullName', 'country']].forEach(args => {
+[['fullName', 'veteranFullName']].forEach(args => {
   schemaHelpers.addDefinitionToSchema(schema, ...args);
 });
 export default schema;
