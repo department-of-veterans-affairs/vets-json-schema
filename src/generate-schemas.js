d406d9e56b311013086cc018461541ed7279011e
Tai Wilkin <tai.wilkin@coforma.io>
2024-10-03 14:03:16 -0400

    Test versioning script

--- src/generate-schemas.js
+++ src/generate-schemas.js
@@ -1,28 +1,52 @@
 import jsonfile from 'jsonfile';
 import fs from 'fs';
+import path from 'path';
+import { version } from '../package.json';
 import definitions from './common/definitions';
 import constants from './common/constants';
 import vaMedicalFacilities from './common/va-medical-facilities';
 import caregiverProgramFacilities from './common/caregiver-program-facilities';
 import form1010cgCertifications from './common/form-10-10cg-certifications';
-import path from 'path';
-import { dist_examples as distExamples } from './examples.js';
+import { dist_examples as distExamples } from './examples';
 
 const files = { definitions, constants, vaMedicalFacilities, caregiverProgramFacilities, form1010cgCertifications };
 
 fs.readdirSync('src/schemas').forEach(schema => {
   if (schema !== '.DS_Store') {
+    // write default schemafile
     jsonfile.writeFileSync(`dist/${schema.toUpperCase()}-schema.json`, require(`./schemas/${schema}/schema`).default, {
       spaces: 2,
+    });
+
+    // write default examples
+    const examples = distExamples(path.resolve(__dirname, '..'), schema);
+    Object.keys(examples).forEach(key => {
+      jsonfile.writeFileSync(`dist/${key}`, examples[key], { spaces: 2 });
     });
   }
 });
 
-fs.readdirSync('src/schemas').forEach(schema => {
-  if (schema !== '.DS_Store') {
-    const examples = distExamples(path.resolve(__dirname, '..'), schema);
-    Object.keys(examples).forEach(key => {
-      jsonfile.writeFileSync(`dist/${key}`, examples[key], { spaces: 2 });
+fs.mkdir(`dist/${version}/`, err => {
+  if (err) {
+    if (err.code === 'EEXIST') {
+      throw new Error('ERROR: Version folder already exists. Did you update the version number in package.json?');
+    } else {
+      throw new Error('Error creating version folder:', err);
+    }
+  } else {
+    fs.readdirSync('src/schemas').forEach(schema => {
+      if (schema !== '.DS_Store') {
+        // store schemafile under version
+        jsonfile.writeFileSync(
+          `dist/${version}/${schema.toUpperCase()}-schema.json`,
+          require(`./schemas/${schema}/schema`).default,
+        );
+        // store examples under version
+        const examples = distExamples(path.resolve(__dirname, '..'), schema);
+        Object.keys(examples).forEach(key => {
+          jsonfile.writeFileSync(`dist/${version}/${key}`, examples[key]);
+        });
+      }
     });
   }
 });
