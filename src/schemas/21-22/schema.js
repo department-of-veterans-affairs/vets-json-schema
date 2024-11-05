c0f5f985b2b3e651ac769f1eac4a511c14539fc2
Josh Fike <josh.fike@oddball.io>
2024-11-05 16:25:50 -0600

    2122 ready to go

--- src/schemas/21-22/schema.js
+++ src/schemas/21-22/schema.js
@@ -11,44 +11,44 @@ const schema = {
     veteran: {
       type: 'object',
       properties: {
-        veteranFullName: definitions.hcaFullName,
-        veteranSocialSecurityNumber: definitions.ssn,
-        veteranVaFileNumber: definitions.vaFileNumber,
-        veteranDateOfBirth: definitions.date,
-        veteranServiceNumber: definitions.veteranServiceNumber,
-        veteranMailingAddress: definitions.hcaAddress,
-        veteranPhone: definitions.hcaPhone,
-        veteranEmail: definitions.hcaEmail,
+        name: definitions.hcaFullName,
+        ssn: definitions.ssn,
+        vaFileNumber: definitions.vaFileNumber,
+        dateOfBirth: definitions.date,
+        serviceNumber: definitions.veteranServiceNumber,
+        address: definitions.hcaAddress,
+        phone: definitions.hcaPhone,
+        email: definitions.hcaEmail,
       },
       required: [
-        'veteranFullName',
-        'veteranSocialSecurityNumber',
-        'veteranDateOfBirth',
-        'veteranMailingAddress',
-        'veteranPhone'
+        'name',
+        'ssn',
+        'dateOfBirth',
+        'address',
+        'phone'
       ],
     },
     // Section II Claimant's Information
     claimant: {
       type: 'object',
       properties: {
-        claimantFullName: definitions.hcaFullName,
-        claimantDateOfBirth: definitions.date,
-        claimantRelationship: definitions.relationship,
-        claimantMailingAddress: definitions.hcaAddress,
-        claimantPhone: definitions.hcaPhone,
-        claimantEmail: definitions.hcaEmail,
+        name: definitions.hcaFullName,
+        dateOfBirth: definitions.date,
+        relationship: definitions.relationship,
+        address: definitions.hcaAddress,
+        phone: definitions.hcaPhone,
+        email: definitions.hcaEmail,
       },
       // If these are required here but the claimant object isn't required at
       // the top level, will that match our use case?  Our use case being that
       // the claimant object isn't required but if it is present, these fields
       // are required.
       required: [
-        'claimantFullName',
-        'claimantDateOfBirth',
-        'claimantRelationship',
-        'claimantMailingAddress',
-        'claimantPhone'
+        'name',
+        'dateOfBirth',
+        'relationship',
+        'address',
+        'phone'
       ],
     },
     // Section III Service Organization Information
@@ -61,47 +61,23 @@ const schema = {
       },
     },
     // Section IV Authorization Information
-    authorizationAccessProtectedRecords: { type: 'boolean' },
-    limitationsOfConsent: { 
+    recordConsent: { type: 'boolean' },
+    consentAddressChange: { type: 'boolean' },
+    consentLimits: { 
       type: 'array',
       items: { 
         type: 'string',
         enum: ['ALCOHOLISM', 'DRUG_ABUSE', 'HIV', 'SICKLE_CELL'],
        }
     },
-
-
-
-    // Fields we need but I'm unsure how to add as of yet:
-    // Org name - string
-    // Limitations of consent - Use some kind of enum? - include all option
-    // Address change authorization - boolean
-
-
-    // Does this file need all fields in the PDF, just those we need to 
-    // process the form, or those actually present in the front end form
-    // experience?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   },
-  required: ['veteranFullName', 'veteranSocialSecurityNumber', 'veteranDateOfBirth', 'veteranMailingAddress', 'veteranPhone'],
+  required: [
+    'veteran',
+    'representative',
+    'recordConsent',
+    'consentAddressChange',
+    'consentLimits',
+  ],
 };
 
 export default schema;
