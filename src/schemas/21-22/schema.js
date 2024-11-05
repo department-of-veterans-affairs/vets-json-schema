8011c721079c172cb9019de3919d5ae9b1a29b35
Josh Fike <josh.fike@oddball.io>
2024-11-05 12:59:32 -0600

    Start section IV

--- src/schemas/21-22/schema.js
+++ src/schemas/21-22/schema.js
@@ -13,30 +13,39 @@ const schema = {
   },
   type: 'object',
   properties: {
-    attachments: (() => {
-      const attachments = _.cloneDeep(definitions.files);
-      attachments.items.properties.dd214 = { type: 'boolean' };
-      return attachments;
-    })(),
+    // Section I Veteran's Information
     veteranFullName: definitions.hcaFullName,
     veteranSocialSecurityNumber: definitions.ssn,
     veteranVaFileNumber: definitions.vaFileNumber,
-    veteranDateOfBirth: {
-      $ref: '#/definitions/date',
-    },
+    veteranDateOfBirth: definitions.date,
     veteranServiceNumber: definitions.veteranServiceNumber,
     veteranMailingAddress: definitions.hcaAddress,
     veteranPhone: definitions.hcaPhone,
     veteranEmail: definitions.hcaEmail,
-
+    // Section II Claimant's Information
     claimantFullName: definitions.hcaFullName,
-    claimantDateOfBirth: {
-      $ref: '#/definitions/date',
-    },
+    claimantDateOfBirth: definitions.date,
     claimantRelationship: definitions.relationship,
     claimantMailingAddress: definitions.hcaAddress,
     claimantPhone: definitions.hcaPhone,
     claimantEmail: definitions.hcaEmail,
+    // Section III Service Organization Information
+    serviceOrganizationName: { type: 'string' },
+    serviceOrganizationRepresentativeName: { type: 'string' },
+    serviceOrganizationRepresentativeJobTitle: { type: 'string' },
+    serviceOrganizationEmail: definitions.hcaEmail,
+    dateOfAppointment: definitions.date,
+    // Section IV Authorization Information
+    authorizationAccessProtectedRecords: { type: 'boolean' },
+    limitationsOfConsent: { 
+      type: 'array',
+      items: { 
+        type: 'string',
+        enum: ['ALCOHOLISM', 'DRUG_ABUSE', 'HIV', 'SICKLE_CELL'],
+       }
+    },
+
+
 
     // Fields we need but I'm unsure how to add as of yet:
     // Org name - string
