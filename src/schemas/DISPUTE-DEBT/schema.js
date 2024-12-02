b3a613bbc389de53a39fc40c5a717308440d0910
Andrew Rodiek <25368370+Dr-Pongo@users.noreply.github.com>
2024-12-02 12:34:24 -0500

    Add schema for Dispute Debt (#961)

---
+++ src/schemas/DISPUTE-DEBT/schema.js
@@ -0,0 +1,33 @@
+import definitions from '../../common/definitions';
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'DISPUTE DEBT',
+  type: 'object',
+  properties: {
+    contactInformation: {
+      type: 'object',
+      email: definitions.email,
+      phoneNumber: definitions.phoneNumber,
+      address: definitions.profileAddress,
+    },
+    debtInformation: {
+      type: 'array',
+      items: {
+        type: 'object',
+        debt: {
+          type: 'string',
+        },
+        disputeReason: {
+          type: 'string',
+        },
+        supportStatement: {
+          type: 'string',
+        },
+      },
+    },
+  },
+  required: ['email', 'phoneNumber', 'address', 'debt', 'disputeReason', 'supportStatement'],
+};
+
+export default schema;
