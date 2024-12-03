8acc4523c37fd2c5c2dd399848cb9b026070b066
Michael Ghisilieri <175352110+mghisilieri@users.noreply.github.com>
2024-12-03 13:58:00 -0800

    22-10215 unit tests updated

--- test/schemas/22-10215/schema.spec.js
+++ test/schemas/22-10215/schema.spec.js
@@ -4,39 +4,58 @@ import { cloneDeep } from 'lodash';
 import schema from '../../../src/schemas/22-10215/schema';
 import SchemaTestHelper from '../../support/schema-test-helper';
 
-const schemaWithoutRequired = cloneDeep(schema);
-delete schemaWithoutRequired.required;
+const schemaClone = cloneDeep(schema);
 
-const schemaTestHelper = new SchemaTestHelper(schemaWithoutRequired);
+const schemaTestHelper = new SchemaTestHelper(schemaClone);
 
 const testData = {
   institutionDetails: {
-    institutionName: {
-      valid: 'Institution of Test',
-      invalid: null,
-    },
-    facilityCode: {
-      valid: 'IoT 123',
-      invalid: 123456,
-    },
-    termStartDate: {
-      valid: '2024-11-25',
-      invalid: 'start date',
-    },
-    dateOfCalculations: {
-      valid: '2024-11-28',
-      invalid: 20241128,
-    },
+    valid: [
+      {
+        institutionName: 'Institution of Test',
+        facilityCode: 'IoT 123',
+        termStartDate: '2024-11-25',
+        dateOfCalculations: '2024-11-28',
+      },
+    ],
+    invalid: [
+      {
+        institutionName: null,
+        facilityCode: 'IoT 123',
+        termStartDate: '2024-11-25',
+        dateOfCalculations: '2024-11-28',
+      },
+      {
+        institutionName: 'Institution of Test',
+        facilityCode: 123456,
+        termStartDate: '2024-11-25',
+        dateOfCalculations: '2024-11-28',
+      },
+      {
+        institutionName: 'Institution of Test',
+        facilityCode: 'IoT 123',
+        termStartDate: 'start date',
+        dateOfCalculations: '2024-11-28',
+      },
+      {
+        institutionName: 'Institution of Test',
+        facilityCode: 'IoT 123',
+        termStartDate: '2024-11-25',
+        dateOfCalculations: 20241128,
+      },
+    ],
   },
 };
 
 describe('22-10215 Schema', () => {
   it('should have required fields', () => {
-    expect(schema.required).to.deep.equal('institutionName', 'facilityCode', 'termStartDate', 'dateOfCalculations');
+    expect(schema.properties.institutionDetails.required).to.deep.equal([
+      'institutionName',
+      'facilityCode',
+      'termStartDate',
+      'dateOfCalculations',
+    ]);
   });
 
-  schemaTestHelper.testValidAndInvalid('institutionName', testData.institutionName);
-  schemaTestHelper.testValidAndInvalid('facilityCode', testData.facilityCode);
-  schemaTestHelper.testValidAndInvalid('termStartDate', testData.termStartDate);
-  schemaTestHelper.testValidAndInvalid('dateOfCalculations', testData.dateOfCalculations);
+  schemaTestHelper.testValidAndInvalid('institutionDetails', testData.institutionDetails);
 });
