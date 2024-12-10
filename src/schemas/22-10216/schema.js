c9187a46cde21a0a8fe92549c35547159ce96949
wafimohamed <158514667+wafimohamed@users.noreply.github.com>
2024-12-10 12:00:47 -0500

    added unit test

--- src/schemas/22-10216/schema.js
+++ src/schemas/22-10216/schema.js
@@ -3,12 +3,14 @@ import definitions from '../../common/definitions';
 
 const origDefinitions = _.cloneDeep(definitions);
 
+const pickedDefinitions = _.pick(origDefinitions, ['date']);
+
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: '22-10216 5% EXEMPTION REQUEST FROM 85/15 REPORTING REQUIREMENT GENERAL INFORMATION (22-10216)',
   type: 'object',
   additionalProperties: false,
-  definitions: origDefinitions,
+  definitions: pickedDefinitions,
   properties: {
     institutionDetails: {
       type: 'object',
--- test/schemas/22-10216/schema.spec.js
+++ test/schemas/22-10216/schema.spec.js
@@ -2,10 +2,51 @@ import { expect } from 'chai';
 import { it } from 'mocha';
 import { cloneDeep } from 'lodash';
 import schema from '../../../src/schemas/22-10216/schema';
+import SchemaTestHelper from '../../support/schema-test-helper';
 
 const schemaWithoutRequired = cloneDeep(schema);
-delete schemaWithoutRequired.required;
 
+const schemaTestHelper = new SchemaTestHelper(schemaWithoutRequired);
+
+const testData = {
+  institutionDetails: {
+    valid: [
+      {
+        institutionName: 'Test Institution',
+        facilityCode: '12345678',
+        termStartDate: '2024-01-01',
+      },
+    ],
+    invalid: [
+      {
+        institutionName: 'Test Institution',
+        facilityCode: '12345f6g',
+        termStartDate: '2024-01-01',
+      },
+    ],
+  },
+  studentRatioCalcChapter: {
+    valid: [
+      {
+        beneficiaryStudent: 1,
+        numOfStudent: 1,
+        dateOfCalculation: '2024-01-01',
+      },
+    ],
+    invalid: [
+      {
+        beneficiaryStudent: '1r',
+        numOfStudent: '1r',
+        dateOfCalculation: '20230101',
+      },
+      {
+        institutionName: 'Institution of Test',
+        facilityCode: 'IoT 123',
+        termStartDate: 'start date',
+      },
+    ],
+  },
+};
 describe('10216 schema', () => {
   it('should have required fields', () => {
     expect(schema.properties.institutionDetails.required).to.deep.equal([
@@ -18,5 +59,7 @@ describe('10216 schema', () => {
       'numOfStudent',
       'dateOfCalculation',
     ]);
+    schemaTestHelper.testValidAndInvalid('institutionDetails', testData.institutionDetails);
+    schemaTestHelper.testValidAndInvalid('studentRatioCalcChapter', testData.studentRatioCalcChapter);
   });
 });
