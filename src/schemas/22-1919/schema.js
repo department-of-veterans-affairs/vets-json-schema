358cc268b222c217ef1e4052cbd24c8206a15a65
Napoleon K <95312667+GcioNapoleon@users.noreply.github.com>
2025-03-04 11:24:01 -0600

    22*1919 Initialize 22-1919 form and add full name to the definitions section.

---
+++ src/schemas/22-1919/schema.js
@@ -0,0 +1,25 @@
+import definitions from '../../common/definitions';
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'CONFLICTING INTERESTS CERTIFICATION FOR PROPRIETARY SCHOOL',
+  type: 'object',
+  definitions: {
+    fullName: {
+      ...definitions.fullName,
+      properties: {
+        ...definitions.fullName.properties,
+        first: {
+          ...definitions.fullName.properties.first,
+          ...definitions.rejectOnlyWhitespace,
+        },
+        last: {
+          ...definitions.fullName.properties.last,
+          ...definitions.rejectOnlyWhitespace,
+        },
+      },
+    },
+  },
+};
+
+export default schema;
