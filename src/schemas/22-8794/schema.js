8481cdf866a5dca3f8d4fbaffd3fe9cafa40f953
fatmakhan0395 <145523589+fatmakhan0395@users.noreply.github.com>
2025-05-16 09:39:58 -0700

    adding schema for 22-8794

--- src/schemas/22-8794/schema.js
+++ src/schemas/22-8794/schema.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 import definitions from '../../common/definitions';
 
 const origDefinitions = _.cloneDeep(definitions);
-
 const pickedDefinitions = _.pick(origDefinitions, ['date']);
+
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'Designation Of Certifying Official(S) (22-8794)',
@@ -64,25 +64,155 @@ const schema = {
         type: 'string',
       },
       address: {
+        $ref: '#/definitions/addressSchema',
+      },
+      required: ['facilityCode', 'hasVaFacilityCode', 'address'],
+    },
+    primaryCertifyingOfficial: {
+      type: 'object',
+      required: ['first', 'last', 'title', 'phone', 'email'],
+      properties: {
+        first: {
+          type: 'string',
+        },
+        middle: {
+          type: 'string',
+        },
+        last: {
+          type: 'string',
+        },
+        title: {
+          type: 'string',
+        },
+        phone: {
+          type: 'string',
+          enum: ['usPhone', 'internationalPhone'],
+          description: 'Type of phone number',
+        },
+        usPhone: {
+          type: 'string',
+          pattern: '^(||d{10}|||+?[0-9])$',
+          description: 'Phone number',
+        },
+        internationalPhone: {
+          type: 'string',
+          pattern: '^(||d{15}|||+?[0-9])$',
+          description: 'Phone number',
+        },
+        email: {
+          type: 'string',
+          format: 'email',
+          description: 'Email address',
+        },
+        trainingCompletionDate: {
+          type: 'string',
+          format: 'date',
+          description: '305 training completion date',
+        },
+        hasVaEducationBenefits: {
+          type: 'string',
+          enum: ['yes', 'no'],
+          description: 'Does the certifying official have VA education benefits?',
+        },
+        trainingExempt: {
+          type: 'string',
+        },
+      },
+    },
+    additionalCertifyingOfficials: {
+      type: 'array',
+      items: {
         type: 'object',
+        required: [' hasAdditionalCertifyingOfficials'],
         properties: {
-          street: {
-            type: 'string',
-          },
-          city: {
-            type: 'string',
-          },
-          state: {
-            type: 'string',
-            pattern: '^[A-Z]{2}$',
-          },
-          zip: {
-            type: 'string',
-            pattern: '^\\d{5}(-\\d{4})?$',
+          additionalCertifyingOfficials: {
+            type: 'object',
+            required: ['first', 'last', 'title', 'phone', 'email'],
+            properties: {
+              first: {
+                type: 'string',
+              },
+              middle: {
+                type: 'string',
+              },
+              last: {
+                type: 'string',
+              },
+              title: {
+                type: 'string',
+              },
+              phone: {
+                type: 'string',
+                enum: ['usPhone', 'internationalPhone'],
+                description: 'Type of phone number',
+              },
+              usPhone: {
+                type: 'string',
+                pattern: '^(||d{10}|||+?[0-9])$',
+                description: 'Phone number',
+              },
+              internationalPhone: {
+                type: 'string',
+                pattern: '^(||d{15}|||+?[0-9])$',
+                description: 'Phone number',
+              },
+              email: {
+                type: 'string',
+                format: 'email',
+                description: 'Email address',
+              },
+              trainingCompletionDate: {
+                type: 'string',
+                format: 'date',
+                description: '305 training completion date',
+              },
+              hasVaEducationBenefits: {
+                type: 'string',
+                enum: ['yes', 'no'],
+                description: 'Does the certifying official have VA education benefits?',
+              },
+              trainingExempt: {
+                type: 'string',
+              },
+            },
           },
         },
       },
-      required: ['facilityCode', 'hasVaFacilityCode'],
+    },
+    hasReadOnlyCertifyingOfficial: {
+      type: 'string',
+      enum: ['yes', 'no'],
+      description: 'Do you have read-only certifying officials?',
+    },
+    readOnlyCertifyingOfficial: {
+      type: 'array',
+      items: {
+        type: 'object',
+        required: ['first', 'last'],
+        properties: {
+          first: {
+            type: 'string',
+          },
+          middle: {
+            type: 'string',
+          },
+          last: {
+            type: 'string',
+          },
+        },
+      },
+    },
+    remarks: {
+      type: 'string',
+      maxLength: 500,
+      description: 'Remarks',
+    },
+    statementOfTruthSignature: {
+      type: 'string',
+    },
+    dateSigned: {
+      $ref: '#/definitions/date',
     },
   },
 };
+export default schema;
