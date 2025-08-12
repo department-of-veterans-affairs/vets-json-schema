2c20e6be7ab013831c6c6c95480ab30f31f1ce0a
Midge-dev <sean.midgley.dev@gmail.com>
2025-08-12 10:25:33 -0700

    Dependents | Add char limit to generic_location_alt

--- src/schemas/686c-674-v2/schema.js
+++ src/schemas/686c-674-v2/schema.js
@@ -82,7 +82,7 @@ const schema = {
               properties: {
                 city: { type: 'string' },
                 state: { type: 'string' },
-                postal_code: { type: 'string' },
+                postal_code: { type: 'string', maxLength: 5 },
               },
               required: ['city', 'state', 'postal_code'],
             },
