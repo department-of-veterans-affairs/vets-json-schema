0caeaf5499fd90a5ec40dda1527e949f53418d93
taras <reloded2004@hotmail.com>
2025-08-19 10:49:18 -0400

    VEBT-2020: Form 22-0839 form schema

---
+++ src/schemas/22-0839/schema.js
@@ -0,0 +1,213 @@
+import _ from 'lodash';
+import definitions from '../../common/definitions';
+
+const origDefinitions = _.cloneDeep(definitions);
+const pickedDefinitions = _.pick(origDefinitions, [
+  'date',
+  'fullNameNoSuffix',
+  'dateRange',
+  'phone',
+  'yesNoSchema',
+  'email',
+  'profileAddress',
+]);
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  title: 'VA Form 22-0839',
+  type: 'object',
+  additionalProperties: false,
+  definitions: pickedDefinitions,
+  required: ['authorizedOfficial', 'agreementType', 'statementOfTruthSignature', 'dateSigned'],
+  properties: {
+    authorizedOfficial: {
+      type: 'object',
+      required: ['fullName', 'title', 'phoneNumber'],
+      properties: {
+        fullName: { $ref: '#/definitions/fullNameNoSuffix' },
+        title: {
+          type: 'string',
+          minLength: 1,
+          maxLength: 50,
+        },
+        phoneNumber: {
+          $ref: '#/definitions/phone',
+        },
+      },
+    },
+    agreementType: {
+      type: 'string',
+      enum: ['startNewOpenEndedAgreement', 'modifyExistingAgreement', 'withdrawFromYellowRibbonProgram'],
+    },
+    withdrawFromYellowRibbonProgram: {
+      type: 'array',
+      items: {
+        type: 'object',
+        required: ['facilityCode', 'institutionName', 'institutionAddress'],
+        properties: {
+          facilityCode: {
+            type: 'string',
+            pattern: '^[a-zA-Z0-9]{8}$',
+          },
+          institutionName: {
+            type: 'string',
+          },
+          institutionAddress: {
+            $ref: '#/definitions/profileAddress',
+          },
+        },
+      },
+    },
+    yellowRibbonProgramTerms: {
+      type: 'object',
+      required: [
+        'firstAcknowledgement',
+        'secondAcknowledgement',
+        'thirdAcknowledgement',
+        'fourthAcknowledgement',
+        'agreeToProvideYellowRibbonProgramContributions',
+      ],
+      properties: {
+        firstAcknowledgement: {
+          type: 'string',
+          minLength: 2,
+          maxLength: 3,
+        },
+        secondAcknowledgement: {
+          type: 'string',
+          minLength: 2,
+          maxLength: 3,
+        },
+        thirdAcknowledgement: {
+          type: 'string',
+          minLength: 2,
+          maxLength: 3,
+        },
+        fourthAcknowledgement: {
+          type: 'string',
+          minLength: 2,
+          maxLength: 3,
+        },
+
+        agreeToProvideYellowRibbonProgramContributions: { type: 'boolean' },
+      },
+    },
+    institutionDetails: {
+      type: 'array',
+      items: {
+        type: 'object',
+        required: ['facilityCode', 'institutionName', 'institutionAddress'],
+        properties: {
+          facilityCode: {
+            type: 'string',
+            pattern: '^(?!..[Xx])[A-Za-z0-9]{8}$',
+          },
+          institutionName: {
+            type: 'string',
+          },
+          institutionAddress: {
+            $ref: '#/definitions/profileAddress',
+          },
+        },
+      },
+    },
+    yellowRibbonProgramAgreementRequest: {
+      type: 'array',
+      items: {
+        type: 'object',
+        required: [
+          'eligibleIndividuals',
+          'yearRange',
+          'maximumNumberofStudents',
+          'degreeLevel',
+          'currencyType',
+          'maximumContributionAmount',
+        ],
+        properties: {
+          eligibleIndividuals: { type: 'integer', minimum: 0, maximum: 1000000 },
+          yearRange: {
+            $ref: '#/definitions/dateRange',
+          },
+          maximumNumberofStudents: { type: 'integer', minimum: 0, maximum: 1000000 },
+          degreeLevel: {
+            type: 'string',
+            enum: ['undergraduate', 'graduate', 'doctoral', 'all'],
+          },
+          degreeProgram: {
+            type: 'string',
+          },
+          //default: 'USD'
+          currencyType: {
+            type: 'string',
+          },
+          maximumContributionAmount: {
+            type: 'number',
+            minimum: 0,
+            maximum: 99999,
+          },
+        },
+      },
+    },
+    pointOfContact: {
+      type: 'object',
+      required: ['fullName', 'phoneNumber', 'emailAddress', 'role'],
+      properties: {
+        fullName: { $ref: '#/definitions/fullNameNoSuffix' },
+        phoneNumber: {
+          $ref: '#/definitions/phone',
+        },
+        emailAddress: definitions.email,
+        role: {
+          type: 'string',
+          enum: ['YellowRibbonProgramPOC', 'schoolFinancialRepresentative', 'schoolCertifyingOfficial'],
+        },
+      },
+    },
+    pointOfContactTwo: {
+      type: 'object',
+      required: ['fullName', 'phoneNumber', 'emailAddress', 'role'],
+      properties: {
+        fullName: { $ref: '#/definitions/fullNameNoSuffix' },
+        phoneNumber: {
+          $ref: '#/definitions/phone',
+        },
+        emailAddress: definitions.email,
+        role: {
+          type: 'string',
+          enum: ['YellowRibbonProgramPOC', 'schoolFinancialRepresentative', 'schoolCertifyingOfficial'],
+        },
+      },
+    },
+
+    statementOfTruthSignature: { type: 'string', minLength: 1 },
+    dateSigned: {
+      $ref: '#/definitions/date',
+    },
+  },
+  allOf: [
+    {
+      oneOf: [
+        {
+          properties: {
+            agreementType: { enum: ['withdrawFromYellowRibbonProgram'] },
+          },
+          required: ['agreementType', 'withdrawFromYellowRibbonProgram'],
+        },
+        {
+          properties: {
+            agreementType: { enum: ['startNewOpenEndedAgreement', 'modifyExistingAgreement'] },
+          },
+          required: [
+            'agreementType',
+            'yellowRibbonProgramTerms',
+            'institutionDetails',
+            'yellowRibbonProgramAgreementRequest',
+            'pointOfContact',
+          ],
+          not: { required: ['withdrawFromYellowRibbonProgram'] },
+        },
+      ],
+    },
+  ],
+};
+export default schema;
---
+++ test/schemas/22-0839/schema.spec.js
@@ -0,0 +1,319 @@
+import { cloneDeep, omit } from 'lodash';
+import { expect } from 'chai';
+import schema from '../../../src/schemas/22-0839/schema';
+import SchemaTestHelper from '../../support/schema-test-helper';
+
+const schemaClone = cloneDeep(schema);
+// Ignore root-level cross-field logic for the unit-style property tests:
+const schemaTestHelper = new SchemaTestHelper(omit(schemaClone, ['required', 'allOf']));
+
+const testData = {
+  authorizedOfficial: {
+    valid: [
+      { fullName: { first: 'Jane', last: 'Smith' }, title: 'Director', phoneNumber: '5555555555' },
+      { fullName: { first: 'A', last: 'B' }, title: 'VP', phoneNumber: '(555) 555-5555' },
+    ],
+    invalid: [{ fullName: { first: null, last: 'Smith' }, title: '', phoneNumber: 'abc' }],
+  },
+  agreementType: {
+    valid: ['startNewOpenEndedAgreement', 'modifyExistingAgreement', 'withdrawFromYellowRibbonProgram'],
+    invalid: ['somethingElse', '', null],
+  },
+  withdrawFromYellowRibbonProgram: {
+    valid: [
+      [
+        {
+          facilityCode: '12345678',
+          institutionName: 'Sample Campus',
+          institutionAddress: {
+            street: '111 2nd St S',
+            city: 'Seattle',
+            state: 'WA',
+            postalCode: '98101',
+            country: 'USA',
+          },
+        },
+      ],
+      [
+        {
+          facilityCode: 'A1B2C3D4',
+          institutionName: 'Another Campus',
+          institutionAddress: {
+            street: '1 First Ave',
+            city: 'Boston',
+            state: 'MA',
+            postalCode: '02108',
+            country: 'USA',
+          },
+        },
+        {
+          facilityCode: '87654321',
+          institutionName: 'Branch',
+          institutionAddress: {
+            street: '500 Main St',
+            city: 'Austin',
+            state: 'TX',
+            postalCode: '73301',
+            country: 'USA',
+          },
+        },
+      ],
+    ],
+    invalid: [[{ facilityCode: '123', institutionName: '', institutionAddress: null }]],
+  },
+  yellowRibbonProgramTerms: {
+    valid: [
+      {
+        firstAcknowledgement: 'AB',
+        secondAcknowledgement: 'CD',
+        thirdAcknowledgement: 'EF',
+        fourthAcknowledgement: 'YZ',
+        agreeToProvideYellowRibbonProgramContributions: true,
+      },
+    ],
+    invalid: [
+      {
+        firstAcknowledgement: 'TOO-LONG',
+        secondAcknowledgement: '',
+        thirdAcknowledgement: null,
+        fourthAcknowledgement: 'OK',
+        agreeToProvideYellowRibbonProgramContributions: null,
+      },
+    ],
+  },
+  institutionDetails: {
+    valid: [
+      [
+        {
+          facilityCode: '12345678',
+          institutionName: 'Institution of Test',
+          institutionAddress: {
+            street: '111 2nd St S',
+            city: 'Seattle',
+            state: 'WA',
+            postalCode: '98101',
+            country: 'USA',
+          },
+        },
+      ],
+      [
+        {
+          facilityCode: 'A1B2C3D4',
+          institutionName: 'Another Institution',
+          institutionAddress: {
+            street: '10 Downing St',
+            city: 'Boston',
+            state: 'MA',
+            postalCode: '02108',
+            country: 'USA',
+          },
+        },
+      ],
+    ],
+    invalid: [
+      [
+        {
+          facilityCode: '12',
+          institutionName: '',
+          institutionAddress: { street: '', city: 'Nowhere' },
+        },
+      ],
+    ],
+  },
+
+  yellowRibbonProgramAgreementRequest: {
+    valid: [
+      [
+        {
+          eligibleIndividuals: 25,
+          yearRange: { from: '2025-08-01', to: '2026-07-31' },
+          maximumNumberofStudents: 100,
+          degreeLevel: 'undergraduate',
+          currencyType: 'USD',
+          degreeProgram: 'Computer Science',
+          maximumContributionAmount: 5000.0,
+        },
+      ],
+      [
+        {
+          eligibleIndividuals: 0,
+          yearRange: { from: '1900-01-01', to: '1900-12-31' },
+          maximumNumberofStudents: 0,
+          degreeLevel: 'doctoral',
+          currencyType: 'USD',
+          degreeProgram: 'Engineering',
+          maximumContributionAmount: 0,
+        },
+        {
+          eligibleIndividuals: 1000,
+          yearRange: { from: '2100-01-01', to: '2100-12-31' },
+          maximumNumberofStudents: 99999,
+          degreeLevel: 'all',
+          currencyType: 'USD',
+          degreeProgram: 'Interdisciplinary',
+          maximumContributionAmount: 99999,
+        },
+      ],
+    ],
+    invalid: [
+      [
+        {
+          eligibleIndividuals: -1,
+          yearRange: '2025-2026',
+          maximumNumberofStudents: 1000001,
+          degreeLevel: 'middle-school',
+          degreeProgram: 123,
+          maximumContributionAmount: -5,
+        },
+      ],
+    ],
+  },
+
+  pointOfContact: {
+    valid: [
+      {
+        fullName: { first: 'Pat', last: 'Lee' },
+        phoneNumber: '555-555-5555',
+        emailAddress: 'pat.lee@example.edu',
+        role: 'schoolCertifyingOfficial',
+      },
+      {
+        fullName: { first: 'Alex', last: 'Kim' },
+        phoneNumber: '+1 (555) 111-2222',
+        emailAddress: 'a.kim@school.edu',
+        role: 'schoolFinancialRepresentative',
+      },
+    ],
+    invalid: [
+      {
+        fullName: { first: null, last: 'Lee' },
+        phoneNumber: 'abc',
+        emailAddress: 'not-an-email',
+        role: 'randomRole',
+      },
+    ],
+  },
+  statementOfTruthSignature: {
+    valid: ['Jane Q. Smith', 'J Smith'],
+    invalid: [null, ''],
+  },
+  dateSigned: {
+    valid: ['2025-08-01', '2024-01-31'],
+    invalid: ['08/01/2025', '2025-13-01', null],
+  },
+};
+
+describe('22-0839 schema', () => {
+  schemaTestHelper.testValidAndInvalid('authorizedOfficial', testData.authorizedOfficial);
+  schemaTestHelper.testValidAndInvalid('agreementType', testData.agreementType);
+  schemaTestHelper.testValidAndInvalid('withdrawFromYellowRibbonProgram', testData.withdrawFromYellowRibbonProgram);
+  schemaTestHelper.testValidAndInvalid('yellowRibbonProgramTerms', testData.yellowRibbonProgramTerms);
+  schemaTestHelper.testValidAndInvalid('institutionDetails', testData.institutionDetails);
+  schemaTestHelper.testValidAndInvalid(
+    'yellowRibbonProgramAgreementRequest',
+    testData.yellowRibbonProgramAgreementRequest,
+  );
+  schemaTestHelper.testValidAndInvalid('pointOfContact', testData.pointOfContact);
+  schemaTestHelper.testValidAndInvalid('statementOfTruthSignature', testData.statementOfTruthSignature);
+  schemaTestHelper.testValidAndInvalid('dateSigned', testData.dateSigned);
+});
+
+// ---- Cross-field business rule checks ----
+const requires = (obj, keys) => keys.every(k => Object.prototype.hasOwnProperty.call(obj, k));
+const forbids = (obj, keys) => keys.every(k => !Object.prototype.hasOwnProperty.call(obj, k));
+
+describe('22-0839 schema (cross-field business rules)', () => {
+  const base = {
+    authorizedOfficial: {
+      fullName: { first: 'Jane', last: 'Smith' },
+      title: 'Director',
+      phoneNumber: '5555555555',
+    },
+    statementOfTruthSignature: 'Jane Q. Smith',
+    dateSigned: '2025-08-01',
+  };
+
+  const usAddress = {
+    street: '111 2nd St S',
+    city: 'Seattle',
+    state: 'WA',
+    postalCode: '98101',
+    country: 'USA',
+  };
+
+  it('withdraw branch requires withdrawFromYellowRibbonProgram and forbids start/modify sections', () => {
+    const payload = {
+      ...base,
+      agreementType: 'withdrawFromYellowRibbonProgram',
+      withdrawFromYellowRibbonProgram: [
+        {
+          facilityCode: '12345678',
+          institutionName: 'Sample Campus',
+          institutionAddress: usAddress,
+        },
+      ],
+    };
+
+    expect(payload.agreementType).to.equal('withdrawFromYellowRibbonProgram');
+    expect(requires(payload, ['authorizedOfficial', 'statementOfTruthSignature', 'dateSigned'])).to.equal(true);
+    expect(requires(payload, ['withdrawFromYellowRibbonProgram'])).to.equal(true);
+    expect(
+      forbids(payload, [
+        'yellowRibbonProgramTerms',
+        'institutionDetails',
+        'yellowRibbonProgramAgreementRequest',
+        'pointOfContact',
+      ]),
+    ).to.equal(true);
+  });
+
+  it('start/modify branch requires terms, institution details, agreement request, and POC; forbids withdraw array', () => {
+    const payload = {
+      ...base,
+      agreementType: 'startNewOpenEndedAgreement',
+      yellowRibbonProgramTerms: {
+        firstAcknowledgement: 'AB',
+        secondAcknowledgement: 'CD',
+        thirdAcknowledgement: 'EF',
+        fourthAcknowledgement: 'GH',
+        agreeToProvideYellowRibbonProgramContributions: true,
+      },
+      institutionDetails: [
+        {
+          facilityCode: 'A1B2C3D4',
+          institutionName: 'Institution of Test',
+          institutionAddress: usAddress,
+        },
+      ],
+      yellowRibbonProgramAgreementRequest: [
+        {
+          eligibleIndividuals: 25,
+          yearRange: { from: '2025-08-01', to: '2026-07-31' },
+          maximumNumberofStudents: 100,
+          degreeLevel: 'undergraduate',
+          currencyType: 'USD',
+          degreeProgram: 'Computer Science',
+          maximumContributionAmount: 5000,
+        },
+      ],
+      pointOfContact: {
+        fullName: { first: 'Pat', last: 'Lee' },
+        phoneNumber: '555-555-5555',
+        emailAddress: 'pat.lee@example.edu',
+        role: 'schoolCertifyingOfficial',
+      },
+    };
+
+    expect(['startNewOpenEndedAgreement', 'modifyExistingAgreement']).to.include(payload.agreementType);
+    expect(requires(payload, ['authorizedOfficial', 'statementOfTruthSignature', 'dateSigned'])).to.equal(true);
+    expect(
+      requires(payload, [
+        'yellowRibbonProgramTerms',
+        'institutionDetails',
+        'yellowRibbonProgramAgreementRequest',
+        'pointOfContact',
+      ]),
+    ).to.equal(true);
+    expect(forbids(payload, ['withdrawFromYellowRibbonProgram'])).to.equal(true);
+  });
+});
