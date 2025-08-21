ba6acddcaf192868dde80dd4c8f25bb366173d33
Liz Wendling <170662489+GovCIOLiz@users.noreply.github.com>
2025-08-21 12:57:12 -0700

    Working on schema updates

--- src/schemas/22-10297/schema.js
+++ src/schemas/22-10297/schema.js
@@ -133,7 +133,6 @@ const schema = {
     'bankAccount',
     'veteranStatus',
     'isEmployed',
-    'highestLevelOfEducation',
     'privacyAgreementAccepted',
     'statementOfTruthSignature',
     'dateSigned',
