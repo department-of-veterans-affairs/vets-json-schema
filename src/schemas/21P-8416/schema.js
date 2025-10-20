49ed6a54ca95df6ca6d0e00d368051bb596bd3d5
Daniel Gading <2748839+dgading@users.noreply.github.com>
2025-10-20 16:28:46 -0400

    BIO Update 21P-8416 schema required fields (#1069)
    * 21P-8416 update required fields
    
    * Add more fields

--- src/schemas/21P-8416/schema.js
+++ src/schemas/21P-8416/schema.js
@@ -4,19 +4,36 @@ const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'MEDICAL EXPENSE REPORT (21P-8416)',
   type: 'object',
-  definitions: {},
-  properties: {
-    // Section I VETERAN'S IDENTIFICATION INFORMATION
-    veteran: {
-      type: 'object',
-      properties: {
-        veteranFullName: definitions.fullName,
-        veteranSsn: definitions.ssn,
-        veteranFileNumber: definitions.fileNumber,
-      },
-    },
+  definitions: {
+    email: definitions.email,
+    ssn: definitions.ssn,
+    vaFileNumber: definitions.vaFileNumber,
   },
-  required: ['privacyAgreementAccepted', 'veteranFullName', 'veteranSsn', 'veteranFileNumber'],
+  properties: {
+    claimantNotVeteran: { type: 'boolean' },
+    veteranSocialSecurityNumber: {
+      $ref: '#/definitions/ssn',
+    },
+    firstTimeReporting: { type: 'boolean' },
+    statementOfTruthSignature: { type: 'string' },
+    statementOfTruthCertified: { type: 'boolean' },
+    vaFileNumber: {
+      $ref: '#/definitions/vaFileNumber',
+    },
+    email: {
+      $ref: '#/definitions/email',
+    },
+    // TODO: Add remaining properties after reviewing IA guidance.
+    // veteranFullName
+    // primaryPhone
+    // claimantFullName
+    // claimantAddress
+    // careExpenses
+    // medicalExpenses
+    // mileageExpenses
+    // files
+  },
+  required: ['statementOfTruthSignature', 'statementOfTruthCertified', 'veteranSocialSecurityNumber'],
 };
 
 export default schema;
