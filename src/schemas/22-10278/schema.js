b75c01560fcf8720d30e9150e48799e161a6641f
govcioChad <162637891+govcioChad@users.noreply.github.com>
2025-10-30 08:56:32 -0700

    #vebt-2445 #comment Added skeleton for step 1

---
+++ src/schemas/22-10278/schema.js
@@ -0,0 +1,146 @@
+import _ from 'lodash';
+import definitions from '../../common/definitions';
+
+const origDefinitions = _.cloneDeep(definitions);
+
+const pickedDefinitions = _.pick(origDefinitions, [
+    'address',
+    'date',
+    'email',
+    'fullNameNoSuffix',
+    'phone',
+    'privacyAgreementAccepted',
+    'ssn',
+    'usaPhone',
+    'yesNoSchema',
+    'vaFileNumber'
+]);
+
+const schema = {
+    $schema: 'http://json-schema.org/draft-04/schema#',
+    title: 'Authorize VA to disclose personal information to a third party (VA Form 22-10278)',
+    type: 'object',
+    additionalProperties: false,
+    definitions: pickedDefinitions,
+    //required: ['agreementType', 'authorizedOfficial', 'mainInstitution', 'statementOfTruthSignature', 'dateSigned'],
+    properties: {
+        // fullName: { $ref: '#/definitions/fullNameNoSuffix' },
+        // ssn: {
+        //     $ref: '#/definitions/ssn',
+        // },
+        // vaFileNumber: {
+        //     $ref: '#/definitions/vaFileNumber',
+        // },
+        // dateOfBirth: {
+        //     $ref: '#/definitions/date',
+        // },
+        // mailingAddress: {
+        //     $ref: '#/definitions/address',
+        // },
+        // phoneNumber: {
+        //     $ref: '#/definitions/phone',
+        // },
+        // emailAddress: {
+        //     $ref: '#/definitions/email',
+        // },
+
+        claimantInformation: {
+            type: 'object',
+            required: ['fullName', 'ssn', 'emailAddress'],
+            properties: {
+                fullName: { $ref: '#/definitions/fullNameNoSuffix' },
+                ssn: {
+                    $ref: '#/definitions/ssn',
+                },
+                vaFileNumber: {
+                    $ref: '#/definitions/vaFileNumber',
+                },
+                dateOfBirth: {
+                    $ref: '#/definitions/date',
+                },
+                mailingAddress: {
+                    $ref: '#/definitions/address',
+                },
+                phoneNumber: {
+                    $ref: '#/definitions/phone',
+                },
+                emailAddress: {
+                    $ref: '#/definitions/email',
+                },
+            },
+            additionalInstitutions: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        facilityCode: {
+                            type: 'string',
+                            pattern: '^[a-zA-Z0-9]{8}$',
+                        },
+                        institutionName: {
+                            type: 'string',
+                        },
+                        institutionAddress: {
+                            $ref: '#/definitions/address',
+                        },
+                        pointOfContact: {
+                            type: 'object',
+                            properties: {
+                                fullName: {
+                                    $ref: '#/definitions/fullNameNoSuffix',
+                                },
+                                email: {
+                                    $ref: '#/definitions/email',
+                                },
+                            },
+                            required: ['fullName', 'email'],
+                        },
+                    },
+                    required: ['facilityCode', 'institutionName', 'institutionAddress', 'pointOfContact'],
+                },
+            },
+            authorizedOfficial: {
+                type: 'object',
+                properties: {
+                    fullName: {
+                        $ref: '#/definitions/fullNameNoSuffix',
+                    },
+                    title: {
+                        type: 'string',
+                    },
+                    usPhone: {
+                        $ref: '#/definitions/usaPhone',
+                    },
+                    internationalPhone: {
+                        $ref: '#/definitions/phone',
+                    },
+                    email: {
+                        $ref: '#/definitions/email',
+                    },
+                },
+                required: ['fullName', 'title', 'email'],
+                anyOf: [
+                    {
+                        required: ['usPhone'],
+                    },
+                    {
+                        required: ['internationalPhone'],
+                    },
+                ],
+                maxProperties: 4,
+            },
+            privacyAgreementAccepted: {
+                $ref: '#/definitions/privacyAgreementAccepted',
+            },
+            statementOfTruthSignature: {
+                type: 'string',
+                minLength: 1,
+            },
+            dateSigned: {
+                $ref: '#/definitions/date',
+            },
+        },
+    }
+};
+
+export default schema;
