c57710d38a70eeccdf6f2fc9ad4f3b405a56afa8
Gregory Hedrick <gregory@pixiitech.net>
2025-11-24 16:09:11 -0500

    Added schema for 21-0966 ITF
    - Fixed 0966 schema
    - Allow additional properties
    - Update version, dist
    - Set additionalProperties back to false
    - Make vaFileNumber optional

---
+++ src/schemas/21-0966/schema.js
@@ -0,0 +1,81 @@
+export default {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    veteran: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        ssn: {
+          type: 'string',
+          pattern: '^\\d{9}$',
+          example: '796126859',
+        },
+        dateOfBirth: {
+          type: ['string', 'null'],
+          format: 'date',
+          example: '1932-02-05',
+        },
+        postalCode: {
+          type: 'string',
+          pattern: '^\\d{5}$',
+          example: '12345',
+        },
+        vaFileNumber: {
+          type: ['string', 'null'],
+          pattern: "^[cC]{0,1}\\d{7,9}$",
+        },
+        name: {
+          type: 'object',
+          additionalProperties: false,
+          properties: {
+            first: {
+              type: 'string',
+              example: 'Hector',
+            },
+            last: {
+              type: 'string',
+              example: 'Allen',
+            },
+          },
+          required: ['first', 'last'],
+        },
+      },
+      required: ['ssn', 'dateOfBirth', 'postalCode', 'name'],
+    },
+    dependent: {
+      type: ['object', 'null'],
+      additionalProperties: false,
+      properties: {
+        ssn: {
+          type: ['string', 'null'],
+          pattern: '^\\d{9}$',
+          example: '796229088',
+        },
+        dateOfBirth: {
+          type: ['string', 'null'],
+          format: 'date',
+          example: '1976-01-16',
+        },
+        name: {
+          type: 'object',
+          additionalProperties: false,
+          properties: {
+            first: {
+              type: ['string', 'null'],
+              example: 'Derrick',
+            },
+            last: {
+              type: ['string', 'null'],
+              example: 'Reid',
+            },
+          },
+          required: ['first', 'last'],
+        },
+      },
+      required: ['ssn', 'dateOfBirth', 'name'],
+    },
+  },
+  required: ['veteran', 'dependent'],
+};
