5b55ac9a9e5301b0b53c66d4405bf540aa1c75ea
Daniel Gading <2748839+dgading@users.noreply.github.com>
2026-01-23 13:19:08 -0500

    section 1-3 updates

--- src/schemas/21P-534EZ/schema.js
+++ src/schemas/21P-534EZ/schema.js
@@ -1,20 +1,115 @@
 import definitions from '../../common/definitions';
+import schemaHelpers from '../../common/schema-helpers';
 
 const schema = {
   $schema: 'http://json-schema.org/draft-04/schema#',
   title: 'APPLICATION FOR D.I.C., SURVIVORS PENSION, AND/OR ACCRUED BENEFITS (21P-534EZ)',
   type: 'object',
-  definitions: {},
+  definitions: {
+    address: definitions.address,
+    date: definitions.date,
+    email: definitions.email,
+    fullName: definitions.fullName,
+    phone: definitions.phone,
+    serviceNumber: definitions.serviceNumber,
+    ssn: definitions.ssn,
+    vaFileNumber: definitions.vaFileNumber,
+  },
   properties: {
-    // Section I VETERAN'S IDENTIFICATION INFORMATION
-    veteran: {
+    // Section 1: Veteran's Identification Information
+    veteranFullName: {
+      $ref: '#/definitions/fullName',
+    },
+    veteranSocialSecurityNumber: {
+      $ref: '#/definitions/ssn',
+    },
+    vaFileNumber: {
+      $ref: '#/definitions/vaFileNumber',
+    },
+    veteranServiceNumber: {
+      $ref: '#/definitions/serviceNumber',
+    },
+    veteranDateOfBirth: {
+      $ref: '#/definitions/date',
+    },
+    vaClaimsHistory: { type: 'boolean' },
+    diedOnDuty: { type: 'boolean' },
+    veteranDateOfDeath: {
+      $ref: '#/definitions/date',
+    },
+    // Section 2: Claimant's Identification Information
+    claimantFullName: {
+      $ref: '#/definitions/fullName',
+    },
+    claimantRelationship: {
+      type: 'string',
+      enum: [
+        'SURVIVING_SPOUSE',
+        'CHILD_18-23_IN_SCHOOL',
+        'CUSTODIAN_FILING_FOR_CHILD_UNDER_18',
+        'HELPLESS_ADULT_CHILD',
+      ],
+    },
+    claimantSocialSecurityNumber: {
+      $ref: '#/definitions/ssn',
+    },
+    claimantDateOfBirth: {
+      $ref: '#/definitions/date',
+    },
+    claimantIsVeteran: { type: 'boolean' },
+    claimantAddress: {
+      $ref: '#/definitions/address',
+    },
+    claimantPhone: {
+      $ref: '#/definitions/phone',
+    },
+    claimantInternationalPhone: {
+      $ref: '#/definitions/phone',
+    },
+    claimantEmail: {
+      $ref: '#/definitions/email',
+    },
+    claims: {
       type: 'object',
       properties: {
-        veteranFullName: definitions.fullName,
-        veteranSsn: definitions.ssn,
-        veteranFileNumber: definitions.fileNumber,
+        DIC: { type: 'boolean' },
+        survivorsPension: { type: 'boolean' },
+        accruedBenefits: { type: 'boolean' },
       },
     },
+    // Section 3: Veteran's Service Information
+    veteranHasPreviousNames: { type: 'boolean' },
+    veteranPreviousNames: {
+      type: 'array',
+      items: {
+        $ref: '#/definitions/fullName',
+      },
+    },
+    activeServiceDateRange: schemaHelpers.getDefinition('dateRange'),
+    serviceBranch: {
+      type: 'string',
+      enum: ['army', 'navy', 'airForce', 'coastGuard', 'marineCorps', 'spaceForce', 'usphs', 'noaa'],
+    },
+    placeOfSeparation: { type: 'string' },
+    nationalGuardActivated: { type: 'boolean' },
+    nationalGuardActivationDate: {
+      $ref: '#/definitions/date',
+    },
+    unitNameAndAddress: { type: 'string' },
+    unitPhone: {
+      $ref: '#/definitions/phone',
+    },
+    pow: { type: 'boolean' },
+    powDateRange: schemaHelpers.getDefinition('dateRange'),
+    // Section 4: Marital Information
+    // Section 5: Marital History
+    // Section 6: Children of the Veteran Information
+    // Section 7: Dependency and Indemnity Compensation (D.I.C.)
+    // Section 8: Nursing Home or Increased Survivors Entitlement
+    // Section 9: Income And Assets (current income entries)
+    // Section 10: Information About Your Medical Or Other Expense
+    // Section 11: Direct Deposit Information
+    // Section 12: Claim Certification And Signature
   },
   required: [],
 };
