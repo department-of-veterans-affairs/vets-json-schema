8737747c0aeb678d08cdab8664e22192e80a5875
Yang Yang <yangyang@navapbc.com>
2026-02-26 16:19:24 -0500

    Ignore -template.json as non-schema file in test suite

--- dist/schemas.js
+++ dist/schemas.js
@@ -5,6 +5,11 @@ var schemas = {};
 fs.readdirSync(__dirname).forEach((fileName) => {
   if (fileName === 'schemas.js') return;
 
+  // The Fully Digital Forms initiative is temporarily hosting a non-schema JSON file here.
+  // It will be removed after an initial March 2026 pilot.
+  // See department-of-veterans-affairs/va.gov-team#117360 or join DSVA #benefits-fullydigitalforms
+  if (fileName.endsWith('-template.json')) return;
+
   schemas[fileName.replace('.json', '').replace('-schema', '')] = require('./' + fileName);
 });
 
